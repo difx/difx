31ee1b16fc43df930510ca992f1276da9843f5e2
adamdeller <adeller@swin.edu.au>
2007-07-26 02:25:34 +0000

    Initial SVN upload
    git-svn-id: https://svn.atnf.csiro.au/difx@6 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

---
+++ mpifxcorr/trunk/src/mpifxcorr.h
@@ -0,0 +1,19 @@
+/***************************************************************************
+ *   Copyright (C) 2006 by Adam Deller                                     *
+ *                                                                         *
+ *   This program is free for non-commercial use: see the license file     *
+ *   at http://astronomy.swin.edu.au:~adeller/software/difx/ for more      *
+ *   details.                                                              *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                  *
+ ***************************************************************************/
+#include <string>
+
+///constants for MPI ids
+namespace fxcorr
+{
+  static const int MANAGERID = 0;
+  static const int FIRSTTELESCOPEID = 1;
+}
