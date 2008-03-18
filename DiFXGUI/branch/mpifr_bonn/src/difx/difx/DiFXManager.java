2e73e75a6c6458857148b4e46c8053f1d5892fcd
DiFX-DEV <alef@mpifr-bonn.mpg.de>
2008-03-18 12:05:09 +0000

    code reformatting
    git-svn-id: https://svn.atnf.csiro.au/difx@368 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- DiFXGUI/branch/mpifr_bonn/src/difx/difx/DiFXManager.java
+++ DiFXGUI/branch/mpifr_bonn/src/difx/difx/DiFXManager.java
@@ -1,25 +1,28 @@
 package difx;
 
-
 import javax.swing.JFrame;
 
-public class DiFXManager {
+public class DiFXManager
+{
 
-	public static final int INITIAL_FRAME_WIDTH = 1200;
+    public static final int INITIAL_FRAME_WIDTH = 1200;
 
-	public static final int INITIAL_FRAME_HEIGHT = 800;
+    public static final int INITIAL_FRAME_HEIGHT = 800;
 
-	public static final int KEYWORD_LENGTH = 20;
+    public static final int KEYWORD_LENGTH = 20;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Stop from closing without checking
-		DiFXgui difx = new DiFXgui();
-		difx.setSize(INITIAL_FRAME_WIDTH, INITIAL_FRAME_HEIGHT);
-		difx.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-		difx.setVisible(true);
-	}
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+
+        // TODO Stop from closing without checking
+        DiFXgui difx = new DiFXgui();
+
+        difx.setSize(INITIAL_FRAME_WIDTH, INITIAL_FRAME_HEIGHT);
+        difx.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        difx.setVisible(true);
+    }
 
 }
