2c8704206346965d7089905a4d03ed9411f1ca66
DiFX-DEV <alef@mpifr-bonn.mpg.de>
2008-03-19 15:17:18 +0000

    + added navigation treeview + removed scrollbar. Use DifxPanel scrollbar instead. This gets rid of double scrollbar confusion. + rearanged code to be conform to JBuilder's GUI designer
    git-svn-id: https://svn.atnf.csiro.au/difx@377 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- DiFXGUI/branch/mpifr_bonn/src/difx/difx/CorrSummaryPanel.java
+++ DiFXGUI/branch/mpifr_bonn/src/difx/difx/CorrSummaryPanel.java
@@ -1,85 +1,266 @@
 package difx;
 
-
 import java.awt.Dimension;
-import java.awt.Font;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
-
 import javax.swing.Box;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import java.awt.BorderLayout;
+import javax.swing.JTree;
+import javax.swing.JSplitPane;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.TreePath;
+import java.awt.Color;
+import java.awt.Component;
 
+public class CorrSummaryPanel
+    extends DiFXPanel implements ComponentListener
+{
 
-public class CorrSummaryPanel extends DiFXPanel implements ComponentListener {
-	private JTextArea[] sectionsummaries;
+    private JTextArea[] sectionsummaries;
+    private int selectedNodeIndex = -1;
+    private JScrollPane scrollpane;
+    private static int NUM_SECTION_SUMMARIES = 5;
+    Box displaybox = Box.createVerticalBox();
+    JTextArea txtSection1 = new JTextArea();
+    JTextArea txtSection2 = new JTextArea();
+    JTextArea txtSection3 = new JTextArea();
+    JTextArea txtSection4 = new JTextArea();
+    JTextArea txtSection5 = new JTextArea();
 
-	private JScrollPane scrollpane;
+    JSplitPane splitpane = new JSplitPane();
+    JTree trvIndex = null;
 
-	private Box displaybox;
-	
-	private static int NUM_SECTION_SUMMARIES = 5;
+    /**
+     * Constructor
+     * @param parent DiFXgui
+     * @param corrconfig CorrelationConfig
+     */
+    public CorrSummaryPanel(DiFXgui parent, CorrelationConfig corrconfig)
+    {
 
-	public CorrSummaryPanel(DiFXgui parent, CorrelationConfig corrconfig) {
-		super(parent, corrconfig);
-		this.addComponentListener(this);
-		sectionsummaries = new JTextArea[NUM_SECTION_SUMMARIES];
-		displaybox = Box.createVerticalBox();
-		for (int i = 0; i < NUM_SECTION_SUMMARIES; i++) {
-			sectionsummaries[i] = new JTextArea();
-			sectionsummaries[i].setEditable(false);
-			sectionsummaries[i].setFont(new Font("Monospaced", Font.PLAIN, 12));
-			displaybox.add(sectionsummaries[i]);
-			displaybox.add(Box.createVerticalStrut(10));
-		}
-		scrollpane = new JScrollPane(displaybox);
-		scrollpane
-				.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-		scrollpane
-				.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-	}
+        super(parent, corrconfig);
+        try
+        {
+            this.addComponentListener(this);
+            scrollpane = new JScrollPane(displaybox);
 
-	private void displaysummary() {
-		mainpanel.removeAll();
-		mainpanel.repaint();
+            jbInit();
 
-		if (corrconfig.getNumConfigsLoaded() > 0) {
-			sectionsummaries[0].setText(corrconfig.getCommonSummary());
-			sectionsummaries[1].setText(corrconfig.getConfigSummary());
-			sectionsummaries[2].setText(corrconfig.getTelescopeSummary());
-			sectionsummaries[3].setText(corrconfig.getDatastreamSummary());
-			sectionsummaries[4].setText(corrconfig.getBaselineSummary());
+            sectionsummaries = new JTextArea[NUM_SECTION_SUMMARIES];
+            sectionsummaries[0] = txtSection1;
+            sectionsummaries[1] = txtSection2;
+            sectionsummaries[2] = txtSection3;
+            sectionsummaries[3] = txtSection4;
+            sectionsummaries[4] = txtSection5;
 
-			mainpanel.add(scrollpane);
-		}
-	}
+            trvIndex.setEnabled(false);
 
-	@Override
-	public void commitcorrchanges() {
-		// TODO Auto-generated method stub
+        }
 
-	}
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
 
-	@Override
-	public void refreshdisplay() {
-		// TODO Auto-generated method stub
-		if (corrconfig != null)
-			displaysummary();
-	}
+    }
 
-	public void componentHidden(ComponentEvent e) {
-	}
+    private void displaysummary()
+    {
+        mainpanel.removeAll();
+        mainpanel.repaint();
 
-	public void componentMoved(ComponentEvent e) {
-	}
+        if (corrconfig.getNumConfigsLoaded() > 0)
+        {
 
-	public void componentShown(ComponentEvent e) {
-	}
+            txtSection1.setText(corrconfig.getCommonSummary());
+            txtSection2.setText(corrconfig.getConfigSummary());
+            txtSection3.setText(corrconfig.getTelescopeSummary());
+            txtSection4.setText(corrconfig.getDatastreamSummary());
+            txtSection5.setText(corrconfig.getBaselineSummary());
 
-	public void componentResized(ComponentEvent e) {
-		if (e.getSource() == this) {
-			scrollpane.setPreferredSize(mainpanel.getSize());
-			doLayout();
-		}
-	}
+            trvIndex.setEnabled(true);
+        }
+
+    }
+
+    @Override
+    public void commitcorrchanges()
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void refreshdisplay()
+    {
+        // TODO Auto-generated method stub
+        if (corrconfig != null)
+        {
+            displaysummary();
+
+        }
+
+        if (txtSection1.getText() == "")
+        {
+            trvIndex.setEnabled(false);
+        }
+        else
+        {
+            trvIndex.setEnabled(true);
+
+        }
+
+    }
+
+    public void componentHidden(ComponentEvent e)
+    {
+    }
+
+    public void componentMoved(ComponentEvent e)
+    {
+    }
+
+    public void componentShown(ComponentEvent e)
+    {
+    }
+
+    public void componentResized(ComponentEvent e)
+    {
+        if (e.getSource() == this)
+        {
+            scrollpane.setPreferredSize(mainpanel.getSize());
+            doLayout();
+        }
+    }
+
+    private void jbInit() throws Exception
+    {
+        trvIndex = new JTree(fillTreeview());
+        displaybox = Box.createVerticalBox();
+        this.setMinimumSize(new Dimension(100, 46));
+        this.setPreferredSize(new Dimension(100, 37));
+        txtSection1.setEditable(false);
+        txtSection1.setText("");
+        txtSection2.setEditable(false);
+        txtSection2.setText("");
+        txtSection3.setEditable(false);
+        txtSection3.setText("");
+        txtSection4.setEditable(false);
+        txtSection4.setText("");
+        txtSection5.setEditable(false);
+        txtSection5.setText("");
+        scrollpane.setMinimumSize(new Dimension(222, 22));
+        scrollpane.setPreferredSize(new Dimension(600, 93));
+        trvIndex.addTreeSelectionListener(new
+                                          CorrSummaryPanel_trvIndex_treeSelectionAdapter(this));
+        trvIndex.setRootVisible(false);
+        displaybox.add(txtSection1);
+        displaybox.add(txtSection2);
+        displaybox.add(txtSection3);
+        displaybox.add(txtSection4);
+        displaybox.add(txtSection5);
+
+        this.add(splitpane, java.awt.BorderLayout.CENTER);
+        splitpane.add(scrollpane, JSplitPane.RIGHT);
+        splitpane.add(trvIndex, JSplitPane.LEFT);
+        scrollpane.getViewport().add(displaybox);
+    }
+
+    /**
+     * Fills the node object with the navigations treeview nodes
+     * @return DefaultMutableTreeNode
+     */
+    private DefaultMutableTreeNode fillTreeview()
+    {
+        DefaultMutableTreeNode rootNode = new DefaultMutableTreeNode();
+        DefaultMutableTreeNode node = null;
+
+        node = new DefaultMutableTreeNode("Common");
+        rootNode.add(node);
+        node = new DefaultMutableTreeNode("Config");
+        rootNode.add(node);
+        node = new DefaultMutableTreeNode("Telescope");
+        rootNode.add(node);
+        node = new DefaultMutableTreeNode("Datastream");
+        rootNode.add(node);
+        node = new DefaultMutableTreeNode("Baseline");
+        rootNode.add(node);
+
+        rootNode.add(node);
+
+        return (rootNode);
+    }
+
+    /**
+     * Highlights the section selected from the treeview. The scrollbar position is set
+     * to the beginning of the currently selected section
+     */
+    private void markSelectedSection()
+    {
+
+        if ( (selectedNodeIndex < 0) ||
+            (selectedNodeIndex > NUM_SECTION_SUMMARIES))
+        {
+            return;
+        }
+        int lineCount = 0;
+        scrollpane.getVerticalScrollBar().setValue(0);
+
+        for (int i = 0; i < selectedNodeIndex; i++)
+        {
+            lineCount += sectionsummaries[i].getHeight();
+        }
+
+        scrollpane.getVerticalScrollBar().setValue(lineCount);
+
+        txtSection1.setBackground(Color.white);
+        txtSection2.setBackground(Color.white);
+        txtSection3.setBackground(Color.white);
+        txtSection4.setBackground(Color.white);
+        txtSection5.setBackground(Color.white);
+
+        txtSection1.setForeground(Color.black);
+        txtSection2.setForeground(Color.black);
+        txtSection3.setForeground(Color.black);
+        txtSection4.setForeground(Color.black);
+        txtSection5.setForeground(Color.black);
+
+        sectionsummaries[selectedNodeIndex].setBackground(Color.getHSBColor(0,
+            0, (float) 0.95));
+        sectionsummaries[selectedNodeIndex].setForeground(Color.red);
+
+    }
+
+    public void trvIndex_valueChanged(TreeSelectionEvent e)
+    {
+
+        TreePath treePath = trvIndex.getSelectionPath();
+        int selectedNodes[] = trvIndex.getSelectionRows();
+
+        if (selectedNodes.length == 1)
+        {
+            selectedNodeIndex = selectedNodes[0];
+            markSelectedSection();
+        }
+
+    }
+}
+
+class CorrSummaryPanel_trvIndex_treeSelectionAdapter
+    implements TreeSelectionListener
+{
+    private CorrSummaryPanel adaptee;
+    CorrSummaryPanel_trvIndex_treeSelectionAdapter(CorrSummaryPanel adaptee)
+    {
+        this.adaptee = adaptee;
+    }
+
+    public void valueChanged(TreeSelectionEvent e)
+    {
+        adaptee.trvIndex_valueChanged(e);
+    }
 }
