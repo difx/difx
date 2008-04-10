3331fc25aefb566bb2080d77b042e998fb2f6920
walterfb <wbrisken@gmail.com>
2008-04-10 14:33:31 +0000

    add memusage to loadmon
    git-svn-id: https://svn.atnf.csiro.au/difx@420 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- common/difxmessage/trunk/utils/loadmon.c
+++ common/difxmessage/trunk/utils/loadmon.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 #include "difxmessage.h"
 
 
@@ -10,12 +11,15 @@ int doload(const char *host)
 	float l1, l5, l15;
 	char line[100];
 	DifxMessageLoad load;
+	int memused=0, memtot=0;
+	char key[64];
+	int val;
 
+	/* LOAD */
 	in = fopen("/proc/loadavg", "r");
 	if(!in)
 	{
-		sprintf(message, "%s Error -- cannot open /proc/loadavg", 
-			host);
+		sprintf(message, "%s Error -- cannot open /proc/loadavg", host);
 		difxMessageSend(message);
 		return -1;
 	}
@@ -24,10 +28,40 @@ int doload(const char *host)
 	sscanf(line, "%f%f%f", &l1, &l5, &l15);
 
 	fclose(in);
+
+	/* MEMORY USAGE */
+	in = fopen("/proc/meminfo", "r");
+	if(!in)
+	{
+		sprintf(message, "%s Error -- cannot open /proc/meminfo", host);
+		difxMessageSend(message);
+		return -1;
+	}
+	for(;;)
+	{
+		fgets(line, 99, in);
+		if(feof(in))
+		{
+			break;
+		}
+		sscanf(line, "%s%d", key, &val);
+		if(strcmp(key, "MemTotal:") == 0)
+		{
+			memtot = val;
+			memused += val;
+		}
+		if(strcmp(key, "MemFree:") == 0 ||
+		   strcmp(key, "Buffers:") == 0 ||
+		   strcmp(key, "Cached:") == 0)
+		{
+			memused -= val;
+		}
+	}
+	fclose(in);
 	
 	load.cpuLoad = l1;
-	load.totalMemory = 1;
-	load.usedMemory = 0;
+	load.totalMemory = memtot;
+	load.usedMemory = memused;
 	
 	return difxMessageSendLoad(&load);
 }
