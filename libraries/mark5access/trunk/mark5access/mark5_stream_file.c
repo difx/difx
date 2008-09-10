b6a9c7d853ecacee53c56eeab3014e7bef467b1f
HelgeRottmann <rottmann@mpifr-bonn.mpg.de>
2008-09-10 14:36:08 +0000

    added svn properties to header
    git-svn-id: https://svn.atnf.csiro.au/difx@775 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- libraries/mark5access/trunk/mark5access/mark5_stream_file.c
+++ libraries/mark5access/trunk/mark5access/mark5_stream_file.c
@@ -16,6 +16,16 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
+//===========================================================================
+// SVN properties (DO NOT CHANGE)
+//
+// $Id$
+// $HeadURL$
+// $LastChangedRevision$
+// $Author$
+// $LastChangedDate$
+//
+//============================================================================
 
 #ifndef _GNU_SOURCE
 #define _GNU_SOURCE
