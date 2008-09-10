ed4cbbf00db55f419254443afd0157dff496aadb
HelgeRottmann <rottmann@mpifr-bonn.mpg.de>
2008-09-10 14:04:21 +0000

    add svn properties to header
    git-svn-id: https://svn.atnf.csiro.au/difx@767 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- mpifxcorr/trunk/src/architecture.h
+++ mpifxcorr/trunk/src/architecture.h
@@ -16,6 +16,16 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
+//===========================================================================
+// SVN properties (DO NOT CHANGE)
+//
+// $Id$
+// $HeadURL$
+// $LastChangedRevision$
+// $Author$
+// $LastChangedDate$
+//
+//============================================================================
 
 /** \file architecture.h
  *  \brief File contains mapping for vector functions to specific architectures
