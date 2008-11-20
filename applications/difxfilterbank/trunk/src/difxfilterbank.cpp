24b0318fa19ae5ae88a9386f0ad69b900e24b5d6
adamdeller <adeller@swin.edu.au>
2008-11-20 01:21:11 +0000

    Added bare bones of difxfilterbank program for catching short-term accumulation messages from mpifxcorr and storing to disk
    git-svn-id: https://svn.atnf.csiro.au/difx@866 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

---
+++ applications/difxfilterbank/trunk/src/difxfilterbank.cpp
@@ -0,0 +1,18 @@
+#include <difxmessage.h>
+#include "configuration.h"
+
+int main(int argc, const char * argv[])
+{
+  //parse the config file
+
+  //wait for a status message indicating correlation 
+  //is ready to receive messages
+
+  //send the message to start the STA dumping
+
+  //while not terminate, get message, inspect and dump
+
+  //close the dump file
+
+  return EXIT_SUCCESS;
+}
