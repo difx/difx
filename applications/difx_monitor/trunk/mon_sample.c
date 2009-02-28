e51c79c45baf209d3fddf8b2f54c09c100073c97
adamdeller <adeller@swin.edu.au>
2009-02-28 04:50:57 +0000

    Use pgplot to plot
    git-svn-id: https://svn.atnf.csiro.au/difx@1057 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- applications/difx_monitor/trunk/mon_sample.c
+++ applications/difx_monitor/trunk/mon_sample.c
@@ -13,15 +13,18 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <cpgplot.h>
+#include <complex>
 
 #include "architecture.h"
 #include "monserver.h"
 
 
 int main(int argc, const char * argv[]) {
-  int status, prod;
+  int status, prod, i, nchan=0;
   char monhostname[] = "localhost";
   struct monclient monserver;
+  float *xval=NULL, *yval=NULL, min, max;
   cf32 *vis;
 
   status  = monserver_connect(&monserver, monhostname, -1);
@@ -31,6 +34,13 @@ int main(int argc, const char * argv[]) {
 
   status = monserver_requestproduct(monserver, 0);
   if (status) exit(1);
+
+  status = cpgbeg(0,"/xs",1,1);
+  if (status != 1) {
+    printf("Error opening pgplot device\n");
+  }
+  
+  cpgask(0);
 
   printf("Sent product request\n");
 
@@ -42,6 +52,35 @@ int main(int argc, const char * argv[]) {
 
       while (!monserver_nextvis(&monserver, &prod, &vis)) {
 	printf("Got visibility for product %d\n", prod);
+
+	if (nchan!=monserver.numchannels) {
+	  nchan = monserver.numchannels;
+	  if (xval!=NULL) delete [] xval;
+	  if (yval!=NULL) delete [] yval;
+	  xval = new float [nchan];
+	  yval =  new float [nchan];
+	  if (xval==NULL || yval==NULL) {
+	    fprintf(stderr, "Failed to allocate memory for plotting arrays\n");
+	    exit(1);
+	  }
+	  for (i=0; i<nchan; i++) {
+	    xval[i] = i;
+	  }
+	}
+
+	status = ippsMagnitude_32fc(vis, yval, nchan);
+	if (status != vecNoErr) {
+	  printf("Error calculating vector magnitude\n");
+	}
+	ippsMax_32f(yval, nchan, &max);
+	ippsMin_32f(yval, nchan, &min);
+
+	cpgsci(1);
+	cpgenv(0,nchan,min,max,0,0);
+	cpglab("Channel", "Amplitude", "");
+	cpgsci(2);
+	cpgline(nchan, xval, yval);
+
       }
       printf("\n");
     }
