4902d226e8c09f91a1adea8d051dd444afe80905
adamdeller <adeller@swin.edu.au>
2009-02-28 05:45:47 +0000

    Plot lags and phas
    git-svn-id: https://svn.atnf.csiro.au/difx@1058 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- applications/difx_monitor/trunk/mon_sample.c
+++ applications/difx_monitor/trunk/mon_sample.c
@@ -24,8 +24,10 @@ int main(int argc, const char * argv[]) {
   int status, prod, i, nchan=0;
   char monhostname[] = "localhost";
   struct monclient monserver;
-  float *xval=NULL, *yval=NULL, min, max;
+  float *xval=NULL, *amp=NULL, *phase=NULL, *lags=NULL, *lagx=NULL;
+  float delta, min, max, temp;
   cf32 *vis;
+  IppsFFTSpec_R_32f* fftspec=NULL;
 
   status  = monserver_connect(&monserver, monhostname, -1);
   if (status) exit(1);
@@ -35,7 +37,7 @@ int main(int argc, const char * argv[]) {
   status = monserver_requestproduct(monserver, 0);
   if (status) exit(1);
 
-  status = cpgbeg(0,"/xs",1,1);
+  status = cpgbeg(0,"/xs",1,3);
   if (status != 1) {
     printf("Error opening pgplot device\n");
   }
@@ -55,31 +57,83 @@ int main(int argc, const char * argv[]) {
 
 	if (nchan!=monserver.numchannels) {
 	  nchan = monserver.numchannels;
-	  if (xval!=NULL) delete [] xval;
-	  if (yval!=NULL) delete [] yval;
-	  xval = new float [nchan];
-	  yval =  new float [nchan];
-	  if (xval==NULL || yval==NULL) {
+	  if (xval!=NULL) vectorFree(xval);
+	  if (amp!=NULL) vectorFree(amp);
+	  if (phase!=NULL) vectorFree(phase);
+	  if (lags!=NULL) vectorFree(lags);
+	  if (lagx!=NULL) vectorFree(lagx);
+	  if (fftspec!=NULL) ippsFFTFree_R_32f(fftspec);
+
+	  xval = vectorAlloc_f32(nchan);
+	  amp = vectorAlloc_f32(nchan);
+	  phase = vectorAlloc_f32(nchan);
+	  lags = vectorAlloc_f32(nchan*2);
+	  lagx = vectorAlloc_f32(nchan*2);
+
+	  if (xval==NULL || amp==NULL || phase==NULL || lags==NULL || lagx==NULL) {
 	    fprintf(stderr, "Failed to allocate memory for plotting arrays\n");
 	    exit(1);
 	  }
 	  for (i=0; i<nchan; i++) {
 	    xval[i] = i;
 	  }
+	  for (i=-nchan; i<nchan; i++) {
+	    lagx[i+nchan] = i;
+	  }
+
+	  int order = 0;
+	  while(((nchan*2) >> order) > 1)
+	    order++;
+	  ippsFFTInitAlloc_R_32f(&fftspec, order, IPP_FFT_NODIV_BY_ANY, ippAlgHintFast);
 	}
 
-	status = ippsMagnitude_32fc(vis, yval, nchan);
-	if (status != vecNoErr) {
-	  printf("Error calculating vector magnitude\n");
+	ippsMagnitude_32fc(vis, amp, nchan);
+	ippsPhase_32fc(vis, phase, nchan);
+	vectorMulC_f32_I(180/M_PI, phase, nchan);
+
+	ippsFFTInv_CCSToR_32f((Ipp32f*)vis, lags, fftspec, 0);
+	//rearrange the lags into order
+	for(i=0;i<nchan;i++) {
+	  temp = lags[i];
+	  lags[i] = lags[i+nchan];
+	  lags[i+nchan] = temp;
 	}
-	ippsMax_32f(yval, nchan, &max);
-	ippsMin_32f(yval, nchan, &min);
+
+	ippsMax_32f(amp, nchan, &max);
+	ippsMin_32f(amp, nchan, &min);
+	delta = (max-min)*0.05;
+	min -= delta;
+	max += delta;
+	printf("MAX=%f, MIN=%f\n", max, min);
 
 	cpgsci(1);
 	cpgenv(0,nchan,min,max,0,0);
 	cpglab("Channel", "Amplitude", "");
 	cpgsci(2);
-	cpgline(nchan, xval, yval);
+	cpgline(nchan, xval, amp);
+
+	ippsMax_32f(phase, nchan, &max);
+	ippsMin_32f(phase, nchan, &min);
+	delta = (max-min)*0.1;
+	min -= delta;
+	max += delta;
+	cpgsci(1);
+	cpgenv(0,nchan,min,max,0,0);
+	cpglab("Channel", "Amplitude", "");
+	cpgsci(2);
+	cpgpt(nchan, xval, phase, 17);
+
+
+	ippsMax_32f(lags, nchan*2, &max);
+	ippsMin_32f(lags, nchan*2, &min);
+	delta = (max-min)*0.05;
+	min -= delta;
+	max += delta;
+	cpgsci(1);
+	cpgenv(-nchan,nchan,min,max,0,0);
+	cpglab("Channel", "Amplitude", "");
+	cpgsci(2);
+	cpgline(nchan*2, lagx, lags);
 
       }
       printf("\n");
