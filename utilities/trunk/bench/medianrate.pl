e339b5b8946085898572b6d3142d73b71f888679
XhrisPhillips <Chris.Phillips@csiro.au>
2009-05-28 01:27:20 +0000

    Initial Version
    git-svn-id: https://svn.atnf.csiro.au/difx@1186 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

---
+++ utilities/trunk/bench/medianrate.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+foreach my $fname (@ARGV) {
+  open(FILE, $fname) || die "Error opening $fname: $!\n";
+  
+  my @rates = ();
+  while (<FILE>) {
+    if (/^\s*(\S+)\s+Mbps\/s\s+\S+\s+sec\s+\S+\s+MB/) {
+      push @rates, $1;
+    }
+  }
+  close(FILE);
+
+  @rates = sort {$a <=> $b} @rates;
+
+  $rates[0] = 'na' if (@rates==0);
+
+  my $mid = length(@rates)/2;
+
+  printf "$fname %s\n", $rates[$mid];
+
+}
