0b4ce6697b1c510e4228c07a66b510985c04915c
adamdeller <adeller@swin.edu.au>
2009-06-30 15:16:06 +0000

    Added capability to compare two or more DiFX files directly to plotDiFX
    git-svn-id: https://svn.atnf.csiro.au/difx@1264 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- utilities/trunk/vis2screen/plotDiFX.py
+++ utilities/trunk/vis2screen/plotDiFX.py
@@ -2,74 +2,150 @@
 import sys, os, struct, time, pylab, math
 from numpy import fft
 
-if not len(sys.argv) == 2:
-    print "Usage: plotDiFX <difx file>\n\nFlashes bandpasses at a rate of 5 Hz"
+if len(sys.argv) < 2:
+    print "Usage: plotDiFX <difx file 1> [difx file 2] ... [difx file N]\n\nFlashes bandpasses at a rate of 5 Hz"
     sys.exit()
 
+numfiles = len(sys.argv[1:])
+MAX_CHANNELS = 16384
+
 chans = []
-for i in range(16384):
+amp = []
+phase = []
+vis = []
+lag = []
+lagamp = []
+for i in range(numfiles):
+    amp.append([])
+    phase.append([])
+    vis.append([])
+    lag.append([])
+    lagamp.append([])
+    
+for i in range(MAX_CHANNELS):
     chans.append(i)
+    for j in range(numfiles):
+        amp[j].append(0.0)
+        phase[j].append(0.0)
+        vis[j].append(0.0)
+        lag[j].append(0.0)
+        lagamp[j].append(0.0)
+    
+
 pylab.xlabel("Channel")
 pylab.ylabel("Amplitude")
+linestyles = ['b', 'r', 'g', 'k', 'y']
+difxinputs  = []
+lines       = []
+baseline    = []
+mjd         = []
+seconds     = []
+configindex = []
+sourceindex = []
+freqindex   = []
+polpair     = []
+psrbin      = []
+flag        = []
+weight      = []
+u           = []
+v           = []
+w           = []
+nchan       = []
+for i in range(numfiles):
+    lines.append("")
+    baseline.append(0)
+    mjd.append(0)
+    seconds.append(0.0)
+    configindex.append(0)
+    sourceindex.append(0)
+    freqindex.append(0)
+    polpair.append("")
+    psrbin.append(0)
+    flag.append(0)
+    weight.append(0.0)
+    u.append(0.0)
+    v.append(0.0)
+    w.append(0.0)
+    nchan.append(0)
 
-difxin = open(sys.argv[1])
-line = difxin.readline()
-while not line == "":
-    amp = []
-    phase = []
-    vis = []
-    lag = []
-    lagamp = []
-    baseline = int((line.split(':')[1]).strip())
-    line = difxin.readline()
-    mjd = int((line.split(':')[1]).strip())
-    line = difxin.readline()
-    seconds = float((line.split(':')[1]).strip())
-    line = difxin.readline()
-    configindex = int((line.split(':')[1]).strip())
-    line = difxin.readline()
-    sourceindex = int((line.split(':')[1]).strip())
-    line = difxin.readline()
-    freqindex = int((line.split(':')[1]).strip())
-    line = difxin.readline()
-    polpair = (line.split(':')[1]).strip()
-    line = difxin.readline()
-    psrbin = int((line.split(':')[1]).strip())
-    line = difxin.readline()
-    flag = int((line.split(':')[1]).strip())
-    line = difxin.readline()
-    weight = float((line.split(':')[1]).strip())
-    line = difxin.readline()
-    u = float((line.split(':')[1]).strip())
-    line = difxin.readline()
-    v = float((line.split(':')[1]).strip())
-    line = difxin.readline()
-    w = float((line.split(':')[1]).strip())
-    nchan = 0
-    nextcfloat = difxin.read(8)
-    while not nextcfloat == "BASELINE" and not nextcfloat == "":
-        cvis = struct.unpack("ff", nextcfloat)
-        vis.append(complex(cvis[0], cvis[1]))
-        amp.append(math.sqrt(cvis[0]*cvis[0] + cvis[1]*cvis[1]))
-        phase.append(math.atan2(cvis[1], cvis[0]))
-        nchan = nchan + 1
-        nextcfloat = difxin.read(8)
-    lag = fft.ifft(vis, nchan)
-    for l in lag:
-        lagamp.append(abs(l))
-    lagamp = lagamp[nchan/2:] + lagamp[:nchan/2]
+for filename in sys.argv[1:]:
+    difxinputs.append(open(filename))
+
+for i in range(numfiles):
+    lines[i] = difxinputs[i].readline()
+while not lines[0] == "":
+    for i in range(numfiles):
+        baseline[i] = int((lines[i].split(':')[1]).strip())
+        lines[i] = difxinputs[i].readline()
+        mjd[i] = int((lines[i].split(':')[1]).strip())
+        lines[i] = difxinputs[i].readline()
+        seconds[i] = float((lines[i].split(':')[1]).strip())
+        lines[i] = difxinputs[i].readline()
+        configindex[i] = int((lines[i].split(':')[1]).strip())
+        lines[i] = difxinputs[i].readline()
+        sourceindex[i] = int((lines[i].split(':')[1]).strip())
+        lines[i] = difxinputs[i].readline()
+        freqindex[i] = int((lines[i].split(':')[1]).strip())
+        lines[i] = difxinputs[i].readline()
+        polpair[i] = (lines[i].split(':')[1]).strip()
+        lines[i] = difxinputs[i].readline()
+        psrbin[i] = int((lines[i].split(':')[1]).strip())
+        lines[i] = difxinputs[i].readline()
+        flag[i] = int((lines[i].split(':')[1]).strip())
+        lines[i] = difxinputs[i].readline()
+        weight[i] = float((lines[i].split(':')[1]).strip())
+        lines[i] = difxinputs[i].readline()
+        u[i] = float((lines[i].split(':')[1]).strip())
+        lines[i] = difxinputs[i].readline()
+        v[i] = float((lines[i].split(':')[1]).strip())
+        lines[i] = difxinputs[i].readline()
+        w[i] = float((lines[i].split(':')[1]).strip())
+        nchan[i] = 0
+        nextcfloat = difxinputs[i].read(8)
+        while not nextcfloat == "BASELINE" and not nextcfloat == "":
+            cvis = struct.unpack("ff", nextcfloat)
+            vis[i][nchan[i]] = complex(cvis[0], cvis[1])
+            amp[i][nchan[i]] = math.sqrt(cvis[0]*cvis[0] + cvis[1]*cvis[1])
+            phase[i][nchan[i]] = math.atan2(cvis[1], cvis[0])
+            nchan[i] = nchan[i] + 1
+            nextcfloat = difxinputs[i].read(8)
+        lag[i] = fft.ifft(vis[i], nchan[i])
+        for j in range(nchan[i]/2):
+            lagamp[i][j+nchan[i]/2] = abs(lag[i][j])
+        for j in range(nchan[i]/2):
+            lagamp[i][j] = abs(lag[i][j+nchan[i]/2])
+        if i > 0:
+           if baseline[i] != baseline[0] or \
+              mjd[i] != mjd[0] or seconds[i] != seconds[0] or \
+              configindex[i] != configindex[0] or \
+              sourceindex[i] != sourceindex[0] or \
+              freqindex[i] != freqindex[0] or \
+              polpair[i] != polpair[0] or psrbin[i] != psrbin[0] or \
+              flag[i] != flag[0] or weight[i] != weight[0] or \
+              u[i] != u[0] or v[i] != v[0] or w[i] != w[0] or \
+              nchan[i] != nchan[0]:
+               print "Warning - file " + str(i) + " does not match file 0!"
+               print "baselines are " + str(baseline[i]) + '/' + str(baseline[0])
+               print "Time is " + str(mjd[i]) + ":" + str(seconds[i]) + " / " + \
+                     str(mjd[0]) + ":" + str(seconds[0])
+               print "uvw is " + str(u[i]) + "," + str(v[i]) + "," + str(w[i]) + \
+                     " / " + str(u[0]) + "," + str(v[0]) + "," + str(w[0])
+        pylab.subplot(311)
+        pylab.plot(chans[:nchan[i]], amp[i][:nchan[i]], linestyles[i])
+        pylab.subplot(312)
+        pylab.plot(chans[:nchan[i]], phase[i][:nchan[i]], linestyles[i])
+        pylab.subplot(313)
+        pylab.plot(chans[:nchan[i]], lagamp[i][:nchan[i]], linestyles[i])
     pylab.subplot(311)
-    titlestr = "Baseline " + str(baseline) + ", Freq " + str(freqindex) + ", pol " + polpair + ", MJD " + str(mjd+seconds/86400.0)
+    titlestr = "Baseline " + str(baseline[0]) + ", Freq " + str(freqindex[0]) + ", pol " + polpair[0] + ", MJD " + str(mjd[0]+seconds[0]/86400.0)
     pylab.title(titlestr)
     pylab.ylabel("Amplitude")
-    pylab.plot(chans[:nchan], amp)
     pylab.subplot(312)
     pylab.ylabel("Phase")
-    pylab.plot(chans[:nchan], phase)
     pylab.subplot(313)
     pylab.ylabel("Lag")
     pylab.xlabel("Channel")
-    pylab.plot(chans[:nchan], lagamp)
     pylab.show()
-    line = difxin.readline()
+    for i in range(numfiles):
+        lines[i] = difxinputs[i].readline()
     
