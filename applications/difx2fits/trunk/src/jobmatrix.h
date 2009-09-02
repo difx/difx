42955a6b175dc27101d78dc15d11ff7cd2afa1a7
walterfb <wbrisken@gmail.com>
2009-09-02 15:33:21 +0000

    Clean-up of difx2fits-trunk before 1.5.1
    git-svn-id: https://svn.atnf.csiro.au/difx@1438 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- applications/difx2fits/trunk/src/jobmatrix.h
+++ applications/difx2fits/trunk/src/jobmatrix.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2008, 2009 by Walter Brisken                            *
+ *   Copyright (C) 2009 by Walter Brisken                                  *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -19,11 +19,11 @@
 /*===========================================================================
  * SVN properties (DO NOT CHANGE)
  *
- * $Id:$
- * $HeadURL:$
- * $LastChangedRevision:$
- * $Author:$
- * $LastChangedDate:$
+ * $Id$
+ * $HeadURL$
+ * $LastChangedRevision$
+ * $Author$
+ * $LastChangedDate$
  *
  *==========================================================================*/
 
