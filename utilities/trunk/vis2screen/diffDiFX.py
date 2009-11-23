d96af79bfa55d7563522ed7d8f5c58a759d056b7
walterfb <wbrisken@gmail.com>
2009-11-23 18:43:27 +0000

    Expanded functionality and usability of diffDiFX.py
    git-svn-id: https://svn.atnf.csiro.au/difx@1664 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- utilities/trunk/vis2screen/diffDiFX.py
+++ utilities/trunk/vis2screen/diffDiFX.py
@@ -1,53 +1,57 @@
 #!/usr/bin/python
-import sys, os, struct, time, pylab, math
+import sys, os, struct, time, math
 from optparse import OptionParser
-from numpy import fft
 
-parser = OptionParser()
+helpstr = "diffDiFX [options] <difx file 1> <difx file 2>\n\n"
+helpstr += "prints an error message if mean difference ever exceeds THRESHOLD, "
+helpstr += "or every PRINTINTERVAL records if PRINTINTERVAL>0"
+parser = OptionParser(helpstr)
 parser.add_option("-f", "--freq", dest="freq", metavar="FREQ", default="-1",
-                  help="Only look at visibilities from this frequency index")
+                  help="Only look at visibilities from this FREQ index")
 parser.add_option("-b", "--baseline", dest="baseline", metavar="BASELINE", default="-1",
-                  help="Only look at visibilities from this baseline num")
+                  help="Only look at visibilities from this BASELINE num")
 parser.add_option("-t", "--threshold", dest="threshold", metavar="THRESHOLD", default="0.0005",
-                  help="Only display difference if greater than THRESHOLD")
+                  help="Display any difference that exceeds THRESHOLD")
+parser.add_option("-s", "--skiprecords", dest="skiprecords", metavar="SKIPRECORDS",
+                  default="0", help="Skip SKIPRECORDS records before starting comparison")
+parser.add_option("-m", "--maxrecords", dest="maxrecords", metavar="MAXRECORDS",
+                  default="-1", help="Stop after comparing MAXRECORDS (if >0) records")
+parser.add_option("-p", "--printinterval", dest="printinterval", metavar="PRINTINTERVAL",
+                  default="1000", help="Print a summary every PRINTINTERVAL records")
+parser.add_option("-c", "--maxchannels", dest="maxchannels", metavar="MAXCHANNELS",
+		  default="16384", 
+		  help="The length of the array that will be allocated to hold vis results")
+parser.add_option("-v", "--verbose", dest="verbose", action="store_true", default=False,
+                  help="Turn verbose printing on")
 (options, args) = parser.parse_args()
 
 if len(args) != 2:
-    print "Usage: diffDiFX [options] <difx file 1> <difx file 2>\n\nShows percentage difference in abs values between two files"
-    sys.exit()
+    parser.error("You must supply two (and only two) difx files to diff!")
 
 numfiles = len(args)
-MAX_CHANNELS = 16384
 
-BASELINE = int(options.baseline)
-FREQ = int(options.freq)
-threshold = float(options.threshold)
+targetbaseline = int(options.baseline)
+targetfreq     = int(options.freq)
+threshold      = float(options.threshold)
+skiprecords    = int(options.skiprecords)
+maxrecords     = int(options.maxrecords)
+printinterval  = int(options.printinterval)
+maxchannels    = int(options.maxchannels)
+verbose        = options.verbose
+
+if skiprecords < 0:
+   parser.error("You can't skip a negative number of records!!")
 
 chans = []
-amp = []
-phase = []
 vis = []
-lag = []
-lagamp = []
 for i in range(numfiles):
-    amp.append([])
-    phase.append([])
     vis.append([])
-    lag.append([])
-    lagamp.append([])
     
-for i in range(MAX_CHANNELS):
+for i in range(maxchannels):
     chans.append(i)
     for j in range(numfiles):
-        amp[j].append(0.0)
-        phase[j].append(0.0)
         vis[j].append(0.0)
-        lag[j].append(0.0)
-        lagamp[j].append(0.0)
     
-
-pylab.xlabel("Channel")
-pylab.ylabel("Amplitude")
 difxinputs  = []
 lines       = []
 baseline    = []
@@ -86,7 +90,15 @@ for filename in args:
 
 for i in range(numfiles):
     lines[i] = difxinputs[i].readline()
-while not lines[0] == "":
+recordcount = 0
+longtermabsdiff = 0.0
+longtermmeandiff = 0.0
+while not lines[0] == "" and not lines[1] == "":
+    if maxrecords > 0:
+        readrecords = recordcount - skiprecords
+	if readrecords >= maxrecords:
+	    print "Have examined " + str(readrecords) + " - finishing up!"
+	    break
     for i in range(numfiles):
         baseline[i] = int((lines[i].split(':')[1]).strip())
         lines[i] = difxinputs[i].readline()
@@ -115,48 +127,61 @@ while not lines[0] == "":
         w[i] = float((lines[i].split(':')[1]).strip())
         nchan[i] = 0
         nextcfloat = difxinputs[i].read(8)
-        while not nextcfloat == "BASELINE" and not nextcfloat == "":
+        while not nextcfloat == "BASELINE" and \
+		not nextcfloat == "" and not nchan[i] >= maxchannels:
             cvis = struct.unpack("ff", nextcfloat)
             vis[i][nchan[i]] = complex(cvis[0], cvis[1])
-            #amp[i][nchan[i]] = math.sqrt(cvis[0]*cvis[0] + cvis[1]*cvis[1])
-            #phase[i][nchan[i]] = math.atan2(cvis[1], cvis[0])
             nchan[i] = nchan[i] + 1
             nextcfloat = difxinputs[i].read(8)
-	if (BASELINE < 0 or BASELINE == baseline[i]) and \
-	    (FREQ < 0 or FREQ == freqindex[i]):
-            if i == 1:
-                if baseline[i] != baseline[0] or \
-                   mjd[i] != mjd[0] or seconds[i] != seconds[0] or \
-                   configindex[i] != configindex[0] or \
-                   sourceindex[i] != sourceindex[0] or \
-                   freqindex[i] != freqindex[0] or \
-                   polpair[i] != polpair[0] or psrbin[i] != psrbin[0] or \
-                   flag[i] != flag[0] or weight[i] != weight[0] or \
-                   u[i] != u[0] or v[i] != v[0] or w[i] != w[0] or \
-                   nchan[i] != nchan[0]:
-                    print "Warning - file " + str(i) + " does not match file 0!"
-                    print "baselines are " + str(baseline[i]) + '/' + str(baseline[0])
-                    print "Time is " + str(mjd[i]) + ":" + str(seconds[i]) + " / " + \
-                          str(mjd[0]) + ":" + str(seconds[0])
-                    print "uvw is " + str(u[i]) + "," + str(v[i]) + "," + str(w[i]) + \
-                          " / " + str(u[0]) + "," + str(v[0]) + "," + str(w[0])
-                    print "polpair is " + polpair[i] + "/" + polpair[0]
-                    print "weight is " + str(weight[i]) + "/" + str(weight[0])
-                    print "indices are " + str(configindex[i]) + "," + str(sourceindex[i]) + \
-                          " / " + str(configindex[0]) + "," + str(sourceindex[0])
-                    print "nchan is " + str(nchan[i]) + "/" + str(nchan[0])
-                    print "flag is " + str(flag[i]) + "/" + str(flag[0])
-                    print "psrbin is " + str(psrbin[i]) + "/" + str(psrbin[0])
-		diffavg = 0.0
-		refavg = 0.0
-                for j in range(nchan[0]):
-		    diff = vis[1][j] - vis[0][j]
-                    diffavg = diffavg + abs(diff)/nchan[0]
-                    refavg = refavg + abs(vis[0][j])/nchan[0]
-    if (BASELINE < 0 or baseline[0] == BASELINE) and \
-       (FREQ < 0 or freqindex[0] == FREQ):
-        if 100.0*diffavg/refavg > threshold:
-            print "The percentage difference on baseline %d, freq %d is %10.8f" % (baseline[0], freqindex[0], 100.0*diffavg/refavg)
+	if nchan[i] >= maxchannels:
+	    print "How embarrassing - you have tried to diff files with more than " + \
+	    	str(maxchannels) + " channels.  Please rerun with --maxchannels=<bigger number>!"
+	    sys.exit()
+    if (targetbaseline < 0 or targetbaseline == baseline[0]) and \
+        (targetfreq < 0 or targetfreq == freqindex[0]):
+        if baseline[1] != baseline[0] or \
+            mjd[1] != mjd[0] or seconds[1] != seconds[0] or \
+            configindex[1] != configindex[0] or \
+            sourceindex[1] != sourceindex[0] or \
+            freqindex[1] != freqindex[0] or \
+            polpair[1] != polpair[0] or psrbin[1] != psrbin[0] or \
+            flag[1] != flag[0] or weight[1] != weight[0] or \
+            u[1] != u[0] or v[1] != v[0] or w[1] != w[0] or \
+            nchan[1] != nchan[0]:
+            print "Warning - file 0 does not match file 1!"
+	    if verbose:
+                print "baselines are " + str(baseline[0]) + '/' + str(baseline[1])
+                print "Time is " + str(mjd[0]) + ":" + str(seconds[0]) + " / " + \
+                       str(mjd[1]) + ":" + str(seconds[1])
+                print "uvw is " + str(u[0]) + "," + str(v[0]) + "," + str(w[0]) + \
+                      " / " + str(u[1]) + "," + str(v[1]) + "," + str(w[1])
+                print "polpair is " + polpair[0] + "/" + polpair[1]
+                print "weight is " + str(weight[0]) + "/" + str(weight[1])
+                print "indices are " + str(configindex[0]) + "," + str(sourceindex[0]) + \
+                      " / " + str(configindex[1]) + "," + str(sourceindex[1])
+                print "nchan is " + str(nchan[0]) + "/" + str(nchan[1])
+                print "flag is " + str(flag[0]) + "/" + str(flag[1])
+                print "psrbin is " + str(psrbin[0]) + "/" + str(psrbin[1])
+        absdiffavg = 0.0
+        meandiffavg = 0.0
+        refavg = 0.0
+	if recordcount >= skiprecords:
+            for j in range(nchan[0]):
+                diff = vis[1][j] - vis[0][j]
+                absdiffavg  = absdiffavg + abs(diff)/nchan[0]
+	        meandiffavg = meandiffavg + diff/nchan[0]
+                refavg = refavg + abs(vis[0][j])/nchan[0]
+	    longtermabsdiff += absdiffavg/refavg
+	    longtermmeandiff += meandiffavg/refavg
+            if 100.0*absdiffavg/refavg > threshold:
+                print "THRESHOLD EXCEEDED! The percentage absolute difference on baseline %d, freq %d at MJD/sec %d/%7.2f is %10.8f, and the percentage mean difference is %10.8f + %10.8f i" % (baseline[0], freqindex[0], mjd[0], seconds[0], 100.0*absdiffavg/refavg, 100.0*meandiffavg.real/refavg, 100.0*meandiffavg.imag/refavg)
     for i in range(numfiles):
         lines[i] = difxinputs[i].readline()
-    
+    if (targetbaseline < 0 or targetbaseline == baseline[0]) and \
+            (targetfreq < 0 or targetfreq == freqindex[0]):
+        recordcount += 1
+    if recordcount > 0 and printinterval>0 and recordcount%printinterval == 0:
+        print "After %d records, the mean percentage absolute difference is %10.8f and the mean percentage mean difference is %10.8f + %10.8f i" % (recordcount-skiprecords, 100.0*longtermabsdiff/(recordcount-skiprecords), 100.0*longtermmeandiff.real/(recordcount-skiprecords), 100.0*longtermmeandiff.imag/(recordcount-skiprecords))
+
+if recordcount > 0:
+    print "After %d records, the mean percentage absolute difference is %10.8f and the mean percentage mean difference is %10.8f + %10.8f i" % (recordcount-skiprecords, 100.0*longtermabsdiff/  (recordcount-skiprecords), 100.0*longtermmeandiff.real/(recordcount-skiprecords), 100.0*             longtermmeandiff.imag/(recordcount-skiprecords))
