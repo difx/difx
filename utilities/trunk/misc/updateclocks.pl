3ed6033b92195675adba23df7d6b20b2aeac4df6
XhrisPhillips <Chris.Phillips@csiro.au>
2009-11-24 11:11:49 +0000

    Simple utility to update station clocks
    git-svn-id: https://svn.atnf.csiro.au/difx@1697 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

---
+++ utilities/trunk/misc/updateclocks.pl
@@ -0,0 +1,66 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+use constant PARSE => 0;
+use constant DELAY => 1;
+
+if (@ARGV<2) {
+  die "Usage: updateclocks.pl exper.input ANT:OFFSET [ANT2:OFFSET ...]\n";
+}
+
+my $inputfile = shift @ARGV;
+
+my %delays = ();
+foreach (@ARGV) {
+  if (/^(\S+):(\S+)$/) {
+    $delays{$1} = $2;
+  } else {
+    die "Bad argment $_\n";
+  }
+}
+
+print "\nUpdate clocks? [n/Y]: ";
+
+my $ans = uc <STDIN>;
+chomp($ans);
+exit(1) if ($ans ne "" && $ans ne "Y" && $ans ne "YES");
+
+my $ant = undef;
+open(INPUT, $inputfile) || die "Failed to open $inputfile: $!\n";
+
+my $outputfile = "$inputfile.tmp.$$";
+open(OUTPUT, '>', $outputfile) || die "Failed to open temp file $outputfile: $!\n";
+
+while (<INPUT>) {
+  if (/^TELESCOPE NAME \d+:\s+(\S+)/) {
+    if (exists $delays{$1}) {
+      $ant = $1;
+    } else {
+      $ant = undef;
+    }
+    print OUTPUT;
+  } elsif (/^(CLOCK DELAY \(us\) \d+):\s+(\S+)/) {
+    if (defined $ant) {
+      my $newdelay = $2 + $delays{$ant};
+      print OUTPUT "$1: $newdelay\n";
+      $ant = undef;
+    } else {
+      print OUTPUT;
+    }
+  } else {
+    $ant = undef;
+    print OUTPUT;
+  }
+}
+
+close(INPUT) || die "Failed to close $inputfile: %!\n";
+close(OUTPUT) || die "Failed to close temp file $outputfile: $!\n";
+
+
+if (-e "$inputfile~") {
+  unlink "$inputfile~" || die "Failed to delete backup file\n";
+}
+
+rename $inputfile, "$inputfile~" || die "Failed to backup $inputfile\n";
+rename $outputfile, $inputfile || die "Failed to rename $outputfile $inputfile\n";
