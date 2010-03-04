f87626ebbea3e55aa0d7886c28131861abdd43d4
walterfb <wbrisken@gmail.com>
2010-03-04 17:40:08 +0000

    Don't include autom4ate.cache in svn
    git-svn-id: https://svn.atnf.csiro.au/difx@2009 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- libraries/vdifio/trunk/src/vdifio.h
+++ libraries/vdifio/trunk/src/vdifio.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2007 by Walter Brisken                                  *
+ *   Copyright (C) 2007-2010 by Adam Deller / Walter Brisken               *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
