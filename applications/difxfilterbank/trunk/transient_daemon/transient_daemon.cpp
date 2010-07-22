8e4dd26e4d89ffcf353b02062b574aba651f820d
walterfb <wbrisken@gmail.com>
2010-07-22 14:46:21 +0000

    transient_daemon in a somewhat usable state
    git-svn-id: https://svn.atnf.csiro.au/difx@2350 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- applications/difxfilterbank/trunk/transient_daemon/transient_daemon.cpp
+++ applications/difxfilterbank/trunk/transient_daemon/transient_daemon.cpp
@@ -5,23 +5,42 @@
 #include <time.h>
 #include <unistd.h>
 #include <difxmessage.h>
+#include <difxio/parsedifx.h>
 
-const char program[] = "transientdaemon";
+const char program[] = "transient_daemon";
 const char author[]  = "Walter Brisken";
 const char version[] = "0.1";
 const char verdate[] = "2010 Jul 19";
 
-
 const char dispatcherProgram[] = "transient_dispatcher";
+/* Command line params are: configfile jobid dmfile */
 
+const char DMGeneratorProgram[] = "dmgen";
+/* Command line params are: freq(MHz) [ chanBW(MHz) [ intTime ] ] */
 
 int die = 0;
+
+const int CommandLength = 256;
+
+const char testMessage[] = 
+"<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><difxMessage><header><from>gui</from><to>boom</to><mpiProcessId>-1</mpiProcessId><identifier>boom_x</identifier><type>DifxStart</type></header><body><difxStart><input>/home/swc/difx/projects/dq024/y_04.input</input><manager node=\"boom\"/><datastream nodes=\"boom1 boom2\"/><process threads=\"7\" nodes=\"boom3 boom4 boom4\"/><force>1</force><difxVersion>DIFX-DEVEL</difxVersion></difxStart></body></difxMessage>";
 
 
 int usage(const char *cmd)
 {
 	printf("\n");
 	printf("%s ver. %s   %s  %s\n\n", program, version, author, verdate);
+	printf("Usage:  %s  [options]\n\n", cmd);
+	printf("Options can include:\n\n");
+	printf("  --verbose\n");
+	printf("  -v           Be more verbose in execution\n\n");
+	printf("  --quiet\n");
+	printf("  -q           Be quieter\n\n");
+	printf("  --test\n");
+	printf("  -t           Perform self test and quit\n\n");
+	printf("  --help\n");
+	printf("  -h           Print this useful help information.\n\n");
+	printf("To quite: Ctrl-c or send a sigint\n\n");
 
 	return 0;
 }
@@ -29,6 +48,84 @@ int usage(const char *cmd)
 void siginthand(int j)
 {
 	die = 1;
+}
+
+/* Taken straight from mpifxcorr.cpp */
+static void generateIdentifier(const char *inputfile, int myID, char *identifier)
+{
+  int i, l, s=0;
+
+  for(i = 0; inputfile[i]; i++)
+  {
+    if(inputfile[i] == '/')
+    {
+      s = i+1;
+    }
+  }
+
+  if(inputfile[s] == 0)
+  {
+    s = 0;
+  }
+
+  strcpy(identifier, inputfile+s);
+  l = strlen(identifier);
+  
+  // strip off ".input"
+  for(i = l-1; i > 0; i--)
+  {
+    if(identifier[i] == '.')
+    {
+      identifier[i] = 0;
+      break;
+    }
+  }
+}
+
+int getDMGenCommand(const char *inputFile, char *command)
+{
+	int r, v, nFreq, i;
+	DifxParameters *dp;
+	double freq, minFreq = 1.0e9;	/* MHz */
+
+	dp = newDifxParametersfromfile(inputFile);
+	if(!dp)
+	{
+		fprintf(stderr, "Cannot open %s for read\n", inputFile);
+		return -1;
+	}
+
+	r = DifxParametersfind(dp, 0, "FREQ ENTRIES");
+	if(r < 0)
+	{
+		fprintf(stderr, "Cannot find FREQ ENTRIES in %s\n", inputFile);
+		return -2;
+	}
+	nFreq = atoi(DifxParametersvalue(dp, r));
+	for(i = 0; i < nFreq; i++)
+	{
+		r = DifxParametersfind1(dp, r, "FREQ (MHZ) %d", i);
+		if(r < 0)
+		{
+			fprintf(stderr, "Cannot find FREQ (MHZ) %d in %s\n", i, inputFile);
+			return -3;
+		}
+		freq = atof(DifxParametersvalue(dp, r));
+		if(freq < minFreq)
+		{
+			minFreq = freq;
+		}
+	}
+
+	v = snprintf(command, CommandLength, "`%s %f`", DMGeneratorProgram, minFreq);
+	if(v >= CommandLength)
+	{
+		fprintf(stderr, "Error: CommandLength=%d is too small.  Needs to be > %d\n",
+			CommandLength, v);
+		return -3;
+	}
+
+	return 0;
 }
 
 int runCommand(const char *command, int verbose)
@@ -61,12 +158,13 @@ int runCommand(const char *command, int verbose)
 
 static int handleMessage(const char *message, int verbose)
 {
-	const int CommandLength = 256;
 //	const int TimeLength = 32;
 	DifxMessageGeneric G;
 	char command[CommandLength];
 	int v, pid;
 //	char timestr[TimeLength];
+	char identifier[32];
+	char dmGenCmd[CommandLength];
 
 	difxMessageParse(&G, message);
 
@@ -83,11 +181,21 @@ static int handleMessage(const char *message, int verbose)
 		{
 			printf("%s: START %s %s\n", G.identifier, G.body.start.inputFilename, G.body.start.difxVersion);
 		}
-		v = snprintf(command, CommandLength, "%s.%s %s\n", dispatcherProgram, G.   body.start.difxVersion, G.body.start.inputFilename);
+
+		generateIdentifier(G.body.start.inputFilename, 0, identifier);
+		v = getDMGenCommand(G.body.start.inputFilename, dmGenCmd);
+		if(v != 0)
+		{
+			fprintf(stderr, "Error %d generating the DM command\n", v);
+			return -1;
+		}
+
+		v = snprintf(command, CommandLength, "%s.%s %s %s %s\n", dispatcherProgram, G.body.start.difxVersion, G.body.start.inputFilename, identifier, dmGenCmd);
 		if(v >= CommandLength)
 		{
 			fprintf(stderr, "Error: CommandLength=%d is too small (needs to be > %d)\n",
 				CommandLength, v);
+			return -2;
 		}
 		else
 		{
@@ -102,16 +210,16 @@ static int handleMessage(const char *message, int verbose)
 		break;
 	}
 
-	return G.type;
+	return 0;
 }
 
-int transientdaemon(int verbose)
+int transientdaemon(int verbose, int selfTest)
 {
 	char message[DIFX_MESSAGE_LENGTH];
 	char from[DIFX_MESSAGE_PARAM_LENGTH];
 	typedef void (*sighandler_t)(int);
 	sighandler_t oldsiginthand;
-	int l, sock;
+	int l, v, sock;
 
 	oldsiginthand = signal(SIGINT, siginthand);
 
@@ -145,7 +253,16 @@ int transientdaemon(int verbose)
 			break;
 		}
 
-		handleMessage(message, verbose);
+		if(selfTest)
+		{
+			die = 1;
+			strcpy(message, testMessage);
+		}
+		v = handleMessage(message, verbose);
+		if(v)
+		{
+			fprintf(stderr, "Error=%d handling command %s\n", v, message);
+		}
 	}
 
 	snprintf(message, DIFX_MESSAGE_LENGTH, "Transient Daemon stopping");
@@ -157,6 +274,7 @@ int transientdaemon(int verbose)
 int main(int argc, char **argv)
 {
 	int verbose = 0;
+	int selfTest = 0;
 	int a;
 
 	for(a = 1; a < argc; a++)
@@ -168,10 +286,20 @@ int main(int argc, char **argv)
 			{
 				verbose++;
 			}
+			else if(strcmp(argv[a], "--quiet") == 0 ||
+				strcmp(argv[a], "-q") == 0)
+			{
+				verbose--;
+			}
 			else if(strcmp(argv[a], "--help") == 0 ||
 				strcmp(argv[a], "-h") == 0)
 			{
 				return usage(argv[0]);
+			}
+			else if(strcmp(argv[a], "--test") == 0 ||
+				strcmp(argv[a], "-t") == 0)
+			{
+				selfTest = 1;
 			}
 			else
 			{
@@ -183,7 +311,7 @@ int main(int argc, char **argv)
 		}
 	}
 
-	transientdaemon(verbose);
+	transientdaemon(verbose, selfTest);
 
 	return 0;
 }
