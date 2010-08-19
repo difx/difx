10962321956988c010acf0b1156ad85b2569c798
adamdeller <adeller@swin.edu.au>
2010-08-19 22:37:23 +0000

    Fix for uninitialised weight array with variable number of IFs
    git-svn-id: https://svn.atnf.csiro.au/difx@2409 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- applications/difx2fits/trunk/src/fitsUV.c
+++ applications/difx2fits/trunk/src/fitsUV.c
@@ -1000,6 +1000,7 @@ static int storevis(DifxVis *dv)
 static int readvisrecord(DifxVis *dv, int verbose, int pulsarBin, int phasecentre)
 {
 	/* blank array */
+	memset(dv->weight, 0, dv->nFreq*dv->D->nPolar*sizeof(float));
 	memset(dv->data, 0, dv->nData*sizeof(float));
 
 	dv->changed = 0;
