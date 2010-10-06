0c4751aadfbff7ebffb35a998b6023d05ced850b
walterfb <wbrisken@gmail.com>
2010-10-06 21:09:17 +0000

    ATD: small printdifx improvements
    git-svn-id: https://svn.atnf.csiro.au/difx@2707 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- utilities/trunk/vis2screen/printDiFX.cpp
+++ utilities/trunk/vis2screen/printDiFX.cpp
@@ -19,8 +19,9 @@ int main(int argc, char** argv)
   string line;
   char pol[3];
   pol[2] = 0;
+  bool badvisibility, goodsync;
   double uvw[3];
-  int baseline, mjd, confindex, srcindex, freqindex, pbin, flag, numchannels;
+  int baseline, mjd, confindex, srcindex, freqindex, pbin, numchannels;
   double weight,sec;
   string filename = argv[1];
   int sepindex = filename.find_last_of('.');
@@ -35,11 +36,32 @@ int main(int argc, char** argv)
 
   cout << "About to start reading" << endl;
   while (!(difxin.eof())) {
-    config->fillHeaderData(&difxin, baseline, mjd, sec, confindex, srcindex, freqindex, pol, pbin, weight, uvw);
+    goodsync = config->fillHeaderData(&difxin, baseline, mjd, sec, confindex, srcindex, freqindex, pol, pbin, weight, uvw);
+    if(!goodsync) {
+      cout << "BAD SYNC! trying again" << endl;
+      continue;
+    }
 
     numchannels = config->getFNumChannels(freqindex)/config->getFChannelsToAverage(freqindex);
     difxin.read((char*)visibilities, numchannels*2*sizeof(float));
+    badvisibility = false;
+    for(int i=0;i<numchannels*2;i++) {
+      if(visibilities[i] > 1000.0 || visibilities[i] < -1000 ||
+         isinf(visibilities[i]) || isnan(visibilities[i]))
+        badvisibility = true;
+    }
 
-    cout << "For baseline " << baseline << ", at time " << mjd << "/" << sec << ", the source was " << srcindex << " and the config was " << confindex << ", with uvw (" << uvw[0] << "," << uvw[1] << "," << uvw[2] << ").  For freq " << freqindex << ", pol " << pol << ", pulsar bin " << pbin << ", there are " << numchannels << " channels; visibilities[0] = " << visibilities[0] << " + " << visibilities[1] << " i.  The middle visibility is " << visibilities[numchannels] << " + " << visibilities[numchannels + 1] << " i." << ", and the weight was " << weight << endl;
+    if(badvisibility)
+    {
+      cout << "NAUGHTY visibility! The full header was baseline " << baseline << ", at time " << mjd << "/" << sec << ", the source was " << srcindex << " and the config was " << confindex << ", with uvw (" << uvw[0] << "," << uvw[1] << "," << uvw[2] << "). For freq " << freqindex << ", pol " << pol << ", pulsar bin " << pbin << ", weight " << weight << ", we have:" << endl;
+      cout << "NAUGHTY visibilities are: ";
+      for(int i=0;i<numchannels;i++)
+        cout << visibilities[2*i] << " + " << visibilities[2*i+1] << " i; ";
+      cout << endl;
+    }
+    else;
+      cout << "For baseline " << baseline << ", at time " << mjd << "/" << sec << ", the source was " << srcindex << " and the config was " << confindex << ", with uvw (" << uvw[0] << "," << uvw[1] << "," << uvw[2] << ").  For freq " << freqindex << ", pol " << pol << ", pulsar bin " << pbin << ", there are " << numchannels << " channels; visibilities[0] = " << visibilities[0] << " + " << visibilities[1] << " i.  The middle visibility is " << visibilities[numchannels] << " + " << visibilities[numchannels + 1] << " i." << ", and the weight was " << weight << endl;
   }
+
+  return 0;
 }
