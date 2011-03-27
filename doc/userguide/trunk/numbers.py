3e0101e5ddcebb7871aa04981c1a34328a5fdb8e
walterfb <wbrisken@gmail.com>
2011-03-27 19:31:33 +0000

    Some slight more doco
    git-svn-id: https://svn.atnf.csiro.au/difx@3170 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- doc/userguide/trunk/numbers.py
+++ doc/userguide/trunk/numbers.py
@@ -5,7 +5,10 @@ from pyx import *
 def makenumber(number, filename):
 	c = canvas.canvas()
 	circ = path.circle(0, 0, 1)
-	c.fill(circ, [trafo.scale(sx=0.4, sy=0.2)])
+	if len(number) > 3:
+		c.fill(circ, [trafo.scale(sx=0.6, sy=0.2)])
+	else:
+		c.fill(circ, [trafo.scale(sx=0.4, sy=0.2)])
 	c.text(0, 0, number, [text.valign.middle, text.halign.center, color.rgb.white])
 	c.writePDFfile(filename)
 
@@ -20,4 +23,5 @@ makenumber("1.1", "11.pdf")
 makenumber("1.2", "12.pdf")
 makenumber("1.5", "15.pdf")
 makenumber("2.0", "20.pdf")
+makenumber("2.0.1", "201.pdf")
 makeblank('blank.pdf')
