bc30682994d8a63db11e7438569eef234e38c7f7
walterfb <wbrisken@gmail.com>
2011-06-20 13:50:31 +0000

    Change default ephemeris interval to match default calcif2 position sampling interval
    git-svn-id: https://svn.atnf.csiro.au/difx@3467 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- applications/vex2difx/trunk/src/corrparams.cpp
+++ applications/vex2difx/trunk/src/corrparams.cpp
@@ -645,7 +645,7 @@ void PhaseCentre::initialise(double r, double d, string name)
 	dec = d;
 	difxName = name;
 	calCode = ' ';
-	ephemDeltaT = 15.0; //seconds
+	ephemDeltaT = 20.0; //seconds; 20 seconds is perfectly matched to the default behavior of calcif2
 	qualifier = 0;
 	string ephemObject = "";
 	string ephemFile = "";
