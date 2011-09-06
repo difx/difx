eafc82cc546aa274a7a9e3a4ecea460a1c7ace31
jwagnerhki <jan.wagner@iki.fi>
2011-09-06 16:50:21 +0000

    plot UV images of all channels stacked together
    git-svn-id: https://svn.atnf.csiro.au/difx@3772 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- libraries/beamformer/trunk/matlab/subspcrfi_illum.m
+++ libraries/beamformer/trunk/matlab/subspcrfi_illum.m
@@ -2,6 +2,7 @@
 % Apertif ch#35 = rfi, ch#17 = likely free of rfi
 ch=31;
 
+fnr=1;
 orig_spec=zeros(Nch,Nant);
 null_spec=zeros(Nch,Nant);
 for cc=1:Nch,
@@ -26,16 +27,38 @@ for cc=1:Nch,
     end
 end
 
-figure(1),subplot(1,2,1),surf(orig_spec),view([90 90]),title('Orignal data Ron-Roff'),
+%% imaging of null(Ron)-null(Roff)
+if 1,
+    cc=20;
+    R_orig = squeeze(alldata(1,:,:,cc)) - squeeze(alldata(2,:,:,cc));
+    R_nulled = squeeze(nulldata(1,:,:,cc)) - squeeze(nulldata(2,:,:,cc));
+    [uvd_orig] = subspcrfi_RtoUV( R_orig, frequencies(cc), elem_positions );
+    [uvd_nulled] = subspcrfi_RtoUV( R_nulled, frequencies(cc), elem_positions );
+    img_orig = abs(fft2(uvd_orig));
+    img_nulled = abs(fft2(uvd_nulled));
+    figure(fnr),fnr=fnr+1;
+    subplot(2,1,1),surf(img_orig), title(['UV image of original Ron-Roff data in channel ' num2str(cc)]), 
+        view([90 90]), axis equal, axis tight;
+    subplot(2,1,2),surf(img_nulled), title(['UV image of null(Ron)-null(Roff) data in channel ' num2str(cc)]),
+        view([90 90]), axis equal, axis tight;
+end
+
+%% plot diffs
+figure(fnr),fnr=fnr+1;
+subplot(1,2,1),surf(orig_spec),view([90 90]),title('Orignal data Ron-Roff'),
 subplot(1,2,2),surf(null_spec),view([90 90]),title('Nulled Ron - nulled Roff');
 
 % note: max power in (Ron-Roff) is in element 31
-pel=31;figure(2),subplot(1,2,1),plot(orig_spec(:,pel)),
-title(['Element ' int2str(pel) ' original']), axx=axis();
+pel=31;
+figure(fnr),fnr=fnr+1;
+subplot(1,2,1),plot(orig_spec(:,pel)),
+title(['Element ' int2str(pel) ' original']), axx=axis(),
+hold on,
+plot(null_spec(:,pel), '-');
 subplot(1,2,2),plot(null_spec(:,pel)), axis(axx),
 title(['Element ' int2str(pel) ' nulled']);
 
-figure(3),
+figure(fnr),fnr=fnr+1;
 subplot(2,1,1),plot(mean(orig_spec,2)),title('Mean over elements, original'),
 subplot(2,1,2),plot(mean(null_spec,2)),title('Mean over elements, nulled data');
 
@@ -57,13 +80,13 @@ end
 
 %% plot raw autocorrelations
 if 0,
-    figure(1)
+    figure(fnr),fnr=fnr+1;
     subplot(2,2,1),plot(abs(diag(Ron))),title('Ron AC orig');
     subplot(2,2,2),plot(abs(diag(Roff))),title('Roff AC orig');
     subplot(2,2,3),plot(abs(diag(RonN))),title('Ron AC nulled');
     subplot(2,2,4),plot(abs(diag(RoffN))),title('Roff AC nulled');
 
-    figure(2),
+    figure(fnr),fnr=fnr+1;
     subplot(2,1,1),plot(deltaAC),
         title(['Original AC data abs(Ron-Roff) on Ch' int2str(ch)]);
     subplot(2,1,2),plot(abs(deltaACN)),
