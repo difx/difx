3dfc513a2ccd26ca268516b1da307bc33f7a2bed
jwagnerhki <jan.wagner@iki.fi>
2011-09-06 07:17:34 +0000

    nulling uses min of user and max 3sig or mdl, numerically improved the MDL calc
    git-svn-id: https://svn.atnf.csiro.au/difx@3759 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- libraries/beamformer/trunk/matlab/subspcrfi_MDLrank.m
+++ libraries/beamformer/trunk/matlab/subspcrfi_MDLrank.m
@@ -2,7 +2,7 @@
 % that are above an unknown noise power threshold.
 % This can work reasonably but requires more than N/2 of
 % eigenvalues are indeed from noise space.
-
+%
 % From http://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=1164557
 % evtest=[21.2359 2.1717 1.4279 1.0979 1.0544 0.9432 0.7324];
 % subspcrfi_MDLrank(evtest, 100);
@@ -10,17 +10,15 @@
 %  AIC=1e3*[1.1808 0.1005 0.0714 0.0755 0.0868 0.0932 0.0960], rank=2+1
 %  MDL=[590.4 67.2 66.9 80.7 95.5 105.2 110.5], rank=2+1
 
-function [det_Nrfi,sorted_evalues]=subspcrfi_MDLrank(evalues, M_samps)
+function [det_Nrfi,sorted_evalues,MDL_k]=subspcrfi_MDLrank(evalues, M_samps)
     v_verbose = 0;
+    Nsig = max(size(evalues));
 
-    M = 1024; % how many samples were averaged into the Rxx estimate
+    M = 3*Nsig; % how many samples were averaged into the Rxx estimate
     if (nargin==2),
         M = M_samps;
     end
     
-    Nsig = max(size(evalues));
-    det_Nrfi = Nsig;
-
     % q interferers in p total signals, q<p
     % lambda{1..k}
     % must be sorted so that lambda(1)>lambda(2)>...>lambda(p)
@@ -28,26 +26,25 @@ function [det_Nrfi,sorted_evalues]=subspcrfi_MDLrank(evalues, M_samps)
     if (size(evalues,2)>size(evalues,1)),
         evalues = transpose(evalues);
     end
-    if (evalues(1)<evalues(2)),
-        fprintf(1, 'Warning: re-sorting eigenvalues to be in descending order\n');
-    end
-    evalues = sort(evalues,1,'descend');
-    sorted_evalues = evalues;
+    sorted_evalues = sort(evalues,1,'descend');
     
-    min_MDL = Inf;
-    IClist = zeros(1, Nsig);
+    MDL_k = zeros(Nsig, 1);
     for kk=0:(Nsig-1),
         Q = Nsig - kk;
-        arith = sum(evalues((kk+1):end)) / Q;
-        geo = prod(evalues((kk+1):end)) ^ (1/Q); % this overflows to NaN very easily!
-        mdl = -Q*M*log(geo/arith) + 0.5*kk*(2*Nsig - kk)*log(M);
-        IClist(kk+1) = mdl;
-        if (mdl < min_MDL),
-            min_MDL = mdl; det_Nrfi = kk + 1;
-        end
-        if (isnan(mdl) && v_verbose),
+        data = sorted_evalues((kk+1):end);
+        arith = sum( data ./ Q);
+        geo = prod( data .^ (1/Q));
+        
+        L = geo/arith; % may reach NaN/Inf quickly!
+        
+        MDL_k(kk+1) = -Q*M*log(L) + 0.5*kk*(2*Nsig - kk)*log(M);
+        
+        if (isnan(MDL_k(kk+1)) && v_verbose),
             fprintf(1, 'Warning: MDL(Nrfi=%d) had geometric mean %f, MDL overflow!\n', nn, geo);
         end
     end
-    % IClist
+    
+    [minMDL,minIdx] = min(MDL_k);
+    det_Nrfi = minIdx - 1;
+    
     
--- libraries/beamformer/trunk/matlab/subspcrfi_nulling.m
+++ libraries/beamformer/trunk/matlab/subspcrfi_nulling.m
@@ -5,77 +5,132 @@
 % For any eigenvector matrices that are singular i.e. not invertible,
 % the output matrix will be a direct copy from the alldata input matrix.
 %
+% If some array elements are not connected or give no signal or their
+% data should be ignored for other reasons, use the ignore_mask parameter.
+% To use all elements use a blank ignore_mask=[];
+%
 % Input: 
 %   alldata = 2 x Nant x Nant x Nch       fall-back data
 %   rfi_evalues = 2 x Nant x Nch          eigenvalues
 %   rfi_evectors = 2 x Nant x Nant x Nch  eigenvectors
+%   Mint = scalar                         nr of time-integrated covariances
 %   Nrfi = scalar                         max count of interferers to null
+%   ignore_mask = 1 x Nignore             list of eigenval indices to ignore
 %
-function [nulldata]=subspcrfi_nulling(alldata, rfi_evalues, rfi_evectors, Nrfi)
+function [nulldata]=subspcrfi_nulling(alldata, rfi_evalues, rfi_evectors, Nrfi, Mint, ignore_mask)
   Nsets = size(alldata, 1);
   Nant = size(alldata, 2);
   Nch = size(alldata, 4);
+  Nrfi_maxclip = Nrfi;
   
   v_verbose = 0;
   nulldata = zeros(size(alldata));
   
   for pp=1:Nsets,
     for cc=1:Nch,
-        null_evalues = squeeze(rfi_evalues(pp,:,cc));
-        null_evectors = squeeze(rfi_evectors(pp,:,:,cc));
+        evalues = squeeze(rfi_evalues(pp,:,cc));
+        evectors = squeeze(rfi_evectors(pp,:,:,cc));
 
-        if (cond(null_evectors) == Inf),
+        if (cond(evectors) == Inf),
             display('Warning: eigenvector matrix is singular! No correction will be done.');
             nulldata(pp,:,:,cc) = alldata(pp,:,:,cc);
             continue;
         end
 
-        % find at most 'Nrfi' dominant eigenvalues that could be nulled
-        [det_Nrfi,sorted_evalues] = subspcrfi_MDLrank(null_evalues, 1024);
-        fprintf(1, 'Set %d Ch %d MDL=%d\n', pp, cc, det_Nrfi);        
+        % Note: don't sort original eigenvalues here, otherwise we loose
+        % the eigenvalue <=> eigenvector correspondency (would need
+        % to re-order SVD or EVD eigenvector matrices accordingly!)
+        
+        % Use MDL to estimate 'Nrfi' dominant eigenvalues that could
+        % be nulled, measured using only the non-ignored eigenvalues
+        mdl_evalues = sort(evalues,2,'descend'); % these not used for nulling
+        mdl_evalues(ignore_mask) = [];
+        [Nrfi_mdl] = subspcrfi_MDLrank(mdl_evalues, Mint);
+        
+        % Use thresholding: exact, estimate, or remove all dominant
+        %rfi_thresh = sorted_evalues(min(Nrfi, det_Nrfi)); % exact/clipped
+        rfi_thresh = mean(mdl_evalues) + 3*std(mdl_evalues);
+        %rfi_thresh = median(abs(mdl_evalues)) + 3*std(mdl_evalues);
+        %rfi_thresh = 0; %always remove dominant
+        
+        % Number of RFI estimated from thresholding
+        Nrfi_thresh = sum(mdl_evalues >= rfi_thresh);
+        
+        % Choose maximum of detected RFI values, clip to user-specified limit
+        Nrfi_det = max(Nrfi_mdl, Nrfi_thresh);
+        Nrfi = min(Nrfi_det, Nrfi_maxclip);
+        % fprintf(1, 'Channel %d 3sig=%d mdl=%d fin=%d\n', cc, Nrfi_thresh, Nrfi_mdl, Nrfi);
 
         % no RFI?
         %if (det_Nrfi>Nant/2),
         %    nulldata(pp,:,:,cc) = alldata(pp,:,:,cc);
         %    continue;
         %end
-        
-        % threshold: exact, estimate, or remove all dominant
-        %rfi_thresh = sorted_evalues(min(Nrfi, det_Nrfi)); % exact/clipped
-        rfi_thresh = mean(abs(null_evalues)) + 3*mean(std(null_evalues));
-        %rfi_thresh = 0; %always remove dominant
 
+        % Prepare for nulling
         rfi_cnt = 0;
         null_idcs = zeros(1,Nrfi);
-        % loop through eigenvalues in descending order
+        null_evalues = evalues;
+        
+        % Loop through eigenvalues in one "find max()" out-of-Nrfi at a time.
+        % The loop is used because eigenvalues need not be ordered.
         idx_dir = 1:Nrfi;
-        if (null_evalues(1)<null_evalues(end)),
+        if (null_evalues(1) < null_evalues(end)),
             idx_dir = Nrfi:-1:1;
         end
         for rii=idx_dir,
             [v,vi] = max(abs(null_evalues));
-            if (v >= rfi_thresh),
-                rfi_cnt = rfi_cnt + 1;
-                null_idcs(rfi_cnt) = vi;
-                null_evalues(vi) = min(null_evalues); % will be overwritten later by mean
-                if (v_verbose),
-                    fprintf(1,'Eig %d Ch#%d = %f > thr=%f\n', rfi_cnt, cc, v, rfi_thresh);
-                end;
-            end
+            rfi_cnt = rfi_cnt + 1;
+            null_idcs(rfi_cnt) = vi;
+            null_evalues(vi) = 0;
+            if (v_verbose),
+                fprintf(1,'Eig %d Ch#%d = %f > thr=%f\n', rfi_cnt, cc, v, rfi_thresh);
+            end;
         end
 
         % compute the median, ignoring to-be-nulled eigenvalues
         shortened = null_evalues;
-        shortened(null_idcs(1:rfi_cnt)) = [];
+        shortened(null_idcs) = [];
         mm_post = median(shortened); % could also use mean()
 
         % "null" via replacing by median/mean (real nulling with 0.0f is sub-optimal)
-        null_evalues(null_idcs(1:rfi_cnt)) = mm_post;
+        null_evalues(null_idcs) = mm_post;
         null_evalues = diag(null_evalues);
+        
+        if 0, %% debug
+            % apply threshold to pruned input list of MDL
+            thresh_evalues = mdl_evalues;
+            thresh_evalues_idx = find(abs(thresh_evalues) >= rfi_thresh);
+            thresh_evalues(thresh_evalues_idx) = 0;
+            % apply thresh to full input eigenvalue list, null with median
+            thresh_nulled = sort(evalues,2,'descend');
+            thresh_nulled_idcs = (thresh_nulled >= rfi_thresh);
+            thresh_nulled(thresh_nulled_idcs) = mm_post;
+            % plotting
+            figure(1), clf; subplot(3,1,1), hold on;
+            plot(abs(mdl_evalues),'g');
+            plot(abs(thresh_evalues),'b-.');
+            plot(abs(thresh_nulled),'r.');
+            line([Nrfi_mdl+1, Nrfi_mdl+1], ylim()); % line([x1 x2], [y1 y2]);
+            legend('Eigenvalues input to MDL', 'Eigenvalues masked to zero by 3sigma', '3sigma nulled eigenvalue result', 'Line=MDL estimate');
+            title(['Channel ' num2str(cc)]);
+            subplot(3,1,2), hold on;
+            thresh_nulled = sort(thresh_nulled,2,'descend');
+            null_evalues_sorted = sort(diag(null_evalues),1,'descend');
+            plot(abs(thresh_nulled),'g');
+            plot(abs(null_evalues_sorted),'b-.');
+            legend('Nulled eigenvalue result (after sort)', 'Matlab func eigenvalue result (after sort)');
+            title(['Channel ' num2str(cc)]);
+            subplot(3,1,3), hold on;
+            plot(abs(evalues),'g');
+            plot(abs(diag(null_evalues)),'b-.');
+            legend('Original eigenvalues (no sorting)', 'Nulled eigenvalues (no sorting)');            
+            input('enter');
+        end
 
         % finally, reconstruct original matrix 
         % (A = evecs * diag(evalues) * inv(evecs))
-        null_mat = null_evectors * null_evalues * inv(null_evectors);
+        null_mat = evectors * null_evalues * inv(evectors);
         nulldata(pp,:,:,cc) = null_mat;
     end
   end
