abf51331305c12979f5cca0639911031c728548a
HelgeRottmann <rottmann@mpifr-bonn.mpg.de>
2011-11-09 11:07:00 +0000

    Add slot related business logic
    git-svn-id: https://svn.atnf.csiro.au/difx@3906 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- libraries/python/trunk/difxdb/business/slotaction.py
+++ libraries/python/trunk/difxdb/business/slotaction.py
@@ -1,5 +1,16 @@
 from difxdb.model import model
 
+def slotExists(session, location):
+    
+    if (session.query(model.Slot).filter_by(location=location).count() > 0):
+        return(True)
+    else:
+        return(False)
+    
+def getSlotByLocation(session, location):
+    
+    return(session.query(model.Slot).filter_by(location=location).one())
+    
 def getOccupiedSlots(session):
     '''
     Returns a collection of Slot objects that are currently occupied by modules 
