eb7e190aec3edc3ab31ac7f279546eb8e0339005
HelgeRottmann <rottmann@mpifr-bonn.mpg.de>
2011-11-11 13:06:43 +0000

    Fix backref for module-slot relation
    git-svn-id: https://svn.atnf.csiro.au/difx@3916 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- libraries/python/trunk/difxdb/model/dbConnection.py
+++ libraries/python/trunk/difxdb/model/dbConnection.py
@@ -40,11 +40,12 @@ class Schema(object):
         
     def createSchema(self):
 
-        self.slotTable = Table('Slot', self.metadata__,
-            Column('id', Integer, primary_key=True),
-            Column('location', String(20)))
+        #self.slotTable = Table('Slot', self.metadata__,
+        #    Column('id', Integer, primary_key=True),
+        #    Column('location', String(20)))
 
-        self.metadata__.create_all(self.engine__)
+        #self.metadata__.create_all(self.engine__)
+        pass
 
     def createMappers(self):
         
@@ -52,7 +53,7 @@ class Schema(object):
         mapper(ExperimentStatus, self.experimentStatusTable)
         mapper(Experiment, self.experimentTable,properties={'status':relation(ExperimentStatus, uselist = False)})
         mapper(Module, self.moduleTable, properties={'experiments': relation(Experiment, secondary=self.experimentModuleTable, primaryjoin=self.experimentModuleTable.c.moduleID==self.moduleTable.c.id, secondaryjoin=self.experimentModuleTable.c.experimentID==self.experimentTable.c.id, foreign_keys = [self.experimentModuleTable.c.experimentID, self.experimentModuleTable.c.moduleID])})
-        mapper(Slot, self.slotTable,properties={'module': relation(Module, uselist = False, backref='slot')})
+        mapper(Slot, self.slotTable,properties={'module': relation(Module, uselist = False, backref=backref('slot', uselist=False))})
         
 
 class Connection(object):
