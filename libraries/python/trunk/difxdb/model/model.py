c861afa67aa93ab30b67dab8a0caa9c2e8d7221f
HelgeRottmann <rottmann@mpifr-bonn.mpg.de>
2011-11-11 13:07:03 +0000

    Code cleanup
    git-svn-id: https://svn.atnf.csiro.au/difx@3917 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- libraries/python/trunk/difxdb/model/model.py
+++ libraries/python/trunk/difxdb/model/model.py
@@ -1,18 +1,9 @@
 class Module(object):
     pass
-    #__tablename__ = 'Module'
-    #id = Column(Integer, primary_key=True)
-    #parent_id = Column(Integer, ForeignKey('Slot.id'))
-    
     
     
 class Slot (object):
     pass
-    #__tablename__ = 'Slot'
-    #id = Column(Integer, primary_key=True)
-    #module_id = Column(Integer, ForeignKey('Module.id'))
-    #child = relation("Module", uselist=False, backref="Slot")
-
     
     
 class Experiment(object):
