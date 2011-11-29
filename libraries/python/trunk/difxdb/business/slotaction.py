ef1d5541652ca8e0a73c3f6e638a0ff3cb5397e5
HelgeRottmann <rottmann@mpifr-bonn.mpg.de>
2011-11-29 20:22:42 +0000

    Order slots by location
    git-svn-id: https://svn.atnf.csiro.au/difx@4002 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- libraries/python/trunk/difxdb/business/slotaction.py
+++ libraries/python/trunk/difxdb/business/slotaction.py
@@ -16,4 +16,4 @@ def getOccupiedSlots(session):
     Returns a collection of Slot objects that are currently occupied by modules 
     '''
     
-    return(session.query(model.Slot).filter(model.Slot.moduleID != None).all())
+    return(session.query(model.Slot).filter(model.Slot.moduleID != None).order_by(model.Slot.location).all())
