3c8f81a8b062180a2059149563da7cbd80cf61c8
HelgeRottmann <rottmann@mpifr-bonn.mpg.de>
2011-12-08 21:27:33 +0000

    Extend python database model to support job queueing
    git-svn-id: https://svn.atnf.csiro.au/difx@4075 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- libraries/python/trunk/difxdb/model/model.py
+++ libraries/python/trunk/difxdb/model/model.py
@@ -11,14 +11,27 @@
 class Module(object):
     pass
     
-    
 class Slot (object):
     pass
-    
-    
+     
 class Experiment(object):
     pass
 
 class ExperimentStatus(object):
+    pass
+
+class Job(object):
+    pass
+
+class JobStatus(object):
+    pass
+
+class Pass(object):
+    pass
+
+class PassType(object):
+    pass
+
+class Queue(object):
     pass
 
