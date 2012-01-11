db51156b28d95a80b2c395032d47f3fabbd5668e
CormacReynolds <Cormac.Reynolds@csiro.au>
2012-01-11 07:52:28 +0000

    Added an option to transfer using ssh/scp instead of GridFTP. Now runs silently by default (-v increases verbosity).
    git-svn-id: https://svn.atnf.csiro.au/difx@4213 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- applications/espresso/trunk/archivedifx.py
+++ applications/espresso/trunk/archivedifx.py
@@ -1,10 +1,37 @@
 #!/usr/bin/python
-# archive a DiFX data archive. Tar the plethora of small files before transfer.
-# Transfer larger files unmodified. Preserve the original directory structure.
-# Uses globus-url-copy (an ssh option could be added without too much trouble).
+# archive DiFX data files to a remote machine. Tar the plethora of small files
+# before transfer. Transfer larger files unmodified. Preserve the original
+# directory structure. Uses globus-url-copy by default or scp/ssh on request. 
 # Cormac Reynolds: Jan 2012
 
 import optparse, os, re, subprocess, sys
+
+def splithost(hostdir):
+    '''split a host/directory (e.g. cormac@cortex.ivec.org/pbstore/) into host and directory parts'''
+    host, dir = re.search(r'(.*?)(/.*)', hostdir).group(1,2)
+
+    return host, dir
+
+def transfer_command(protocol, preamble, source, dest):
+    '''return a suitable command string for either an ssh or gridftp based transfer'''
+
+    command = str()
+    if protocol == 'ssh':
+        host, dir = splithost(dest)
+        if source == '-':
+            command = preamble + ' ssh ' + host + ' "cat > ' + dir + '"'
+        else:
+            command = preamble + ' scp ' + source + ' ' + host + ':' + dir
+
+    elif protocol == 'gridftp':
+        verbose = ''
+        if options.verbose:
+            verbose = '-vb'
+        command = preamble + ' globus-url-copy -cd ' + verbose + ' ' + source + ' sshftp://' + dest
+    else:
+        raise Exception('Unrecognised transfer protocol!')
+
+    return command
 
 usage = '''%prog <path> <destination>
 will transfer <path> and all its subdirectories to <destination>. Small files (<10 MB) are tarred before transfer. Larger files are transferred unmodified.
@@ -17,11 +44,22 @@ parser = optparse.OptionParser(usage=usage, version='%prog ' + '1.0')
 parser.add_option( "--maxtarsize", "-m",
         type='float', dest="maxtarsize", default=5,
         help='files larger than MAXTARSIZE (MB) will be transferred untarred [default = %default]' )
+parser.add_option( "--ssh", "-s",
+        dest="usessh", action="store_true", default=False,
+        help='Use scp/ssh for transfers instead of GridFTP' )
 parser.add_option( "--checksum", "-c",
         dest="doChecksum", action="store_true", default=False,
         help='Create and send checksum files to the destination' )
+parser.add_option( "--verbose", "-v",
+        dest="verbose", action="store_true", default=False,
+        help='Be verbose' )
 
 (options, args) = parser.parse_args()
+
+protocol = 'gridftp'
+if options.usessh:
+    protocol = 'ssh'
+
 
 # get the list of subdirectories. Note that we don't want to tar large files.
 # In each  subdirectory form a list of files to be tarred, and a list of files
@@ -43,12 +81,10 @@ for dir in os.walk(os.curdir):
 
     tarfile = args[1] + os.sep + dir[0] + os.sep + os.path.basename(os.path.abspath(dir[0])) + '.tar'
 
-    # -cd flag to globus-url copy (version 5) does not create the output
+    # -cd flag to globus-url-copy (version 5) does not create the output
     # directory when the input is stdin. Clearly a bug. Work round it by
     # ensuring the output dir is created.
-    match = re.search(r'(.*?)/(.*)', args[1])
-    host = match.group(1)
-    rootdir = os.sep + match.group(2)
+    host, rootdir = splithost(args[1]) 
     command = 'ssh ' + host + ' mkdir -p ' + rootdir + os.sep + dir[0]
     subprocess.check_call(command, shell=True, stdout=sys.stdout)
 
@@ -57,21 +93,27 @@ for dir in os.walk(os.curdir):
         # just carry on if this fails for some reason
         try:
             checkfile = args[1] + os.sep + dir[0] + os.sep + 'check.cksum'
-            command = 'cd ' + dir[0] + ';' + ' cksum '  + '* | globus-url-copy -vb -cd - sshftp://' + checkfile
-            print command
+            preamble = 'cd ' + dir[0] + '; md5sum * | '
+            command = transfer_command(protocol, preamble, '-', checkfile)
+            if options.verbose:
+                print command
             subprocess.check_call(command, shell=True, stdout=sys.stdout, stderr=sys.stderr)
         except:
-            print 'WARNING: Could not create cksum!'
+            print 'WARNING: Could not create checksum!'
 
     
     # tar up small files in this directory and transfer (on the fly)
     if tarlist:
-        command = 'tar -C ' + dir[0] + ' -cf - ' + tarlist + ' | globus-url-copy -vb -cd - sshftp://' + tarfile
-        print command
+        preamble = 'tar -C ' + dir[0] + ' -cf - ' + tarlist + ' | '
+        command = transfer_command(protocol, preamble, '-', tarfile)
+        if options.verbose:
+            print command
         subprocess.check_call(command, shell=True, stdout=sys.stdout, stderr=sys.stderr)
 
     # transfer each of the large files in turn
-    for file in transfer:
-        command = 'globus-url-copy -vb -cd file://' + file + ' sshftp://' + args[1] + os.sep + dir[0] + os.sep
-        #print command
+    for srcfile in transfer:
+        destdir = args[1] + os.sep + dir[0] + os.sep
+        command = transfer_command(protocol, '', srcfile, destdir)
+        if options.verbose:
+            print command
         subprocess.check_call(command, shell=True, stdout=sys.stdout)
