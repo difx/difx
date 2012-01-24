32c35d56b2304058689e98edb3ecd09bfb404f1a
HelgeRottmann <rottmann@mpifr-bonn.mpg.de>
2012-01-24 13:17:20 +0000

    add new python module
    git-svn-id: https://svn.atnf.csiro.au/difx@4242 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

---
+++ libraries/python/trunk/difxutil/dbutil.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#===========================================================================
+# SVN properties (DO NOT CHANGE)
+#
+# $Id$
+# $HeadURL$
+# $LastChangedRevision$
+# $Author$
+# $LastChangedDate$
+#
+#============================================================================
+
+def none2String(variable):
+    
+    if variable is None:
+        return("")
+    else:
+        return(variable)
