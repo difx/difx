a09f5c7714cc571a0dbede171de52ef4e524c033
CormacReynolds <Cormac.Reynolds@csiro.au>
2012-01-31 05:56:42 +0000

    Give option to carry on even if some files cannot be transferred (-i). Added some options to pass additional switches to tar and globus-url-copy (-t, -g). Provide slightly nicer output when the -v switch is used.
    git-svn-id: https://svn.atnf.csiro.au/difx@4261 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- applications/espresso/trunk/archivedifx.py
+++ applications/espresso/trunk/archivedifx.py
@@ -8,26 +8,28 @@ import optparse, os, re, subprocess, sys
 
 def splithost(hostdir):
     '''split a host/directory (e.g. cormac@cortex.ivec.org/pbstore/) into host and directory parts'''
-    host, dir = re.search(r'(.*?)(/.*)', hostdir).group(1,2)
+    host, directory = re.search(r'(.*?)(/.*)', hostdir).group(1,2)
 
-    return host, dir
+    return host, directory
 
 def transfer_command(protocol, preamble, source, dest):
     '''return a suitable command string for either an ssh or gridftp based transfer'''
 
     command = str()
     if protocol == 'ssh':
-        host, dir = splithost(dest)
+        host, directory = splithost(dest)
         if source == '-':
-            command = preamble + ' ssh ' + host + ' "cat > ' + dir + '"'
+            command = preamble + ' ssh ' + host + ' "cat > ' + directory + '"'
         else:
-            command = preamble + ' scp ' + source + ' ' + host + ':' + dir
+            scpoptions = ' '
+            if not options.verbose:
+                scpoptions = ' -q '
+            command = preamble + ' scp ' + scpoptions + source + ' ' + host + ':' + directory
 
     elif protocol == 'gridftp':
-        verbose = ''
         if options.verbose:
-            verbose = ' -vb '
-        command = preamble + ' globus-url-copy -cd ' + verbose + source + ' sshftp://' + dest
+            options.globusoptions += ' -vb'
+        command = preamble + ' globus-url-copy -cd ' + options.globusoptions + ' ' + source + ' sshftp://' + dest
     else:
         raise Exception('Unrecognised transfer protocol!')
 
@@ -44,17 +46,31 @@ parser = optparse.OptionParser(usage=usage, version='%prog ' + '1.0')
 parser.add_option( "--maxtarsize", "-m",
         type='float', dest="maxtarsize", default=10,
         help='files larger than MAXTARSIZE (MB) will be transferred untarred [default = %default]' )
+parser.add_option( "--taroptions", "-t",
+        type='str', dest="taroptions", default=' ',
+        help='specify additional options to pass to tar' )
+parser.add_option( "--globusoptions", "-g",
+        type='str', dest="globusoptions", default=' ',
+        help='specify additional options to pass to globus-url-copy' )
 parser.add_option( "--ssh", "-s",
         dest="usessh", action="store_true", default=False,
         help='Use scp/ssh for transfers instead of GridFTP' )
 parser.add_option( "--checksum", "-c",
         dest="doChecksum", action="store_true", default=False,
-        help='Create and send checksum files to the destination' )
+        help='Create checksum files and send to the destination' )
 parser.add_option( "--verbose", "-v",
         dest="verbose", action="store_true", default=False,
         help='Be verbose' )
+parser.add_option( "--ignorefails", "-i",
+        dest="ignorefails", action="store_true", default=False,
+        help='Continue even if some files cannot be transferred' )
 
 (options, args) = parser.parse_args()
+
+if len(args) < 2:
+    parser.print_help()
+    parser.error("Give source and destination directories!")
+
 
 protocol = 'gridftp'
 if options.usessh:
@@ -67,11 +83,11 @@ if options.usessh:
 # transferred).
 
 os.chdir(args[0])
-for dir in os.walk(os.curdir):
+for directory in os.walk(os.curdir):
     transfer = []
     tarlist = str()
-    for file in dir[2]:
-        fileWithPath = os.path.join(os.path.abspath(dir[0]), file)
+    for file in directory[2]:
+        fileWithPath = os.path.join(os.path.abspath(directory[0]), file)
         if (os.path.getsize(fileWithPath)/1e6 > options.maxtarsize):
             # transfer this large file without tarring
             transfer.append(fileWithPath)
@@ -79,24 +95,24 @@ for dir in os.walk(os.curdir):
             # add to list of files to be tarred
             tarlist += ' ' + file
 
-    tarfile = args[1] + os.sep + dir[0] + os.sep + os.path.basename(os.path.abspath(dir[0])) + '.tar'
+    tarfile = args[1] + os.sep + directory[0] + os.sep + os.path.basename(os.path.abspath(directory[0])) + '.tar'
 
     # -cd flag to globus-url-copy (version 5) does not create the output
     # directory when the input is stdin. Clearly a bug. Work round it by
     # ensuring the output dir is created.
     host, rootdir = splithost(args[1]) 
-    command = 'ssh ' + host + ' mkdir -p ' + rootdir + os.sep + dir[0]
+    command = 'ssh ' + host + ' mkdir -p ' + rootdir + os.sep + directory[0]
     subprocess.check_call(command, shell=True, stdout=sys.stdout)
 
     # create a checksum if you are paranoid.
     if options.doChecksum:
         # just carry on if this fails for some reason
         try:
-            checkfile = args[1] + os.sep + dir[0] + os.sep + 'check.cksum'
-            preamble = 'cd ' + dir[0] + '; md5sum * | '
+            checkfile = args[1] + os.sep + directory[0] + os.sep + 'check.cksum'
+            preamble = 'cd ' + directory[0] + '; md5sum * | '
             command = transfer_command(protocol, preamble, '-', checkfile)
             if options.verbose:
-                print command
+                print '\n' + command
             subprocess.check_call(command, shell=True, stdout=sys.stdout, stderr=sys.stderr)
         except:
             print 'WARNING: Could not create checksum!'
@@ -104,16 +120,30 @@ for dir in os.walk(os.curdir):
     
     # tar up small files in this directory and transfer (on the fly)
     if tarlist:
-        preamble = 'tar -C ' + dir[0] + ' -cf - ' + tarlist + ' | '
+        preamble = 'tar ' + options.taroptions + ' -C ' + directory[0] + ' -cf - ' + tarlist + ' | '
         command = transfer_command(protocol, preamble, '-', tarfile)
         if options.verbose:
-            print command
-        subprocess.check_call(command, shell=True, stdout=sys.stdout, stderr=sys.stderr)
+            print '\n' + command
+        errors = subprocess.Popen(command, shell=True, stdout=sys.stdout, stderr=subprocess.PIPE).communicate()[1]
+        if errors:
+            if options.ignorefails:
+                print errors
+                #print 'Warning! Could not transfer file'
+            else:
+                raise Exception(errors + '\nTransfer aborting!')
+
 
     # transfer each of the large files in turn
     for srcfile in transfer:
-        destdir = args[1] + os.sep + dir[0] + os.sep
+        destdir = args[1] + os.sep + directory[0] + os.sep
         command = transfer_command(protocol, '', srcfile, destdir)
         if options.verbose:
-            print command
-        subprocess.check_call(command, shell=True, stdout=sys.stdout)
+            print '\n' + command
+        try:
+            subprocess.check_call(command, shell=True, stdout=sys.stdout)
+        except:
+            if options.ignorefails:
+                #print 'Warning! Could not transfer', srcfile
+                pass
+            else:
+                raise Exception('Could not transfer ' + srcfile)
