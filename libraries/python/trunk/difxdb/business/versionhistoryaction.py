f52163f0baf527d6c3c4e8f042039de4c066a728
HelgeRottmann <rottmann@mpifr-bonn.mpg.de>
2012-03-21 12:49:26 +0000

    add module for difxdb version history functionality
    git-svn-id: https://svn.atnf.csiro.au/difx@4336 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

---
+++ libraries/python/trunk/difxdb/business/versionhistoryaction.py
@@ -0,0 +1,35 @@
+#===========================================================================
+# SVN properties (DO NOT CHANGE)
+#
+# $Id$
+# $HeadURL$
+# $LastChangedRevision$
+# $Author$
+# $LastChangedDate$
+#
+#============================================================================
+from difxdb.model import model
+from sqlalchemy import desc
+
+def getCurrentSchemaVersionNumber(session):
+    '''
+    Returns the major and minor version numbers of the current schema. The current schema is determined by
+    the dateCreated field in the versionhistory table.
+    '''
+    
+    version = session.query(model.VersionHistory).order_by(desc(model.VersionHistory.dateCreated)).first()
+    return( version.major, version.minor)
+
+def isSchemaVersion(session, major, minor):
+    '''
+    Compares the major and minor version numbers of the current schema against the major and minor versions
+    passed to  the routine as arguments. Returns True if the schema major and minor version are equal or greater
+    than the arguments, False otherwise.
+    '''
+    
+    dbMajor, dbMinor = getCurrentSchemaVersionNumber(session)
+    
+    if (major <= dbMajor) and (minor <= dbMinor):
+        return(True)
+    else:
+        return(False)
