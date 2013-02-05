62fa1c0a21ace7ec7eda3564fb7e43dc9a8f73c9
DiFX-DEV <matthiasbark@domain.com>
2013-02-05 18:21:46 +0000

    roll version number
    git-svn-id: https://svn.atnf.csiro.au/difx@5149 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- applications/vex2difx/trunk/src/vex2opt.cpp
+++ applications/vex2difx/trunk/src/vex2opt.cpp
@@ -39,8 +39,8 @@
 using namespace std;
 
 const string program("vex2opt");
-const string version("0.2");
-const string verdate("20130108");
+const string version("0.3.2");
+const string verdate("20130201");
 const string author("Matthias Bark");
 
 static void usage(int argc, char **argv)
