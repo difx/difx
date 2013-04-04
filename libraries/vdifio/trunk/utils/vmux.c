8f6c9cd51841047d9adf96f564fde8d113eb753b
walterfb <wbrisken@gmail.com>
2013-04-04 15:37:12 +0000

    Forgot to add a file
    git-svn-id: https://svn.atnf.csiro.au/difx@5229 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

---
+++ libraries/vdifio/trunk/utils/vmux.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <vdifio.h>
+
+#define NSRC	10000000
+
+int main(int argc, char **argv)
+{
+	unsigned char *src;
+	unsigned char *dest;
+	FILE *in;
+	int n;
+	int threads[] = { 0, 640, 256, 896 };
+	int nThread = sizeof(threads)/sizeof(int);
+	int inputframesize = 5032;
+	int nGap = 100;
+	int nSort = 5;
+
+	if(argc < 2)
+	{
+		printf("Give filename\n");
+
+		return 0;
+	}
+
+	in = fopen(argv[1], "r");
+
+	if(!in)
+	{
+		printf("Can't open %s\n", argv[1]);
+
+		return 0;
+	}
+
+	src = malloc(NSRC);
+	dest = malloc(2*NSRC);	// conservative
+
+	n = fread(src, 1, NSRC, in);
+
+	printf("%d bytes read\n", n);
+
+	fclose(in);
+
+	n = vdifmux(dest, n/(nThread*inputframesize), src, n, inputframesize, 12800, 2, nThread, threads, nSort, nGap);
+
+	return 0;
+}
