7fec05a1d3530a301e56a2eeb4579780947db331
DiFX-DEV <matthiasbark@domain.com>
2013-07-12 19:48:30 +0000

    back out synthesizer changes until sched is updated
    git-svn-id: https://svn.atnf.csiro.au/difx@5474 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- applications/vex2difx/trunk/src/pystream.cpp
+++ applications/vex2difx/trunk/src/pystream.cpp
@@ -1591,18 +1591,15 @@ int pystream::writeLoifTable(const VexData *V)
 					int len = strlen(comment);
 					int off = 1;
 					int field_count = 0;
-					int MAX_FIELDS = 5;
-					int synthFreq = 0;
-					// parse BACKWARDS from end of string for space-separated tokens
-					// comment format: * [other comments] [{FirstSynth} {SecondSynth} {receiver} {FirstLO} {BROAD|NARROW|NA}]
+					// parse BACKWARDS from end of string for three space-separated tokens
+					// comment format: * [other comments] [{receiver} {FirstLO} {BROAD|NARROW|NA}]
 					// trailing spaces are permitted
-					// printf("process comment\n");
-					while(field_count <= (MAX_FIELDS-1) && off < len)
+					while(field_count <= 2 && off < len)
 					{
 						// remove trailing WS
 						while(comment[len - off] == ' ' || comment[len - off] == '\t')
 						{
-							printf("len: %i -- off: %i -- str: <%s>\n", len, off, (&comment[len - off]));
+							// printf("len: %i -- off: %i -- str: <%s>\n", len, off, (&comment[len - off]));
 							++off;
 						}
 						// terminate string and advance offset past WS
@@ -1618,12 +1615,10 @@ int pystream::writeLoifTable(const VexData *V)
 						if(field_count == 0)
 						{
 							//check format of comment
-							if(strcmp("BROAD", &(comment[len - off + 1])) != 0 
-								&& strcmp("NARROW", &(comment[len - off + 1])) != 0 
-								&& strcmp("NA", &(comment[len - off + 1])) != 0)
+							if(strcmp("BROAD", &(comment[len - off + 1])) != 0 && strcmp("NARROW", &(comment[len - off + 1])) != 0 && strcmp("NA", &(comment[len - off + 1])) != 0)
 							{
 								// comment doesn't fit our "special format", don't process
-								field_count = MAX_FIELDS;
+								field_count = 3;
 								continue;
 							}
 						}
@@ -1645,27 +1640,11 @@ int pystream::writeLoifTable(const VexData *V)
 								*this << ", '" << &(comment[len - off + 1]) << "'";
 								++field_count;
 								break;
-							// firstSynth
-							case 3:
-								if( isdigit(comment[len - off + 1]) )
-									synthFreq = atoi(&comment[len - off + 1]);
-								else
-									synthFreq = 0;
-								++field_count;
-								break;
-							// secondSynth
-							case 4:
-								if( isdigit(comment[len - off + 1]) )
-									*this << ", " << atoi(&comment[len - off + 1]) << ", " << synthFreq;
-								else
-									*this << ", 0, " << synthFreq;
-								++field_count;
-								break;
 						}
 						// terminate partial string
 						comment[len - off] = '\0';
 						++off;
-//						printf("remaining comment: >%s<\n", comment);
+						// printf("remaining comment: >%s<\n", comment);
 					}
 				} 
 				else 
@@ -1850,7 +1829,6 @@ int pystream::writeDDCLoifTable(const VexData *V)
 */
 			string ifname;
 			bool ifFound;
-			int MAX_FIELDS = 5;
 			for(int DBEloop = 0; DBEloop < (need2DBEbyMode[modeNum]?2:1); DBEloop++) {
 				if( need2DBEbyMode[modeNum] ) {
 					strcpy( dbeAppend, (DBEloop==0?"a":"b"));
@@ -1890,7 +1868,6 @@ int pystream::writeDDCLoifTable(const VexData *V)
 						const int MaxCommentLength = 256;
 						char comment[MaxCommentLength] = {0};
 						double firstTune;
-						int synthFreq = 0;
 						firstTune = fabs(setup->firstTuningForIF(i.name) - i.ifSSLO);
 
 						//*this << "# first tuning = " << (firstTune * 1.0e-6) << " MHz" << endl;
@@ -1911,7 +1888,7 @@ int pystream::writeDDCLoifTable(const VexData *V)
 							// parse BACKWARDS from end of string for three space-separated tokens
 							// comment format: * [other comments] [{receiver} {FirstLO} {BROAD|NARROW|NA}]
 							// trailing spaces are permitted
-							while(field_count <= (MAX_FIELDS-1) && off < len)
+							while(field_count <= 2 && off < len)
 							{
 								// remove trailing WS
 								while(comment[len - off] == ' ' || comment[len - off] == '\t')
@@ -1937,7 +1914,7 @@ int pystream::writeDDCLoifTable(const VexData *V)
 										&& strcmp("NA", &(comment[len - off + 1])) != 0)
 									{
 										// comment doesn't fit our "special format", don't process
-										field_count = MAX_FIELDS;
+										field_count = 3;
 										continue;
 									}
 								}
@@ -1959,27 +1936,11 @@ int pystream::writeDDCLoifTable(const VexData *V)
 										*this << ", '" << &(comment[len - off + 1]) << "'";
 										++field_count;
 										break;
-									// firstSynth
-									case 3:
-										if( isdigit(comment[len - off + 1]) )
-											synthFreq = atoi(&comment[len - off + 1]);
-										else
-											synthFreq = 0;
-										++field_count;
-										break;
-									// secondSynth
-									case 4:
-										if( isdigit(comment[len - off + 1]) )
-											*this << ", " << atoi(&comment[len - off + 1]) << ", " << synthFreq;
-										else
-											*this << ", 0, " << synthFreq;
-										++field_count;
-										break;
 								}
 								// terminate partial string
 								comment[len - off] = '\0';
 								++off;
-//								printf("remaining comment: >%s<\n", comment);
+								// printf("remaining comment: >%s<\n", comment);
 							}
 						} 
 						else 
