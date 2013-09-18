16adb2657d61da0384f83cb117186de124086b2f
walterfb <wbrisken@gmail.com>
2013-09-18 20:14:02 +0000

    rolled version
    git-svn-id: https://svn.atnf.csiro.au/difx@5615 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- applications/vex2difx/trunk/src/vex2script.cpp
+++ applications/vex2difx/trunk/src/vex2script.cpp
@@ -37,8 +37,8 @@
 using namespace std;
 
 const string program("vex2script");
-const string version("0.19");
-const string verdate("20130912");
+const string version("0.20");
+const string verdate("20130918");
 const string author("Walter Brisken, Adam Deller, Matthias Bark");
 
 static void usage(int argc, char **argv)
