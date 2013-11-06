f5c830e5d92d7a6a650eaeaa9e3b01697914f302
HelgeRottmann <rottmann@mpifr-bonn.mpg.de>
2013-11-06 13:17:09 +0000

    oder types by id
    git-svn-id: https://svn.atnf.csiro.au/difx@5722 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- libraries/python/trunk/difxdb/business/experimenttypeaction.py
+++ libraries/python/trunk/difxdb/business/experimenttypeaction.py
@@ -2,11 +2,11 @@
 #===========================================================================
 # SVN properties (DO NOT CHANGE)
 #
-# $Id:$
-# $HeadURL:$
-# $LastChangedRevision:$
-# $Author:$
-# $LastChangedDate:$
+# $Id$
+# $HeadURL$
+# $LastChangedRevision$
+# $Author$
+# $LastChangedDate$
 #
 #============================================================================
 from difxdb.model import model
@@ -15,11 +15,11 @@ def getTypes(session):
     """
     Returns a list of all experiment types
     """
-    return(session.query(model.ExperimentType).all())
+    return(session.query(model.ExperimentType).order_by("id").all())
 
 def getActiveTypes(session):
     """
     Returns a list of all acive experiment types
     """
-    return(session.query(model.ExperimentType).filter_by("active=1").all())
+    return(session.query(model.ExperimentType).filter_by(active=1).order_by("id").all())
     
