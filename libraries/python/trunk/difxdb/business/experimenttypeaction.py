9620c9e77fc542099baf00c694d3c680a726baf4
HelgeRottmann <rottmann@mpifr-bonn.mpg.de>
2013-11-06 12:52:49 +0000

    initial commit
    git-svn-id: https://svn.atnf.csiro.au/difx@5721 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

---
+++ libraries/python/trunk/difxdb/business/experimenttypeaction.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+#===========================================================================
+# SVN properties (DO NOT CHANGE)
+#
+# $Id:$
+# $HeadURL:$
+# $LastChangedRevision:$
+# $Author:$
+# $LastChangedDate:$
+#
+#============================================================================
+from difxdb.model import model
+
+def getTypes(session):
+    """
+    Returns a list of all experiment types
+    """
+    return(session.query(model.ExperimentType).all())
+
+def getActiveTypes(session):
+    """
+    Returns a list of all acive experiment types
+    """
+    return(session.query(model.ExperimentType).filter_by("active=1").all())
+    
