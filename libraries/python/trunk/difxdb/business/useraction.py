5f668611354476ec9b264d830ab594e2b6918639
HelgeRottmann <rottmann@mpifr-bonn.mpg.de>
2013-11-26 13:26:42 +0000

    add more user related business logic
    git-svn-id: https://svn.atnf.csiro.au/difx@5774 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- libraries/python/trunk/difxdb/business/useraction.py
+++ libraries/python/trunk/difxdb/business/useraction.py
@@ -11,13 +11,18 @@
 #============================================================================
 from difxdb.model import model
 
+def getUserByName(session, userName):
+    '''
+    Returns the User object that has the given name
+    '''
+    if userName == "":
+        return None
+    
+    return(session.query(model.User).filter_by(name=userName).one())
+
 def getEnabledUsers(session):   
     '''
     Returns a collection of all enabled User objects
     '''
     
-    result =  session.query(model.User).filter_by(enabled = 1);
-    
-    return(result)
-
-
+    return(session.query(model.User).filter_by(enabled=1).order_by("id").all())
