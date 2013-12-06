4aa0a4ead041983a59c015952ac7ef73cbfa3529
walterfb <wbrisken@gmail.com>
2013-12-06 16:19:40 +0000

    Better text provided to user
    git-svn-id: https://svn.atnf.csiro.au/difx@5787 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- applications/mk5daemon/trunk/mk5dir/mk5test.cpp
+++ applications/mk5daemon/trunk/mk5dir/mk5test.cpp
@@ -49,8 +49,13 @@ const char verdate[] = "20131205";
 static void usage(const char *pgm)
 {
 	printf("\n%s ver. %s   %s %s\n\n", program, version, author, verdate);
-
 	printf("Performs StreamStor self tests using the XLRSelfTest() function call\n\n");
+	printf("Usage: %s [options]\n\n", pgm);
+	printf("options can include:\n\n");
+	printf("  --help\n");
+	printf("  -h     print this help information and quit\n\n");
+	printf("  --verbose\n");
+	printf("  -v     be more verbose\n\n");
 
 	printf("This program appears to be compiled for SDK version %d\n\n", SDKVERSION);
 }
@@ -60,7 +65,6 @@ int main(int argc, char **argv)
 {
 	int a, v;
 	int verbose = 0;
-	int force = 0;
 	int retval = EXIT_SUCCESS;
 
 	for(a = 1; a < argc; ++a)
@@ -76,11 +80,6 @@ int main(int argc, char **argv)
 			   strcmp(argv[a], "--quiet") == 0)
 			{
 				--verbose;
-			}
-			else if(strcmp(argv[a], "-f") == 0 ||
-			   strcmp(argv[a], "--force") == 0)
-			{
-				++force;
 			}
 			else if(strcmp(argv[a], "-h") == 0 ||
 			   strcmp(argv[a], "--help") == 0)
@@ -124,11 +123,27 @@ int main(int argc, char **argv)
 		SSHANDLE xlrDevice;
 		int i;
 
+		printf("\nPlease note!!!  This program requires a module to be keyed on in\n");
+		printf("bank A.  For XF2-based Mark5 units only drive buses 0-3 will be tested\n");
+		printf("if no module is in bank B.  For Amazon-based Mark5 units all buses will\n");
+		printf("always be probed.\n\n");
+
+		if(verbose <= 0)
+		{
+			printf("Run with -v to see more information.\n\n");
+		}
+
 		printf("Opening Streamstor...\n");
 		WATCHDOGTEST( XLROpen(1, &xlrDevice) );
 		printf("  Done\n");
 
 		printf("Testing PCI...\n");
+		if(verbose > 0)
+		{
+			printf("According to the StreamStor manual, this test \"tests\n");
+			printf("communications with PCI bus\".  This test may be useful\n");
+			printf("in identifying a card that is not properly seated.\n");
+		}
 		WATCHDOG( xlrRC = XLRSelfTest( xlrDevice, XLR_BIST_PCI) );
 		if(xlrRC == XLR_SUCCESS)
 		{
@@ -146,6 +161,12 @@ int main(int argc, char **argv)
 		}
 
 		printf("Testing buffer...\n");
+		if(verbose > 0)
+		{
+			printf("According to the StreamStor manual, this test will \"write\n");
+			printf("and then read all 512 MB of RAM - checks for errors.\"  This\n");
+			printf("test is useful for identifying bad StreamStor memory.\n");
+		}
 		WATCHDOG( xlrRC = XLRSelfTest( xlrDevice, XLR_BIST_BUFFER) );
 		if(xlrRC == XLR_SUCCESS)
 		{
@@ -163,12 +184,20 @@ int main(int argc, char **argv)
 		}
 
 		printf("Testing buses...\n");
+		if(verbose > 0)
+		{
+			printf("According to the StreamStor manual, \"This will test all\n");
+			printf("disks present on the specifed bus (i.e., master and slave\n");
+			printf("if present).\"  Below each bus is to be tested separately.\n");
+		}
 		for(i = XLR_BIST_DISK0; i <= XLR_BIST_DISK7; ++i)
 		{
+			printf("  Bus %d (bank %c slots %d and %d): ", i-XLR_BIST_DISK0, (i < XLR_BIST_DISK4 ? 'A' : 'B'), 2*((i-XLR_BIST_DISK0)%4), 2*((i-XLR_BIST_DISK0)%4)+1);
+
 			WATCHDOG( xlrRC = XLRSelfTest( xlrDevice, (SS_SELFTEST)i) );
 			if(xlrRC == XLR_SUCCESS)
 			{
-				printf("  Bus %d Success\n", i-XLR_BIST_DISK0);
+				printf("Success\n", i-XLR_BIST_DISK0);
 			}
 			else
 			{
@@ -178,7 +207,7 @@ int main(int argc, char **argv)
 				xlrError = XLRGetLastError();
 				if(xlrError == 7)
 				{
-					printf("  Bus %d Unpopulated\n", i-XLR_BIST_DISK0);
+					printf("Unpopulated\n", i-XLR_BIST_DISK0);
 				}
 				else
 				{
@@ -197,7 +226,8 @@ int main(int argc, char **argv)
 		WATCHDOG( XLRCardReset(1) );
 		printf("  Done\n");
 		
-		printf("\nNote: The next program to access the Streamstor program will require many seconds to open the device because of the reset.\n");
+		printf("\nNote: The next program to access the Streamstor program will require many\n");
+		printf("seconds to open the device because of the reset.\n\n");
 	}
 
 	unlockMark5();
