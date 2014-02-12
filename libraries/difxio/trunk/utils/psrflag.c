26a50aa755be4c206c54ca693377f01624129fdc
walterfb <wbrisken@gmail.com>
2014-02-12 00:31:32 +0000

    Add start of new utility to generate pulsar flag tables
    git-svn-id: https://svn.atnf.csiro.au/difx@5943 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

---
+++ libraries/difxio/trunk/utils/psrflag.c
@@ -0,0 +1,319 @@
+/***************************************************************************
+ *   Copyright (C) 2014 by Walter Brisken                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+/*===========================================================================
+ * SVN properties (DO NOT CHANGE)
+ *
+ * $Id: difxcalculator.c 1419 2009-08-29 21:37:08Z WalterBrisken $
+ * $HeadURL: https://svn.atnf.csiro.au/difx/libraries/difxio/branches/difx-1.5/utils/difxcalculator.c $
+ * $LastChangedRevision: 1419 $
+ * $Author: WalterBrisken $
+ * $LastChangedDate: 2009-08-29 15:37:08 -0600 (Sat, 29 Aug 2009) $
+ *
+ *==========================================================================*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <complex.h>
+#include <fftw3.h>
+#include "difx_input.h"
+
+const char program[] = "psrflag";
+const char author[]  = "Walter Brisken <wbrisken@nrao.edu>";
+const char version[] = "0.1";
+const char verdate[] = "20140211";
+
+void usage()
+{
+	printf("%s  ver. %s  %s  %s\n\n", program, version, author, verdate);
+	printf("Usage : %s [options] <inputfilebase1> [ <inputfilebase2> [...] ]\n\n", program);
+	printf("options can include:\n");
+	printf("--verbose\n");
+	printf("-v         be a bit more verbose\n\n");
+	printf("--help\n");
+	printf("-h         print help information and quit\n\n");
+	printf("<inputfilebaseN> is the base name of a difx fileset.\n\n");
+}
+
+double interpolate(double x0, double y0, double x1, double y1, double x)
+{
+	if(x1 < x0)
+	{
+		x1 += 1.0;
+	}
+	if(x < x0)
+	{
+		x += 1.0;
+	}
+	return y0 + (y1-y0)*(x-x0)/(x1-x0);
+}
+
+int fftGate(const DifxInput *D, int pulsarId, int configId, const char *pulsarName)
+{
+	const int dataSize=80000;
+	const int fftSize=560000;
+	char fileName[DIFXIO_FILENAME_LENGTH];
+	double gate[fftSize];
+	double tInt;
+	double f0;	/* reference spin period, seconds */
+	fftw_complex Fgate[fftSize/2+1];
+	fftw_plan plan;
+	int i;
+	int nBin, bin;
+	double m;
+	double binEnd0, binEnd1, binWeight1;
+	FILE *out;
+
+	for(i = 0; i < fftSize; ++i)
+	{
+		gate[i] = 0;
+	}
+
+	tInt = D->config[configId].tInt;
+	f0 = D->pulsar[pulsarId].polyco[0].f0;
+
+	printf("tInt = %f sec, pulsar f0 = %f Hz\n", tInt, f0);
+
+	nBin = D->pulsar[pulsarId].nBin;
+	
+	for(i = 0; i < dataSize; ++i)
+	{
+		double phi;	/* turns of pulsar phase */
+
+		phi = f0*tInt*i/dataSize;
+		phi -= (int)phi;
+		
+		for(bin = 0; bin < nBin; ++bin)
+		{
+			if(D->pulsar[pulsarId].binEnd[bin] > phi)
+			{
+				break;
+			}
+		}
+		if(bin == nBin)
+		{
+			bin = 0;
+		}
+
+		binEnd0 = D->pulsar[pulsarId].binEnd[(bin+nBin-1) % nBin];
+		binEnd1 = D->pulsar[pulsarId].binEnd[bin];
+		binWeight1 = D->pulsar[pulsarId].binWeight[bin];
+
+		gate[i] = binWeight1;
+	}
+	
+	snprintf(fileName, DIFXIO_FILENAME_LENGTH, "%s.bin", pulsarName);
+	out = fopen(fileName, "w");
+	if(!out)
+	{
+		fprintf(stderr, "Error: cannot open %s for write\n", fileName);
+	}
+	else
+	{
+		for(i = 0; i < dataSize; ++i)
+		{
+			double phi;	/* turns of pulsar phase */
+
+			phi = f0*tInt*i/dataSize;
+
+			fprintf(out, "%d %f %f %f %f\n", i, tInt*i/fftSize, phi, phi-(int)phi, gate[i]);
+		}
+		fclose(out);
+	}
+
+	plan = fftw_plan_dft_r2c_1d(fftSize, gate, Fgate, FFTW_ESTIMATE);
+	fftw_execute(plan);
+	fftw_destroy_plan(plan);
+
+	m = Fgate[0];
+	for(i = 0; i <= fftSize/2; ++i)
+	{
+		Fgate[i] /= m;
+	}
+
+	snprintf(fileName, DIFXIO_FILENAME_LENGTH, "%s.fft", pulsarName);
+	out = fopen(fileName, "w");
+	if(!out)
+	{
+		fprintf(stderr, "Error: cannot open %s for write\n", fileName);
+	}
+	else
+	{
+		for(i = 0; i <= dataSize/2; ++i)
+		{
+			fprintf(out, "%d %f %f\n", i, (1.0/tInt)*i*dataSize/fftSize, creal(Fgate[i]*~Fgate[i]));
+		}
+		fclose(out);
+	}
+
+
+	return 0;
+}
+
+int main(int argc, char **argv)
+{
+	DifxInput *D = 0;
+	int a;
+	int verbose = 0;
+	int mergable, compatible;
+	int nJob = 0;
+	int pulsarId;
+	
+	for(a = 1; a < argc; ++a)
+	{
+		if(argv[a][0] == '-')
+		{
+			if(strcmp(argv[a], "-v") == 0 ||
+			   strcmp(argv[a], "--verbose") == 0)
+			{
+				++verbose;
+				continue;
+			}
+			else if(strcmp(argv[a], "-h") == 0 ||
+			   strcmp(argv[a], "--help") == 0)
+			{
+				usage();
+
+				exit(EXIT_SUCCESS);
+			}
+			else
+			{
+				fprintf(stderr, "Unknown option %s\n", argv[a]);
+
+				exit(EXIT_FAILURE);
+			}
+		}
+		else if(D == 0)
+		{
+			D = loadDifxInput(argv[a]);
+		}
+		else
+		{
+			DifxInput *D1, *D2;
+
+			D1 = D;
+			D2 = loadDifxInput(argv[a]);
+			if(D2)
+			{
+				mergable = areDifxInputsMergable(D1, D2);
+				compatible = areDifxInputsCompatible(D1, D2);
+				if(mergable && compatible)
+				{
+					D = mergeDifxInputs(D1, D2, verbose);
+					deleteDifxInput(D1);
+					deleteDifxInput(D2);
+				}
+				else
+				{
+					printf("cannot merge job %s: mergable=%d compatible=%d\n", argv[a], mergable, compatible);
+					deleteDifxInput(D1);
+					deleteDifxInput(D2);
+					D = 0;
+				}
+			}
+			else
+			{
+				deleteDifxInput(D);
+				D = 0;
+			}
+		}
+		if(!D)
+		{
+			fprintf(stderr, "File %s -> D == 0.  Quitting\n", argv[a]);
+
+			return EXIT_FAILURE;
+		}
+		else
+		{
+			++nJob;
+		}
+	}
+
+	if(nJob == 0)
+	{
+		printf("Nothing to do!  Quitting.  Run with -h for help information\n");
+
+		return EXIT_SUCCESS;
+	}
+
+	D = updateDifxInput(D);
+	if(!D)
+	{
+		fprintf(stderr, "Update failed: D == 0.  Quitting\n");
+		
+		return EXIT_FAILURE;
+	}
+
+	//printDifxInput(D);
+
+	for(pulsarId = 0; pulsarId < D->nPulsar; ++pulsarId)
+	{
+		int sourceId;
+		int scanId;
+		int configId;
+		const char *psrName;
+
+		for(configId = 0; configId < D->nConfig; ++configId)
+		{
+			if(D->config[configId].pulsarId == pulsarId)
+			{
+				break;
+			}
+		}
+		if(configId >= D->nConfig)
+		{
+			printf("Skipping pulsarId=%d because no config uses it\n", pulsarId);
+
+			continue;
+		}
+
+		for(scanId = 0; scanId < D->nScan; ++scanId)
+		{
+			if(D->scan[scanId].configId == configId)
+			{
+				break;
+			}
+		}
+		if(scanId >= D->nScan)
+		{
+			printf("Skipping pulsarId=%d because no scan uses it\n", pulsarId);
+
+			continue;
+		}
+
+		sourceId = D->scan[scanId].phsCentreSrcs[D->scan[scanId].nPhaseCentres-1];
+
+		if(sourceId < 0 || sourceId >= D->nSource)
+		{
+			printf("Skipping pulsarId=%d because sourceId = %d and nSource = %d\n", pulsarId, sourceId, D->nSource);
+
+			continue;
+		}
+
+		psrName = D->source[sourceId].name;
+
+		printf("Processing pulsar %s:\n", psrName);
+
+		fftGate(D, pulsarId, configId, psrName);
+	}
+
+	deleteDifxInput(D);
+
+	return 0;
+}
