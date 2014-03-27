965ef9dee7bd4ab46260dfa323e0352eff298c0f
walterfb <wbrisken@gmail.com>
2014-03-27 18:04:03 +0000

    Fix build
    git-svn-id: https://svn.atnf.csiro.au/difx@6023 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- mpifxcorr/trunk/src/mark5dir.cpp
+++ mpifxcorr/trunk/src/mark5dir.cpp
@@ -36,60 +36,10 @@
 #include <unistd.h>
 #include <difxmessage.h>
 #include <mark5access.h>
+#include <mark5directorystructs.h>
 #include "mark5dir.h"
 #include "watchdog.h"
 
-#define MODULE_LEGACY_MAX_SCANS	1024
-
-/* as implemented in Mark5A */
-struct Mark5LegacyDirectory
-{
-	int nscans; /* Number of scans herein */
-	int n; /* Next scan to be accessed by "next_scan" */
-	char scanName[MODULE_LEGACY_MAX_SCANS][MODULE_LEGACY_SCAN_LENGTH]; /* Extended name */
-	unsigned long long start[MODULE_LEGACY_MAX_SCANS]; /* Start byte position */
-	unsigned long long length[MODULE_LEGACY_MAX_SCANS]; /* Length in bytes */
-	unsigned long long recpnt; /* Record offset, bytes (not a pointer) */
-	long long plapnt; /* Play offset, bytes */
-	double playRate; /* Playback clock rate, MHz */
-};
-
-/* first updated version as defined by Hastack Mark5 Memo #081 */
-struct Mark5DirectoryHeaderVer1
-{
-	int version;		/* should be 1 */
-	int status;		/* bit field: see MODULE_STATUS_xxx above */
-	char vsn[MODULE_EXTENDED_VSN_LENGTH];
-	char vsnPrev[MODULE_EXTENDED_VSN_LENGTH];	/* "continued from" VSN */
-	char vsnNext[MODULE_EXTENDED_VSN_LENGTH];	/* "continued to" VSN */
-	char zeros[24];
-};
-
-struct Mark5DirectoryScanHeaderVer1
-{
-	unsigned int typeNumber;	/* and scan number; see memo 81 */
-	unsigned short frameLength;
-	char station[2];
-	char scanName[MODULE_SCAN_NAME_LENGTH];
-	char expName[8];
-	long long startByte;
-	long long stopByte;
-};
-
-struct Mark5DirectoryLegacyBodyVer1
-{
-	unsigned char timeBCD[8];	/* version dependent time code. */
-	int firstFrame;
-	int byteOffset;
-	int trackRate;
-	int nTrack;
-	char zeros[40];
-};
-
-struct Mark5DirectoryVDIFBodyVer1
-{
-	unsigned short data[8][4];	/* packed bit fields for up to 8 thread groups */
-};
 
 const char *moduleStatusName(int status)
 {
@@ -183,32 +133,6 @@ void countReplaced(const streamstordatatype *data, int len,
 	*wBad += nBad;
 }
 
-/* This useful routine counts the number of bits set */
-static int countbits(unsigned long int v)
-{
-	int c; // c accumulates the total bits set in v
-
-	for(c = 0; v; ++c)
-	{
-		v &= v - 1; // clear the least significant bit set
-	}
-
-	return c;
-}
-
-/* round the number up to the nearest power of 2 */
-/* from: http://stackoverflow.com/questions/364985/algorithm-for-finding-the-smallest-power-of-two-thats-greater-or-equal-to-a-give */
-static int upround2(int value)
-{
-	int result = 1;
-
-	while(result < value)
-	{
-		result <<= 1;
-	}
-
-	return result;
-}
 
 static int mjd2ymd(long mjd, int *pYear, int *pMonth, int *pDay)
 /*
@@ -321,52 +245,6 @@ int addDecades(int mjd, int nDecade)
 	y += 10*nDecade;
 
 	return doy2mjd(y, doy);
-}
-
-static void convertTimeBCD(const unsigned char *timeBCD, int *mjd, int *sec)
-{
-	int year, doy;
-
-	if(sec)
-	{
-		*sec =	((timeBCD[0] & 0x0F) >> 0)*1 + 
-			((timeBCD[0] & 0xF0) >> 4)*10 +
-			((timeBCD[1] & 0x0F) >> 0)*60 + 
-			((timeBCD[1] & 0xF0) >> 4)*600 +
-			((timeBCD[2] & 0x0F) >> 0)*3600 +
-			((timeBCD[2] & 0xF0) >> 4)*36000;
-	}
-	if(mjd)
-	{
-		doy =	((timeBCD[3] & 0x0F) >> 0)*1 +
-			((timeBCD[3] & 0xF0) >> 4)*10 +
-			((timeBCD[4] & 0x0F) >> 0)*100;
-		year =	((timeBCD[4] & 0xF0) >> 4) +
-			((timeBCD[5] & 0x0F) >> 0)*10 +
-			((timeBCD[5] & 0xF0) >> 4)*100 +
-			((timeBCD[6] & 0x0F) >> 0)*1000;
-		*mjd = doy2mjd(year, doy);
-	}
-}
-
-static void expandScanName1(string &dest, const struct Mark5DirectoryScanHeaderVer1 *scanHeader)
-{
-	char str1[MODULE_SCAN_NAME_LENGTH+1];
-	char str2[3];
-	char str3[9];
-	std::stringstream out;
-
-	strncpy(str1, scanHeader->scanName, MODULE_SCAN_NAME_LENGTH);
-	str1[MODULE_SCAN_NAME_LENGTH] = 0;
-
-	strncpy(str2, scanHeader->station, 2);
-	str2[2] = 0;
-
-	strncpy(str3, scanHeader->expName, 8);
-	str3[8] = 0;
-
-	out << str3 << "_" << str2 << "_" << str1;
-	dest = out.str();
 }
 
 //------------------------- Mark5Scan ------------------------------
@@ -923,16 +801,11 @@ int Mark5Module::readDirectory(SSHANDLE xlrDevice, int mjdref, int (*callback)(i
 	struct Mark5NeoLegacyDirectory *neolegacyDir;
 	unsigned char *dirData;
 	int dirLength;
-	struct mark5_format *mf;
 	char newLabel[XLR_LABEL_LENGTH];
 	int newBank;
 	int bufferLength;
 	struct Mark5DirectoryInfo dirInfo;
-	int die = 0;
-	int oldFast;
 	int v;
-
-	streamstordatatype *bufferStart, *bufferStop, *bufferPrefil;
 
 	if(replacedFrac)
 	{
---
+++ mpifxcorr/trunk/src/mark5directorystructs.h
@@ -0,0 +1,118 @@
+#ifndef __MARK5_DIRECTORY_STRUCTS__
+#define __MARK5_DIRECTORY_STRUCTS__
+
+#define MODULE_LEGACY_MAX_SCANS		1024
+#define MODULE_NEOLEGACY_MAX_SCANS	1024
+#define MODULE_EXTENDED_VSN_LENGTH	32
+#define MODULE_STATION_NAME_LENGTH	2
+#define MODULE_SCAN_NAME_LENGTH         32
+#define MODULE_EXPERIMENT_NAME_LENGTH	8
+#define MODULE_LEGACY_SCAN_LENGTH       64
+
+#define MODULE_STATUS_UNKNOWN           0x00
+#define MODULE_STATUS_ERASED            0x01
+#define MODULE_STATUS_PLAYED            0x02
+#define MODULE_STATUS_RECORDED          0x04
+#define MODULE_STATUS_BANK_MODE         0x08
+
+enum Mark5DirClass
+{
+	Mark5DirClassLegacy = 0,	/* Mark5A or B, with 1024 scan max */
+	Mark5DirClassMark5C,		/* Mark5C */
+	Mark5DirClassNeoLegacy,		/* Mark5A or B, with 65536 scan max */
+	Mark5DirClassNone,		/* no dir on module */
+	Mark5DirClassIllegal		/* does not map to any legal dir type */
+};
+
+struct Mark5DirectoryInfo
+{
+	enum Mark5DirClass dirClass;
+	int dirVersion;
+	int nScan;
+	unsigned int signature;
+};
+
+/* as implemented in Mark5A and Mark5B */
+struct Mark5LegacyDirectory
+{
+	int nscans; /* Number of scans herein */
+	int n; /* Next scan to be accessed by "next_scan" */
+	char scanName[MODULE_LEGACY_MAX_SCANS][MODULE_LEGACY_SCAN_LENGTH]; /* Extended name */
+	unsigned long long start[MODULE_LEGACY_MAX_SCANS]; /* Start byte position */
+	unsigned long long length[MODULE_LEGACY_MAX_SCANS]; /* Length in bytes */
+	unsigned long long recpnt; /* Record offset, bytes (not a pointer) */
+	long long plapnt; /* Play offset, bytes */
+	double playRate; /* Playback clock rate, MHz */
+};
+
+struct Mark5NeoLegacyDirectory
+{
+	int nscans; /* Number of scans herein */
+	int n; /* Next scan to be accessed by "next_scan" */
+	char scanName[MODULE_NEOLEGACY_MAX_SCANS][MODULE_LEGACY_SCAN_LENGTH]; /* Extended name */
+	unsigned long long start[MODULE_NEOLEGACY_MAX_SCANS]; /* Start byte position */
+	unsigned long long length[MODULE_NEOLEGACY_MAX_SCANS]; /* Length in bytes */
+	unsigned long long recpnt; /* Record offset, bytes (not a pointer) */
+	long long plapnt; /* Play offset, bytes */
+	double playRate; /* Playback clock rate, MHz */
+};
+
+/* first updated version as defined by Hastack Mark5 Memo #081 */
+struct Mark5DirectoryHeaderVer1
+{
+	int version;		/* should be 1 */
+	int status;		/* bit field: see MODULE_STATUS_xxx above */
+	char vsn[MODULE_EXTENDED_VSN_LENGTH];
+	char vsnPrev[MODULE_EXTENDED_VSN_LENGTH];	/* "continued from" VSN */
+	char vsnNext[MODULE_EXTENDED_VSN_LENGTH];	/* "continued to" VSN */
+	char zeros[24];
+};
+
+struct Mark5DirectoryScanHeaderVer1
+{
+	unsigned int typeNumber;	/* and scan number; see memo 81 */
+	unsigned short frameLength;
+	char station[MODULE_STATION_NAME_LENGTH];
+	char scanName[MODULE_SCAN_NAME_LENGTH];
+	char expName[MODULE_EXPERIMENT_NAME_LENGTH];
+	long long startByte;
+	long long stopByte;
+};
+
+struct Mark5DirectoryLegacyBodyVer1
+{
+	unsigned char timeBCD[8];	/* version dependent time code. */
+	int firstFrame;
+	int byteOffset;
+	int trackRate;
+	int nTrack;
+	char zeros[40];
+};
+
+struct Mark5DirectoryVDIFBodyVer1
+{
+	unsigned short data[8][4];	/* packed bit fields for up to 8 thread groups */
+};
+
+enum Mark5DirectoryInfoStatus
+{
+	Mark5DirectoryInfoSuccess = 0,
+	Mark5DirectoryInfoErrorSize,
+	Mark5DirectoryInfoErrorGranularity,
+	Mark5DirectoryInfoErrorConnection,
+	Mark5DirectoryInfoErrorVersion,
+	Mark5DirectoryInfoErrorNScan,
+
+	Mark5DirectoryInfoEnumSize	/* list terminator */
+};
+
+extern const char Mark5DirectoryInfoStatusStrings[][40];
+
+
+int calculateMark5DirSignature(const unsigned char *data, int size);
+
+enum Mark5DirectoryInfoStatus getMark5DirectoryInfo(struct Mark5DirectoryInfo *info, const unsigned char *dirData, int dirSize);
+
+void fprintMark5DirectoryInfo(FILE *out, const struct Mark5DirectoryInfo *info);
+
+#endif
