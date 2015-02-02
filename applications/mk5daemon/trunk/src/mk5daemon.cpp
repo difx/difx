bd4347d1ff41d6a16f55a59504c603c67c6b6948
walterfb <wbrisken@gmail.com>
2015-02-02 20:54:26 +0000

    Test if there is a streamstor card
    git-svn-id: https://svn.atnf.csiro.au/difx@6477 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- applications/mk5daemon/trunk/src/mk5daemon.cpp
+++ applications/mk5daemon/trunk/src/mk5daemon.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2008-2013 by Walter Brisken                             *
+ *   Copyright (C) 2008-2015 by Walter Brisken                             *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -976,6 +976,11 @@ int main(int argc, char **argv)
 	int justStarted = 1;
 	int recordFD;
 	int isMk5 = 1;
+
+	if(XLRDeviceFind() < 1)
+	{
+		isMk5 = 0;
+	}
 #else
 	int isMk5 = 0;
 #endif
