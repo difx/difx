6405147008477bcceb8c88a56d38129f222ddea6
jwagnerhki <jan.wagner@iki.fi>
2015-04-17 08:01:46 +0000

    added helper to generate arrays for mark5_stream_decode() more easily
    git-svn-id: https://svn.atnf.csiro.au/difx@6607 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- libraries/mark5access/trunk/python/mark5access/mark5access_helpers.py
+++ libraries/mark5access/trunk/python/mark5access/mark5access_helpers.py
@@ -15,3 +15,12 @@ def get_frame_time(ms):
 	ns  = ctypes.c_double(0.0)
 	mark5access.mark5_stream_get_frame_time(ms, mjd,sec,ns)
 	return (mjd.value,sec.value,ns.value)
+
+def make_decoder_array(ms, nsamples, dtype=ctypes.c_float):
+	"""Returns a 'nchan x nsamples' array that can be passed to mark5_stream_decode()."""
+	DT_ARR   = dtype*nsamples
+	P_DT_ARR = ctypes.POINTER(dtype)*ms.contents.nchan
+	pdata = P_DT_ARR()
+	for i in range(ms.contents.nchan):
+		pdata[i] = DT_ARR()
+	return pdata
