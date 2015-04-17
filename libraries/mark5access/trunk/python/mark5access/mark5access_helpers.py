da9d1c2b84bfa2a606cae4fd86ef81e23a7530fc
jwagnerhki <jan.wagner@iki.fi>
2015-04-17 08:30:06 +0000

    added more pythonic helpers also for mark5_stream_get_sample_time and mark5_stream_count_high_states
    git-svn-id: https://svn.atnf.csiro.au/difx@6609 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- libraries/mark5access/trunk/python/mark5access/mark5access_helpers.py
+++ libraries/mark5access/trunk/python/mark5access/mark5access_helpers.py
@@ -9,11 +9,19 @@ import ctypes
 from . import mark5access
 
 def get_frame_time(ms):
-	"""Returns the timestamp of the current data frame as a tuple (mjd,sec,ns)."""
+	"""Returns the timestamp of the most recently read data frame as a tuple (mjd,sec,ns)."""
 	mjd = ctypes.c_int(0)
 	sec = ctypes.c_int(0)
 	ns  = ctypes.c_double(0.0)
 	mark5access.mark5_stream_get_frame_time(ms, mjd,sec,ns)
+	return (mjd.value,sec.value,ns.value)
+
+def get_sample_time(ms):
+	"""Returns the timestamp of next sample to be decoded as a tuple (mjd,sec,ns)."""
+	mjd = ctypes.c_int(0)
+	sec = ctypes.c_int(0)
+	ns  = ctypes.c_double(0.0)
+	mark5access.mark5_stream_get_sample_time(ms, mjd,sec,ns)
 	return (mjd.value,sec.value,ns.value)
 
 def make_decoder_array(ms, nsamples, dtype=ctypes.c_float):
@@ -24,3 +32,9 @@ def make_decoder_array(ms, nsamples, dtype=ctypes.c_float):
 	for i in range(ms.contents.nchan):
 		pdata[i] = DT_ARR()
 	return pdata
+
+def count_high_states(ms, nsamples):
+	DT_ARR = ctypes.c_uint*ms.contents.nchan
+	counts = DT_ARR()
+	mark5access.mark5_stream_count_high_states(ms, ctypes.c_int(nsamples), counts)
+	return counts
