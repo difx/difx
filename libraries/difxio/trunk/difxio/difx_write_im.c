436f6be88a7d6389aac8935f7515707da452855f
walterfb <wbrisken@gmail.com>
2015-06-24 19:17:26 +0000

    Write Parallactic Angle and/or Refraction Corrected Elevation to .im file if they are populated
    git-svn-id: https://svn.atnf.csiro.au/difx@6791 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- libraries/difxio/trunk/difxio/difx_write_im.c
+++ libraries/difxio/trunk/difxio/difx_write_im.c
@@ -142,9 +142,15 @@ int writeDifxIM(const DifxInput *D)
 					writeDifxLineArray2(out, "SRC %d ANT %d DRY (us)", i, a, scan->im[a][i][p].dry, order+1);
 					writeDifxLineArray2(out, "SRC %d ANT %d WET (us)", i, a, scan->im[a][i][p].wet, order+1);
 					writeDifxLineArray2(out, "SRC %d ANT %d AZ", i, a, scan->im[a][i][p].az, order+1);
-			//		writeDifxLineArray2(out, "SRC %d ANT %d EL CORR", i, a, scan->im[a][i][p].elcorr, order+1);
+					if(scan->im[a][i][p].elcorr[0] != 0.0 || scan->im[a][i][p].elcorr[1] != 0.0)
+					{
+						writeDifxLineArray2(out, "SRC %d ANT %d EL CORR", i, a, scan->im[a][i][p].elcorr, order+1);
+					}
 					writeDifxLineArray2(out, "SRC %d ANT %d EL GEOM", i, a, scan->im[a][i][p].elgeom, order+1);
-			//		writeDifxLineArray2(out, "SRC %d ANT %d PAR ANGLE", i, a, scan->im[a][i][p].parangle, order+1);
+					if(scan->im[a][i][p].parangle[0] != 0.0 || scan->im[a][i][p].parangle[1] != 0.0)
+					{
+						writeDifxLineArray2(out, "SRC %d ANT %d PAR ANGLE", i, a, scan->im[a][i][p].parangle, order+1);
+					}
 					writeDifxLineArray2(out, "SRC %d ANT %d U (m)", i, a, scan->im[a][i][p].u, order+1);
 					writeDifxLineArray2(out, "SRC %d ANT %d V (m)", i, a, scan->im[a][i][p].v, order+1);
 					writeDifxLineArray2(out, "SRC %d ANT %d W (m)", i, a, scan->im[a][i][p].w, order+1);
