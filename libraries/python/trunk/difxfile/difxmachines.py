56c6568fa173f391eba52f58a493ad2c57b0a929
DiFX-DEV <anderson@gfz-potsdam.de>
2015-07-02 13:58:51 +0000

    cleanup tabs/spaces issues in python code, and correct machine core number calculations for small clusters in difxmachines.py and startdifx
    git-svn-id: https://svn.atnf.csiro.au/difx@6809 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- libraries/python/trunk/difxfile/difxmachines.py
+++ libraries/python/trunk/difxfile/difxmachines.py
@@ -19,226 +19,226 @@ import sys
 
 
 class DifxMachines(object):
-    """
-    class for parsing the DiFX cluster definition file
-    """
-
-    def __init__(self, machinefile):
-	""" 
-	Constructor. Checks that machinefile exists. If not, an IOError is raised.
+	"""
+	class for parsing the DiFX cluster definition file
 	"""
 
-	self.machinefile = machinefile
-	self.version = ""
-	self.nodes = {}
+	def __init__(self, machinefile):
+		""" 
+		Constructor. Checks that machinefile exists. If not, an IOError is raised.
+		"""
 
-        if (not os.path.isfile(self.machinefile)):
-            raise IOError("DiFX machinefile: %s does not exist. " % self.machinefile)
+		self.machinefile = machinefile
+		self.version = ""
+		self.nodes = {}
 
-        self._parse()
+		if (not os.path.isfile(self.machinefile)):
+			raise IOError("DiFX machinefile: %s does not exist. " % self.machinefile)
 
-    def __str__(self):
-	"""
-	Executed when the DifxMachines instance is printed
-	"""
-	result = ""
-	for name, node in self.nodes.iteritems():
-		result += node.__str__() + "\n"
-	return result
+		self._parse()
 
-    def _charRange(self, c1, c2):
-    	"""Generates the characters from `c1` to `c2`, inclusive."""
-	if len(c1) > 1 or len(c2) > 1:
-		raise Exception("Illegal letter range")
-	for c in xrange(ord(c1), ord(c2)+1):
-		yield chr(c)
+	def __str__(self):
+		"""
+		Executed when the DifxMachines instance is printed
+		"""
+		result = ""
+		for name, node in self.nodes.iteritems():
+			result += node.__str__() + "\n"
+		return result
 
-    def getMk5Nodes(self):
-	"""
-	returns a list of all Mk5 nodes
-	"""
-	nodes = []
-	for name, node in self.nodes.iteritems():
-		if node.isMk5 == 1:
-			nodes.append(node)
-	return nodes
-    
-    def getMk5NodeNames(self):
-	"""
-	returns a list of all Mk5 node names
-	"""
-	nodes = []
-	for name, node in self.nodes.iteritems():
-		if node.isMk5 == 1:
-			nodes.append(node.name)
-	return nodes
+	def _charRange(self, c1, c2):
+		"""Generates the characters from `c1` to `c2`, inclusive."""
+		if len(c1) > 1 or len(c2) > 1:
+			raise Exception("Illegal letter range")
+		for c in xrange(ord(c1), ord(c2)+1):
+			yield chr(c)
 
-    def getComputeNodes(self):
-        """
-        Returns a list of nodes serving as compute nodes. 
-        Compute nodes have a threads setting > 0
-        """
-        nodes = []
-        for name, node in self.nodes.iteritems():
-		if node.threads > 0:
-			nodes.append(node)
-	return nodes
-        
-    def getStorageNodes(self):
-	"""
-	returns a list of all nodes having storage areas
-	"""
-	nodes = []
-	for name, node in self.nodes.iteritems():
-		if len(node.fileUrls) > 0:
-			nodes.append(node)
-	return nodes
-    
-    def getHeadNodes(self):
-        """
-        returns a list of allowed headnodes
-        """
-        nodes = []
-	for name, node in self.nodes.iteritems():
-		if node.isHeadnode == 1:
-			nodes.append(node)
-	return nodes
-    
-    def getHeadNodeNames(self):
-        """
-        returns a list of allowed headnode names
-        """
-        nodes = []
-	for name, node in self.nodes.iteritems():
-		if node.isHeadnode == 1:
-			nodes.append(node.name)
-	return nodes
-
-    def getNetworkNodes(self):
-	"""
-	returns a list of all nodes serving as network nodes
-	"""
-	nodes = []
-	for name, node in self.nodes.iteritems():
-		if len(node.networkUrls) > 0:
-			nodes.append(node)
-	return nodes
-
-    def getVersion(self):
-	"""
-	returns a tuple containing the major and minor version of the parsed cluster definition file
-	"""
-	# Note: this code handles a 2-part version code despite the general consensus being that just 
-	# a single integer would do and would be simpler.  Here the version is augmented with ".0" if
-	# it is just an integer to allow the rest of the 2-part version to be handled with no change.
-	versionParts = self.version.split(".")
-	if len(versionParts) < 2:
-		versionParts.append('0')
-	return versionParts
-
-    def _parse(self):
-        '''
-        Parses the machinefile
-        '''
+	def getMk5Nodes(self):
+		"""
+		returns a list of all Mk5 nodes
+		"""
+		nodes = []
+		for name, node in self.nodes.iteritems():
+			if node.isMk5 == 1:
+				nodes.append(node)
+		return nodes
 	
-	lineCount = 0
+	def getMk5NodeNames(self):
+		"""
+		returns a list of all Mk5 node names
+		"""
+		nodes = []
+		for name, node in self.nodes.iteritems():
+			if node.isMk5 == 1:
+				nodes.append(node.name)
+		return nodes
 
-	reLine = re.compile("^\s*(.*?)\s*,\s*([0-2]?),\s*(\d{1,})\s*,?\s*(.*)")	
-	reRange = re.compile("(.+)\[(.*)\-(.*)\]")
-	reLetter = re.compile("[a-z]")		
-
-	lines = open(self.machinefile).readlines()
-	for line in lines:
+	def getComputeNodes(self):
+		"""
+		Returns a list of nodes serving as compute nodes. 
+		Compute nodes have a threads setting > 0
+		"""
+		nodes = []
+		for name, node in self.nodes.iteritems():
+			if node.threads > 0:
+				nodes.append(node)
+		return nodes
 		
-		lineCount += 1	
-		line = line.strip()
+	def getStorageNodes(self):
+		"""
+		returns a list of all nodes having storage areas
+		"""
+		nodes = []
+		for name, node in self.nodes.iteritems():
+			if len(node.fileUrls) > 0:
+				nodes.append(node)
+		return nodes
+	
+	def getHeadNodes(self):
+		"""
+		returns a list of allowed headnodes
+		"""
+		nodes = []
+		for name, node in self.nodes.iteritems():
+			if node.isHeadnode == 1:
+				nodes.append(node)
+		return nodes
+	
+	def getHeadNodeNames(self):
+		"""
+		returns a list of allowed headnode names
+		"""
+		nodes = []
+		for name, node in self.nodes.iteritems():
+			if node.isHeadnode == 1:
+				nodes.append(node.name)
+		return nodes
 
-		# skip empty lines
-		if len(line) == 0:
-			continue
+	def getNetworkNodes(self):
+		"""
+		returns a list of all nodes serving as network nodes
+		"""
+		nodes = []
+		for name, node in self.nodes.iteritems():
+			if len(node.networkUrls) > 0:
+				nodes.append(node)
+		return nodes
 
-		# skip comments
-		if line.strip().startswith('#'):
-			continue
+	def getVersion(self):
+		"""
+		returns a tuple containing the major and minor version of the parsed cluster definition file
+		"""
+		# Note: this code handles a 2-part version code despite the general consensus being that just 
+		# a single integer would do and would be simpler.  Here the version is augmented with ".0" if
+		# it is just an integer to allow the rest of the 2-part version to be handled with no change.
+		versionParts = self.version.split(".")
+		if len(versionParts) < 2:
+			versionParts.append('0')
+		return versionParts
 
-		
-		#remove trailing comment
-		#line = line[:line.find("#")]
+	def _parse(self):
+		'''
+		Parses the machinefile
+		'''
+	
+		lineCount = 0
 
-		# look for version string
-		if line.startswith("version"):
-			verToks= line.split("=")
-			if len(verToks) == 2:
-				self.version = verToks[1].strip()
-			continue
+		reLine = re.compile("^\s*(.*?)\s*,\s*([0-2]?),\s*(\d{1,})\s*,?\s*(.*)")	
+		reRange = re.compile("(.+)\[(.*)\-(.*)\]")
+		reLetter = re.compile("[a-z]")		
 
-		result = reLine.match(line)
-		if result is None:
-			raise Exception ("Missformed line in the machine file (line : %s)" % lineCount)
-		
-		nodeNames = []
-		# check if name field contains a range
-		sub = reRange.match(result.group(1))
-		if (sub):
-			start = sub.group(2)
-			stop = sub.group(3)
-                        
-                        if  not start.isdigit() or  not stop.isdigit():
-                            raise Exception("Illegal range specified in line %s of the machine file" % lineCount)
-                        
-                        if int(start) > int(stop):
-                            raise Exception("Illegal range specified in line %s of the machine file" % lineCount)
-                        
-			try:
-                            for nodeNum in range(int(start), int(stop)+1):
-                                    nodeNames.append(sub.group(1) + str(nodeNum).zfill(len(stop)))
+		lines = open(self.machinefile).readlines()
+		for line in lines:
 
-			except:
-				raise Exception("Illegal range specified in line %s of the machine file" % lineCount)
-		else:
-			nodeNames.append(result.group(1))	
-		
-		for nodeName in nodeNames:
-			node = Node()
-			node.name = nodeName
-			node.threads = int(result.group(3).strip())
+			lineCount += 1	
+			line = line.strip()
 
-			fileUrls = []
-			networkUrls = []
-                        
-                        # check for headnode
-                        if int(result.group(2).strip()) == 2:
-                            node.isHeadnode = 1
-                           			
-			if len(result.groups()) == 4:
-				      
-			        for url in result.group(4).split():
-			                if url.strip().startswith("mark5://"):
-			                        node.isMk5 = 1
-			                elif  url.strip().startswith("file://"):
-                                                # add trailing "/" (if not present)
-                                                if url.strip().endswith("/"):
-                                                    fileUrls.append(url.strip()[7:])
-                                                else:
-                                                    fileUrls.append(url.strip()[7:] + "/")
-			                elif url.strip().startswith("network://"):
-			                        networkUrls.append(url.strip()[10:])
+			# skip empty lines
+			if len(line) == 0:
+				continue
 
-			node.fileUrls = fileUrls
-			node.networkUrls = networkUrls
+			# skip comments
+			if line.strip().startswith('#'):
+				continue
 
-			self.nodes[nodeName] = node
 
-			# remove previous node entry if a later one is found
-			if self.nodes.has_key(node.name):
-			        del self.nodes[node.name]
-			# add node if enabled
-			if int(result.group(2).strip()) > 0:
-			        self.nodes[node.name] = node
+			#remove trailing comment
+			#line = line[:line.find("#")]
 
-	# check that version string was properly set in the cluster definition file
-	if len(self.version) == 0:
-		sys.exit("ERROR: Missing or malformed version statement in the machines file: %s" % machinesfile)
+			# look for version string
+			if line.startswith("version"):
+				verToks= line.split("=")
+				if len(verToks) == 2:
+					self.version = verToks[1].strip()
+				continue
+
+			result = reLine.match(line)
+			if result is None:
+				raise Exception ("Missformed line in the machine file (line : %s)" % lineCount)
+
+			nodeNames = []
+			# check if name field contains a range
+			sub = reRange.match(result.group(1))
+			if (sub):
+				start = sub.group(2)
+				stop = sub.group(3)
+
+				if  not start.isdigit() or  not stop.isdigit():
+					raise Exception("Illegal range specified in line %s of the machine file" % lineCount)
+
+				if int(start) > int(stop):
+					raise Exception("Illegal range specified in line %s of the machine file" % lineCount)
+
+				try:
+					for nodeNum in range(int(start), int(stop)+1):
+						nodeNames.append(sub.group(1) + str(nodeNum).zfill(len(stop)))
+
+				except:
+					raise Exception("Illegal range specified in line %s of the machine file" % lineCount)
+			else:
+				nodeNames.append(result.group(1))	
+
+			for nodeName in nodeNames:
+				node = Node()
+				node.name = nodeName
+				node.threads = int(result.group(3).strip())
+
+				fileUrls = []
+				networkUrls = []
+
+				# check for headnode
+				if int(result.group(2).strip()) == 2:
+					node.isHeadnode = 1
+
+				if len(result.groups()) == 4:
+
+					for url in result.group(4).split():
+						if url.strip().startswith("mark5://"):
+							node.isMk5 = 1
+						elif  url.strip().startswith("file://"):
+							# add trailing "/" (if not present)
+							if url.strip().endswith("/"):
+								fileUrls.append(url.strip()[7:])
+							else:
+								fileUrls.append(url.strip()[7:] + "/")
+						elif url.strip().startswith("network://"):
+							networkUrls.append(url.strip()[10:])
+
+				node.fileUrls = fileUrls
+				node.networkUrls = networkUrls
+
+				self.nodes[nodeName] = node
+
+				# remove previous node entry if a later one is found
+				if self.nodes.has_key(node.name):
+					del self.nodes[node.name]
+				# add node if enabled
+				if int(result.group(2).strip()) > 0:
+					self.nodes[node.name] = node
+
+		# check that version string was properly set in the cluster definition file
+		if len(self.version) == 0:
+			sys.exit("ERROR: Missing or malformed version statement in the machines file: %s" % self.machinefile)
 
 class Node:
 	"""
@@ -247,7 +247,7 @@ class Node:
 	name = ""
 	threads = 0
 	isMk5 = 0
-        isHeadnode = 0
+	isHeadnode = 0
 	fileUrls = []
 	networkUrls = []
 
@@ -264,11 +264,11 @@ if __name__ == "__main__":
 	difxmachines = DifxMachines(sys.argv[1])
 
 	print difxmachines
-        
-        print "------------\nCompute nodes:\n------------"
+		
+	print "------------\nCompute nodes:\n------------"
 	for node in difxmachines.getComputeNodes():
 		print node.name, node.threads
-                
+				
 	print "------------\nMark5 nodes:\n------------"
 	for node in difxmachines.getMk5Nodes():
 		print node.name
@@ -277,10 +277,10 @@ if __name__ == "__main__":
 	for node in difxmachines.getStorageNodes():
 		print node.name
 
-        print "------------\nHead nodes:\n------------"
+		print "------------\nHead nodes:\n------------"
 	for node in difxmachines.getHeadNodes():
 		print node.name
-                
+		
 	print "------------\nNetwork nodes:\n------------"
 	for node in difxmachines.getNetworkNodes():
 		print node.name
