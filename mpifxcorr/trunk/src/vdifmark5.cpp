30135470d9d1a1b95845764023e4f7e5cd0d47aa
DiFX-DEV <john.spitzak.ctr@usno.navy.mil>
2015-11-18 04:24:02 +0000

    would not compile due to stray items
    git-svn-id: https://svn.atnf.csiro.au/difx@7079 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- mpifxcorr/trunk/src/vdifmark5.cpp
+++ mpifxcorr/trunk/src/vdifmark5.cpp
@@ -404,21 +404,19 @@ void VDIFMark5DataStream::initialiseFile(int configindex, int fileindex)
 			cerror << startl << "Error: " << nrecordedbands << " recorded channels (bands) were recorded but they are divided unequally across " << nthreads << " threads.  This is not allowed.  Things will probably get very bad soon..." << endl;
 		}
 		setvdifmuxinputchannels(&vm, nBandPerThread);
-		vdiffilesummarysetsamplerate(&fileSummary, static_cast<int64_t>(bw*2000000LL*nBandPerThread));
 	}
 	else if(nrecordedbands < nthreads)
 	{
 		/* must be a fanout mode (DBBC3 probably) */
 		int nThreadPerBand = nthreads/nrecordedbands;
 
-		cinfo << startl << "Note: " << nThreadPerBand << " threads are used to store each channel (band; e.g., this is a VDIF fanout mode).  Support for this is new.  Congratulations for experimenting with plausible code.  Warranty void on weekdays and select weekends." << endl.
+		cinfo << startl << "Note: " << nThreadPerBand << " threads are used to store each channel (band; e.g., this is a VDIF fanout mode).  Support for this is new.  Congratulations for experimenting with plausible code.  Warranty void on weekdays and select weekends." << endl;
 
 		if(nThreadPerBand * nrecordedbands != nthreads)
 		{
 			cerror << startl << "Error: " << nthreads << " threads were recorded but they are divided unequally across " << nrecordedbands << " record channels (bands).  This is not allowed.  Things are about to go from bad to worse.  Hold onto your HAT..." << endl;
 		}
 		setvdifmuxfanoutfactor(&vm, nThreadPerBand);
-		vdiffilesummarysetsamplerate(&fileSummary, static_cast<int64_t>(bw*2000000LL/nThreadPerBand));
 	}
 
 	/* Note: the following fanout concept is an explicit one and is not relevant to VDIF in any way */
