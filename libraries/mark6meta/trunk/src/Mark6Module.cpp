62cc13f36740cd611e3c538cfe30f96114993cf2
HelgeRottmann <rottmann@mpifr-bonn.mpg.de>
2016-01-15 13:14:07 +0000

    Add GPLv3 license
    git-svn-id: https://svn.atnf.csiro.au/difx@7192 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- libraries/mark6meta/trunk/src/Mark6Module.cpp
+++ libraries/mark6meta/trunk/src/Mark6Module.cpp
@@ -1,10 +1,18 @@
-/* 
- * File:   Mark6Module.cpp
- * Author: Helge Rottmann (MPIfR)
- * 
- * Created on 20. Oktober 2015, 15:08
- */
-
+/*******************************************************************************
+* Copyright (C) 2016  Max-Planck-Institut für Radioastronomie, Bonn, Germany 
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+********************************************************************************/
 #include "Mark6.h"
 #include "Mark6Module.h"
 #include "Mark6DiskDevice.h"
