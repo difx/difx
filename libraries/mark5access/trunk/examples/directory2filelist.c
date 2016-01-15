8d033dac54592f3d6b4b087636d6112691a2442a
HelgeRottmann <rottmann@mpifr-bonn.mpg.de>
2016-01-15 13:18:35 +0000

    Update GPLv3 license
    git-svn-id: https://svn.atnf.csiro.au/difx@7194 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- libraries/mark5access/trunk/examples/directory2filelist.c
+++ libraries/mark5access/trunk/examples/directory2filelist.c
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2011-2015 by Helge Rottmann                             *
+ *   Copyright (C) 2011-2016 by Helge Rottmann                             *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
