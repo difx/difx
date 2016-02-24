353b755bdb9bc7fb05d8403ef8398e65a645b068
adamdeller <adeller@swin.edu.au>
2016-02-24 10:58:31 +0000

    GPLv3'd some code in the applications directory also
    git-svn-id: https://svn.atnf.csiro.au/difx@7250 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- applications/vdif_server/trunk/src/vdif_server.h
+++ applications/vdif_server/trunk/src/vdif_server.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2007 by Walter Brisken                                  *
+ *   Copyright (C) 2007-2016 by Adam Deller and Walter Brisken             *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
