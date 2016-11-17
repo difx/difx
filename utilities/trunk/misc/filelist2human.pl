602cb80615d06c1c757322aa2708418e9ba6dc0a
XhrisPhillips <Chris.Phillips@csiro.au>
2016-11-17 10:27:02 +0000

    Print filelist MJD times as human readable times
    git-svn-id: https://svn.atnf.csiro.au/difx@7545 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

---
+++ utilities/trunk/misc/filelist2human.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+use Astro::Time;
+
+my $day = undef;
+my $dayno = undef;
+my $month = undef;
+my $year = undef;
+my $mjd = undef;
+
+my $ut;
+
+my ($file, $start, $stop, $startday, $startmonth, $startyear, $startut,
+    $stopday, $stopmonth, $stopyear, $stoput);
+
+while (<>) {
+  s/#.*#//; # Ignore comments
+  next if /^\s*$/; # Ignore blank lines
+  ($file, $start, $stop) = split;
+  if (!defined $file || !defined $start || !defined $stop) {
+    warn "Could not understand $_\n";
+    next;
+  }
+
+  ($startday, $startmonth, $startyear, $startut) = mjd2cal($start);
+  ($stopday, $stopmonth, $stopyear, $stoput) = mjd2cal($stop);
+
+  printf("$file %02d/%02d/%04d/%s  %02d/%02d/%04d/%s\n",
+	 $startday, $startmonth, $startyear, turn2str($startut,'H',1),
+	 $stopday, $stopmonth, $stopyear, turn2str($stoput,'H',1));
+}
+
