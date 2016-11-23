cd5050605a06675bce14bc02ff24e350c28662bb
XhrisPhillips <Chris.Phillips@csiro.au>
2016-11-23 23:34:03 +0000

    Initialise threadId
    git-svn-id: https://svn.atnf.csiro.au/difx@7564 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- applications/vex2difx/trunk/vexdatamodel/vex_channel.h
+++ applications/vex2difx/trunk/vexdatamodel/vex_channel.h
@@ -38,7 +38,7 @@
 class VexChannel		// Antenna-specific baseband channel details
 {
 public:
-	VexChannel() : recordChan(-1), subbandId(-1), bbcFreq(0.0), bbcBandwidth(0.0), bbcSideBand(' ') {}
+	VexChannel() : recordChan(-1), subbandId(-1), bbcFreq(0.0), bbcBandwidth(0.0), bbcSideBand(' '), threadId(0) {}
 	void selectTones(int toneIntervalMHz, enum ToneSelection selection, double guardBandMHz);
 	char bandCode() const;
 	friend bool operator ==(const VexChannel &c1, const VexChannel &c2);
