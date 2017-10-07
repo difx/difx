39def3e04a29ae47e448ca7a9664c587a9711391
walterfb <wbrisken@gmail.com>
2017-10-07 10:21:10 +0000

    antenna database starting
    git-svn-id: https://svn.atnf.csiro.au/difx@8016 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

---
+++ libraries/difxio/trunk/difxio/antenna_db.h
@@ -0,0 +1,23 @@
+#ifndef __ANTENNA_DB_H__
+#define __ANTENNA_DB_H__
+
+#include <stdio.h>
+
+#define ANTENNA_DB_NAME_LENGTH	16
+
+typedef struct
+{
+	char name[ANTENNA_DB_NAME_LENGTH];
+	double x, y, z;				/* ITRF coords [m] */
+	double diameter;			/* [m] */
+} AntennaDBEntry;
+
+void fprintAntennaDBEntry(FILE *out, const AntennaDBEntry *ae);
+
+const AntennaDBEntry *antennaDBGetByXYZ(double x, double y, double z);
+
+const AntennaDBEntry *antennaDBGetByName(const char *name);
+
+void ecef2lla(double *lat, double *lon, double *alt, double x, double y, double z);
+
+#endif
