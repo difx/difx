8e5e8653876a6640b145aed9b5834c8c4421623a
HelgeRottmann <rottmann@mpifr-bonn.mpg.de>
2018-05-08 13:18:45 +0000

    Add tkinter widget for date selection
    git-svn-id: https://svn.atnf.csiro.au/difx@8277 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

---
+++ libraries/python/trunk/tkinter/DatePicker.py
@@ -0,0 +1,147 @@
+# coding: latin-1
+
+#===========================================================================
+# Copyright (C) 2016  Max-Planck-Institut für Radioastronomie, Bonn, Germany
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#===========================================================================
+# SVN properties (DO NOT CHANGE)
+#
+# $Id
+# $HeadURL
+# $LastChangedRevision
+# $Author
+# $LastChangedDate
+#
+#============================================================================
+
+import calendar
+import datetime
+import sys
+
+if sys.version[0] == '2':
+    import Tkinter as tk
+else:
+    import tkinter as tk
+ 
+class DatePicker:
+    def __init__(self, parent):
+        self.parent = parent
+        self.cal = calendar.TextCalendar(calendar.SUNDAY)
+        self.year = datetime.date.today().year
+        self.month = datetime.date.today().month
+        self.wid = []
+        self.day_selected = 1
+        self.month_selected = self.month
+        self.year_selected = self.year
+        self.day_name = ''
+	self.selectedDate = datetime.date.today()
+
+        self.setup(self.year, self.month)
+         
+
+    def clear(self):
+        for w in self.wid[:]:
+            w.grid_forget()
+            #w.destroy()
+            self.wid.remove(w)
+     
+    def go_prev(self):
+        if self.month > 1:
+            self.month -= 1
+        else:
+            self.month = 12
+            self.year -= 1
+        #self.selected = (self.month, self.year)
+        self.clear()
+        self.setup(self.year, self.month)
+ 
+    def go_next(self):
+        if self.month < 12:
+            self.month += 1
+        else:
+            self.month = 1
+            self.year += 1
+         
+        #self.selected = (self.month, self.year)
+        self.clear()
+        self.setup(self.year, self.month)
+         
+    def selection(self, day, name):
+        self.day_selected = day
+        self.month_selected = self.month
+        self.year_selected = self.year
+        self.day_name = name
+         
+        #data
+	self.selectedDate = datetime.date(self.year, self.month, day)
+         
+        self.clear()
+        self.setup(self.year, self.month)
+         
+    def setup(self, y, m):
+        left = tk.Button(self.parent, text='<', command=self.go_prev)
+        self.wid.append(left)
+        left.grid(row=0, column=1)
+         
+        header = tk.Label(self.parent, height=2, text='{}   {}'.format(calendar.month_abbr[m], str(y)))
+        self.wid.append(header)
+        header.grid(row=0, column=2, columnspan=3)
+         
+        right = tk.Button(self.parent, text='>', command=self.go_next)
+        self.wid.append(right)
+        right.grid(row=0, column=5)
+         
+        days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+        for num, name in enumerate(days):
+            t = tk.Label(self.parent, text=name[:3])
+            self.wid.append(t)
+            t.grid(row=1, column=num)
+         
+        for w, week in enumerate(self.cal.monthdayscalendar(y, m), 2):
+            for d, day in enumerate(week):
+                if day:
+                    #print(calendar.day_name[day])
+                    b = tk.Button(self.parent, width=1, text=day, command=lambda day=day:self.selection(day, calendar.day_name[(day-1) % 7]))
+                    self.wid.append(b)
+                    b.grid(row=w, column=d)
+                     
+        sel = tk.Label(self.parent, height=2, text='{} {} {} {}'.format(
+            self.day_name, calendar.month_name[self.month_selected], self.day_selected, self.year_selected))
+        self.wid.append(sel)
+        sel.grid(row=8, column=0, columnspan=7)
+         
+         
+ 
+if __name__ == '__main__':
+ 
+    class Control:
+        def __init__(self, parent):
+            self.parent = parent
+            self.choose_btn = tk.Button(self.parent, text='Choose',command=self.popup)
+            self.show_btn = tk.Button(self.parent, text='Show Selected',command=self.print_selected_date)
+            self.choose_btn.grid()
+            self.show_btn.grid()
+            self.data = {}
+             
+        def popup(self):
+            child = tk.Toplevel()
+            cal = DatePicker(child, self.data)
+             
+        def print_selected_date(self):
+            print(self.data)
+             
+ 
+    root = tk.Tk()
+    app = Control(root)
+    root.mainloop()
