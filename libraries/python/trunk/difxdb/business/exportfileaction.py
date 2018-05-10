39ea9d9df11aa18c1f3ae0e08b9c56a6f75db5ce
HelgeRottmann <rottmann@mpifr-bonn.mpg.de>
2018-05-10 17:48:35 +0000

    Add business logic for handling file-based media and exported files
    git-svn-id: https://svn.atnf.csiro.au/difx@8291 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

---
+++ libraries/python/trunk/difxdb/business/exportfileaction.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+#===========================================================================
+# Copyright (C) 2016  Max-Planck-Institut für Radioastronomie, Bonn, Germany
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#===========================================================================
+# SVN properties (DO NOT CHANGE)
+#
+# $Id$
+# $HeadURL$
+# $LastChangedRevision$
+# $Author$
+# $LastChangedDate$
+#
+#============================================================================
+from difxdb.model import model
+
+def getExportFilesByExperimentId(session, expId):
+    '''
+    Returns a list of exported files associated with the experiment of the given id.
+    Returns None when no associated files have been found
+    '''
+
+    try:
+        ret = session.query(model.ExportFile).filter_by(experimentID=expId).all()
+    except:
+        ret = None
+    return(ret)
+
+
