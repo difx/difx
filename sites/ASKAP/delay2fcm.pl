92d1d427d9d44402b1d6110310354813bf3d1497
XhrisPhillips <Chris.Phillips@csiro.au>
2018-07-24 06:14:05 +0000

    Add AIPS delays to fcm
    git-svn-id: https://svn.atnf.csiro.au/difx@8384 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

---
+++ sites/ASKAP/delay2fcm.pl
@@ -0,0 +1,73 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+my @args = ();
+my %delays = ();
+
+sub usage () {
+  print<<EOF;
+Usage:  delat2fcm.pl -akXX N -akXX N [--akXX N ...] <fcm>
+EOF
+  exit();
+}
+
+# Grab the command line values and save delays onto hash and non-arguments onto an array
+while (my $val = shift @ARGV) {
+  if ($val =~ /^-(\S+$)/ || $val =~ /^--(\S+)$/) {
+    my $arg = shift @ARGV;
+    die "Missing argument for $val\n" if (! defined $arg);
+    $delays{$1} = $arg;
+    
+  } else {
+    push @args, $val;
+  }
+}
+
+if (scalar(@args)>1) {
+  die "Too many files passed (@args)\n";
+} elsif (scalar(@args==0)) {
+  usage();
+}
+
+my $fcm = $args[0];
+
+my $index = 1;
+while (-f "$fcm.$index") { $index++ }
+
+my $backupFCM = "$fcm.$index";
+my $newfcm = "$fcm.$index.$$";
+
+open(FCM, $fcm) || die "Could not open $fcm: $!\n";
+
+open(NEWFCM, '>', $newfcm) || die "Could not open $newfcm for writing: $!\n";
+
+while (<FCM>) {
+  if (/^(\s*common.antenna.ant(\d+).delay\s*=\s*)(\S+)\s*ns/) {
+    my $line = $1;
+    my $ant = $2;
+    my $delay = $3;
+    my $deltaDelay = undef;
+    if (exists($delays{"ak$ant"})) {
+      $deltaDelay = $delays{"ak$ant"} * 1e9;  # sec -> nsec
+    } elsif (exists($delays{"co$ant"})) {
+      $deltaDelay = $delays{"co$ant"} * 1e9;
+    }
+    if (defined $deltaDelay) {
+      chomp;
+      my $newDelay = $delay + $deltaDelay;
+      print NEWFCM "$line${newDelay}ns\n";
+    } else {
+      print NEWFCM;
+    }
+  } else {
+    print NEWFCM;
+  }
+}
+close(FCM);
+close(NEWFCM);
+
+rename $fcm, $backupFCM;
+rename $newfcm, $fcm;
+
