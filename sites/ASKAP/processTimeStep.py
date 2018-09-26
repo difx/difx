2d94dae1655d64272bbc76c285adb31ea0810bf1
adamdeller <adeller@swin.edu.au>
2018-09-26 06:52:52 +0000

    Add new convenience script for processing all card/freq groups for a given voltage dump
    git-svn-id: https://svn.atnf.csiro.au/difx@8505 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

---
+++ sites/ASKAP/processTimeStep.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+import os,sys,glob,argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", "--timestep", help="Timestep (the directory name to process")
+parser.add_argument("-r", "--ra", help="Force RA value")
+parser.add_argument("-d", "--dec", help="Force Dec value: use no space if declination is negative, i.e., -d-63:20:23.3")
+parser.add_argument("-b", "--bits", type=int, default=1,help="Number of bits")
+parser.add_argument("-f", "--fcm", default="fcm.txt", help="Name of the fcm file")
+args = parser.parse_args()
+
+if args.timestep == "":
+    parser.error("You must specify a timestep / target directory")
+
+timestep = args.timestep
+
+if not os.path.exists(timestep):
+    parser.error("Target directory (timestep)", timestep, "doesn't exist")
+
+if not os.path.exists(args.fcm):
+    parser.error(fcm, "doesn't exist")
+
+fcm = os.path.abspath(args.fcm)
+examplefiles = []
+antennadirs = sorted(glob.glob(timestep + "/ak*"))
+for a in antennadirs:
+    beamdirs = sorted(glob.glob(a + "/*"))
+    for b in beamdirs:
+        vcraftfiles = glob.glob(b + "/*vcraft")
+        if len(vcraftfiles) > 0:
+            examplefiles = sorted(vcraftfiles)
+            beamname = b.split('/')[-1]
+            break
+
+if len(examplefiles) == 0:
+    print "Couldn't find any vcraft files"
+    sys.exit()
+
+os.chdir(timestep)
+
+difx2fitscommand = "difx2fits -u"
+for e in examplefiles:
+    freqlabel = e.split('/')[-1][5:10]
+    print "Going to process", freqlabel
+    difx2fitscommand = difx2fitscommand + " " + freqlabel + "/craftfrbD2D.input"
+    os.mkdir(freqlabel)
+    os.chdir(freqlabel)
+    os.system("cp %s fcm.txt" % fcm)
+    if os.path.exists("../../eopjunk.txt"):
+        os.system("cp ../../eopjunk.txt .")
+
+    torun = "vcraft2obs.py"
+    if not args.ra == "":
+        torun = torun + " -r" + args.ra
+    if not args.dec == "":
+        torun = torun + " -d" + args.dec
+    if not args.bits == "":
+        torun = torun + " --bits=" + str(args.bits)
+    torun += ' "../ak*/%s/*%s*vcraft"' % (beamname, freqlabel)
+
+    print torun
+    os.system(torun + "> vcraft2obs.log")
+    os.system("tail -n 1 vcraft2obs.log > runaskap2difx")
+    os.system("chmod 775 runaskap2difx")
+    os.system("./runaskap2difx > askap2difx.log")
+    os.system("tail -n 2 askap2difx.log | head -n 1 > runmergedifx")
+    os.system("chmod 775 runmergedifx")
+    os.system("./run.sh")
+    os.system("./runmergedifx")
+    os.chdir("../")
+    
+output = open("rundifx2fits","w")
+output.write(difx2fitscommand + "\n")
+output.close()
+os.system(difx2fitscommand)
