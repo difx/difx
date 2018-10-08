2a37afc5fc4bf8f8b06fdfa9e9103ea67b0366c2
XhrisPhillips <Chris.Phillips@csiro.au>
2018-10-08 05:05:25 +0000

    Added --seperate option to note merge all FPGA data
    git-svn-id: https://svn.atnf.csiro.au/difx@8523 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- sites/ASKAP/processTimeStep.py
+++ sites/ASKAP/processTimeStep.py
@@ -8,6 +8,7 @@ parser.add_argument("-d", "--dec", help="Force Dec value: use no space if declin
 parser.add_argument("-b", "--bits", type=int, default=1,help="Number of bits. Default 1")
 parser.add_argument("-f", "--fcm", default="fcm.txt", help="Name of the fcm file")
 parser.add_argument("-c", "--correctfpgadelays", default=False, action="store_true", help="Figure out and correct 7 microsec FPGA delays")
+parser.add_argument("-s", "--separate", default=False, action="store_true", help="Don't attempt to combine data from each FPGA")
 parser.add_argument("-B", "--beam", default="", help="Correlate a specific beam: blank means the first one (numerically)")
 parser.add_argument("--card", default="", help="Correate only a specific card; blank means all")
 parser.add_argument("-k", "--keep", default=False, action="store_true", help="Keep exisiting codif files")
@@ -88,10 +89,12 @@ for e in examplefiles:
         os.system("./run.sh")
         os.system("rm -rf craftfrbD2D*")
         os.system("./runmergedifx")
+    if args.separate:
+        os.system("difx2fits craftfrbD2D")
     os.chdir("../")
     
 output = open("rundifx2fits","w")
 output.write(difx2fitscommand + "\n")
 output.close()
 os.system("chmod 775 rundifx2fits")
-os.system(difx2fitscommand)
+if not args.separate: os.system(difx2fitscommand)
