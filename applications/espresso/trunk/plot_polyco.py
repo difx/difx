7b53dbd1ff55d9abd6e45178061d9adac31c5d92
CormacReynolds <Cormac.Reynolds@csiro.au>
2018-11-02 08:24:03 +0000

    In mpifxcorr, polyco values are actually split on white space, not by column number as the polyco documentation indicates.
    git-svn-id: https://svn.atnf.csiro.au/difx@8564 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- applications/espresso/trunk/plot_polyco.py
+++ applications/espresso/trunk/plot_polyco.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 # program to evaluate and plot polyco.
 import sys
 import os
@@ -12,19 +12,25 @@ import numpy
 
 
 usage = """%prog [options] <filename>
-plots the contents of polyco in <filename>"""
+plots the contents of polyco in <filename>
+"""
 
 parser = optparse.OptionParser(usage=usage, version="%prog " + "1.0")
 parser.add_option(
         "--outfile", "-o",
         type="str", dest="outfile", default=None,
         help="outfile")
+parser.add_option(
+        "-n",
+        type="int", dest="npolyco", default=0,
+        help="Print Nth polyco value instead of 0th")
 (options, args) = parser.parse_args()
 if len(args) < 1:
     parser.print_help()
     parser.error("give a file name")
 
 polyco_filename = args[0]
+p = options.npolyco
 
 pulsar = []
 date = []
@@ -48,30 +54,40 @@ with open(polyco_filename) as polyco_file:
     ncoeff = 12
     coeff = []
     for lineno, line in enumerate(polyco_file):
+        line = line.rstrip()
+        values = line.split()
         p_line = lineno % (2+ncoeff//3)
         if p_line == 0:
-            pulsar.append(line[0:10])
-            date.append(line[10:19])
-            utc.append(line[19:31])
-            tmid.append(float(line[31:51]))
-            dm.append(float(line[51:72]))
-            doppler.append(float(line[73:79]))
-            residual.append(float(line[79:86]))
+            pulsar.append(values[0])
+            date.append(values[1])
+            utc.append(values[2])
+            tmid.append(float(values[3]))
+            dm.append(float(values[4]))
+            doppler.append(float(values[5]))
+            residual.append(float(values[6]))
         elif p_line == 1:
-            rphase.append(float(line[0:20]))
-            f0.append(float(line[20:38]))
-            obs.append(line[38:43])
-            span.append(int(line[43:49]))
-            ncoeff = int(line[49:54])
-            obs_freq.append(float(line[54:63]))
-            binary_phase.append(float(line[75:80]))
+            rphase.append(float(values[0]))
+            f0.append(float(values[1]))
+            obs.append(values[2])
+            span.append(int(values[3]))
+            ncoeff = int(values[4])
+            obs_freq.append(float(values[5]))
+            if len(values) > 6:
+                binary_phase.append(float(values[6]))
         else:
-            coeff.append(float(line[0:25]))
-            coeff.append(float(line[25:50]))
-            coeff.append(float(line[50:75]))
+            coeff.append(float(values[0]))
+            coeff.append(float(values[1]))
+            coeff.append(float(values[2]))
             if p_line == 1+ncoeff//3:
                 coeffs.append(coeff)
                 coeff = []
+
+# print values of a representative polyco entry, for visual check
+print pulsar[p], date[p], utc[p], tmid[p], dm[p], doppler[p], residual[p],
+print rphase[p], f0[p], obs[p], span[p], obs_freq[p], 
+if binary_phase:
+    print binary_phase[p]
+print coeffs[p]
 
 phases = []
 times = []
@@ -79,7 +95,7 @@ times = []
 for i in range(len(rphase)):
     start = tmid[i]
     end = tmid[i] + span[i]/(24.*60.)
-    step = 10./(24.*60.)
+    step = 1./(24.*60.)
     for time in numpy.arange(start, end, step):
         dt = (time - tmid[i])*1440.
         coeff_sum = 0
@@ -92,14 +108,14 @@ for i in range(len(rphase)):
 #pyplot.tight_layout()
 
 pyplot.subplot(2, 1, 1)
-pyplot.plot(times, phases, '.', label="Polyco phase")
+pyplot.plot(times, phases, ',', label="Polyco phase")
 pyplot.legend(loc="best")
 pyplot.ylabel("Phase")
 
 poly_fit = numpy.polyfit(times, phases, 1)
 poly_eval = numpy.array(phases) - numpy.polyval(poly_fit, times)
 pyplot.subplot(2, 1, 2)
-pyplot.plot(times, poly_eval, '.', label="Residual to Linear fit")
+pyplot.plot(times, poly_eval, ',', label="Residual to Linear fit")
 pyplot.legend(loc="best")
 pyplot.xlabel("MJD")
 
