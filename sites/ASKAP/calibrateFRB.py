58f04fc28f3eba5bc0e7ec409edfc5464cbd6410
XhrisPhillips <Chris.Phillips@csiro.au>
2019-03-14 02:52:54 +0000

    add ability to flag target data and suppress CASA logger
    git-svn-id: https://svn.atnf.csiro.au/difx@8803 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- sites/ASKAP/calibrateFRB.py
+++ sites/ASKAP/calibrateFRB.py
@@ -35,6 +35,8 @@ parser.add_option("-a", "--averagechannels", type=int, default=24,
                   help="Number of channels to average together per cube slice")
 parser.add_option("-F", "--flagfile", default="", 
                   help="Flag file to apply to calibrator data only, if desired. Used to ensure RFI doesn't corrupt FRING or BPASS.")
+parser.add_option("-g", "--tarflagfile", default="", 
+                  help="Flag file to apply to target data only, if desired. Used to flag any necessary channels for, e.g., RFI or missing data")
 parser.add_option("-p","--phasecenter", default="",
                   help="phase center for the target field (blank will leave it at correlation centre)")
 parser.add_option("-l", "--leakagecorrect", default=False, action="store_true", 
@@ -87,6 +89,7 @@ vlbatasks.fitld_corr(options.target, targetdata, [], '', 0.0001)
 
 # Get the number of channels in the dataset
 numchannels = vlbatasks.getNumChannels(targetdata)
+
 # Load up the calibrator data
 caldata = vlbatasks.zapAndCreateUVData("CRAFTCAL","UVDATA", aipsdisk, 1)
 if caldata.exists():
@@ -96,6 +99,10 @@ vlbatasks.fitld_corr(options.calibrator, caldata, [], '', 0.0001)
 # Flag the calibrator data, if desired
 if options.flagfile != "":
     vlbatasks.userflag(caldata, 1, options.flagfile)
+
+# Flag the target data, if desired
+if options.tarflagfile != "":
+    vlbatasks.userflag(targetdata, 1, options.tarflagfile)
 
 # Run CLCOR to correct PANG if needed
 if leakagecorrect:
@@ -229,14 +236,14 @@ vlbatasks.splitmulti(targetdata, clversion, outklass, options.sourcename, seqno)
 vlbatasks.writedata(outputdata, targetoutputfilename, True)
 
 # Convert to a measurement set
-#casaout = open("loadtarget.py","w")
-#casaout.write("importuvfits(fitsfile='%s',vis='%s',antnamescheme='old')\n" % (calibratoroutputfilename, calibratormsfilename))
-#casaout.close()
-#os.system("casa -c loadtarget.py")
-#casaout = open("loadtarget.py","w")
-#casaout.write("importuvfits(fitsfile='%s',vis='%s',antnamescheme='old')\n" % (targetoutputfilename, targetmsfilename))
-#casaout.close()
-#os.system("casa -c loadtarget.py")
+casaout = open("loadtarget.py","w")
+casaout.write("importuvfits(fitsfile='%s',vis='%s',antnamescheme='old')\n" % (calibratoroutputfilename, calibratormsfilename))
+casaout.close()
+os.system("casa --nologger -c loadtarget.py")
+casaout = open("loadtarget.py","w")
+casaout.write("importuvfits(fitsfile='%s',vis='%s',antnamescheme='old')\n" % (targetoutputfilename, targetmsfilename))
+casaout.close()
+os.system("casa --nologger -c loadtarget.py")
 
 
 # Run the imaging via CASA if desired
@@ -251,14 +258,14 @@ if options.imagecube:
         #casaout.write('clean(vis="%s",imagename="%s",outlierfile="",field="",spw="",selectdata=True,timerange="",uvrange="",mode="channel",gridmode="widefield",wprojplanes=-1,niter=100,gain=0.1,threshold="0.0mJy",psfmode="clark",imagermode="csclean",multiscale=[],interactive=False,mask="FRB.cube.mask",nchan=-1,start=1,width=24,outframe="",veltype="radio",imsize=128,cell=["1.0arcsec", "1.0arcsec"],phasecenter="%s",restfreq="",stokes="%s",weighting="natural",robust=0.0,uvtaper=False,pbcor=False,minpb=0.2,usescratch=False,noise="1.0Jy",npixels=0,npercycle=100,cyclefactor=1.5,cyclespeedup=-1,nterms=1,reffreq="",chaniter=False,flatnoise=True,allowchunk=False)\n' % (targetmsfilename, imagebase, options.phasecentre, pol))
         casaout.write('clean(vis="%s",imagename="%s",outlierfile="",field="",spw="",selectdata=True,timerange="",uvrange="",mode="channel",gridmode="widefield",wprojplanes=-1,niter=100,gain=0.1,threshold="0.0mJy",psfmode="clark",imagermode="csclean",multiscale=[],interactive=False,mask=%s,nchan=-1,start=1,width=%d,outframe="",veltype="radio",imsize=%s,cell=["%.2farcsec", "%.2farcsec"],phasecenter="%s",restfreq="",stokes="%s",weighting="natural",robust=0.0,uvtaper=False,pbcor=False,minpb=0.2,usescratch=False,noise="1.0Jy",npixels=0,npercycle=100,cyclefactor=1.5,cyclespeedup=-1,nterms=1,reffreq="",chaniter=False,flatnoise=True,allowchunk=False)\n' % (targetmsfilename, imagebase, maskstr, options.averagechannels, imagesize, pixelsize, pixelsize, options.phasecenter, pol))
         casaout.close()
-        os.system("casa -c imagescript.py")
+        os.system("casa --nologger -c imagescript.py")
 
         # If desired, also make the JMFIT output
         if options.imagejmfit:
             casaout = open("imagescript.py","w")
             casaout.write('exportfits(imagename="%s.image",fitsimage="%s.fits")\n' % (imagebase, imagebase))
             casaout.close()
-            os.system("casa -c imagescript.py")
+            os.system("casa --nologger -c imagescript.py")
             for i in range(numchannels/options.averagechannels):
                 locstring = "%d,%d,%d,%d,%d,%d" % (imagesize/2-12, imagesize/2-12, i, imagesize/2+12, imagesize/2+12, i)
                 os.system("jmfitfromfile.py %s.fits %s.slice%03d.jmfit.stats %s" % (imagebase, imagebase, i, locstring))
