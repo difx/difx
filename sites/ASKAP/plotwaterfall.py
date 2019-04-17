464ee98fa1dde7a34314ec6ce863d86ffe65aeea
adamdeller <adeller@swin.edu.au>
2019-04-17 01:43:01 +0000

    Also plot fscrunched time series
    git-svn-id: https://svn.atnf.csiro.au/difx@8855 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- sites/ASKAP/plotwaterfall.py
+++ sites/ASKAP/plotwaterfall.py
@@ -49,26 +49,39 @@ startfreq = basefreq + (startchan*bandwidth)/nchan
 endfreq = basefreq + (endchan*bandwidth)/nchan
 starttime = 0
 endtime = nbins*res
+dynspec = {}
+fscrunch = {}
 
 for stokes in ["I","Q","U","V","XX","YY"]:
-    dynspec = np.loadtxt("{0}-imageplane-dynspectrum.stokes{1}.txt".format(src, stokes))
-    print dynspec.shape
+    dynspec[stokes] = np.loadtxt("{0}-imageplane-dynspectrum.stokes{1}.txt".format(src, stokes))
+    fscrunch[stokes] = np.sum(dynspec[stokes], 1)
+    print dynspec[stokes].shape
     
     if nbins == 1:
         f = np.linspace(startfreq, endfreq, endchan)
 
-        ax.plot(f,dynspec[startchan:endchan])
+        ax.plot(f,dynspec[stokes][startchan:endchan])
         ax.set_xlabel("Frequency (MHz)")
         ax.set_ylabel("Amp")
         plt.savefig('{0}-imageplane-dynspectrum.stokes{1}.png'.format(src, stokes))
         plt.clf()
 
     else:
-        dynspec[0] = dynspec[0]/90.
+        dynspec[stokes][0] = dynspec[stokes][0]/90.
     
-        ax.imshow(dynspec[:,startchan:endchan].transpose(), cmap=plt.cm.plasma, interpolation='none', extent=[starttime,endtime,endfreq,startfreq])
+        ax.imshow(dynspec[stokes][:,startchan:endchan].transpose(), cmap=plt.cm.plasma, interpolation='none', extent=[starttime,endtime,endfreq,startfreq])
         ax.set_aspect(0.03) # you may also use am.imshow(..., aspect="auto") to restore the aspect ratio
         ax.set_xlabel("Time (ms)")
         ax.set_ylabel("Frequency (MHz)")
         plt.savefig('{0}-imageplane-dynspectrum.stokes{1}.png'.format(src, stokes))
         plt.clf()
+
+times = np.arange(0, (nbins-1)*res, res)
+plt.plot(times,fscrunch["I"][1:],label="I")
+plt.plot(times,fscrunch["Q"][1:],label="Q")
+plt.plot(times,fscrunch["U"][1:],label="U")
+plt.plot(times,fscrunch["V"][1:],label="V")
+plt.legend()
+plt.xlabel("Time (ms)")
+plt.ylabel("Amplitude (arbitrary units)")
+plt.savefig("{0}-fscrunch.png".format(src))
