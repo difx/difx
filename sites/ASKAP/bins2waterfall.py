aa3a981a3a1d12ae003b2390df7f834cb08143a6
XhrisPhillips <Chris.Phillips@csiro.au>
2019-04-24 04:14:24 +0000

    Added more user options
    git-svn-id: https://svn.atnf.csiro.au/difx@8860 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- sites/ASKAP/bins2waterfall.py
+++ sites/ASKAP/bins2waterfall.py
@@ -7,6 +7,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-n", "--nbins", type=int, default=None, help="Number of images files from which to extract the spectrum; note: zero-indexed, so nbins=11 means bin00 to bin10")
 parser.add_argument("-c", "--nchan", type=int, default=None, help="Number of channel slices in each cube image; note: zero-indexed")
 parser.add_argument("-s", "--src", type=str, default=None, help="Source name to be used for the spectra text file prefix")
+parser.add_argument("--rms", help="Set if noise estimation is required", action="store_true")
+parser.add_argument("-p", "--prefix", type=str, default = "", help="Prefix for bin directory")
 
 args = parser.parse_args()
 
@@ -28,14 +30,19 @@ if args.src is None:
 nbins = args.nbins
 nchan = args.nchan
 src = args.src
+prefix = args.prefix
 
-os.system("mkdir bins2waterlogs")
+os.system("mkdir {0}bins2waterlogs".format(prefix))
 
 for stokes in ["I","Q","U","V","XX","YY"]:
     dynspec = np.zeros(nbins*nchan).reshape(nbins,nchan)
+    
     for i in range(nbins):
-        inputimage = "bin{0:02g}/TARGET.cube.{1}.image".format(i, stokes)
+        inputimage = "{0}bin{1:02g}/TARGET.cube.{2}.image".format(prefix, i, stokes)
         runfile = "waterfall.py"
+        print inputimage
+
+        # Get the max flux of the source from source images
         output = open(runfile, "w")
         for chan in range(nchan):
             output.write('imstat(imagename="{0}",box="64,63,65,64",chans="{1:d}",logfile="waterfall.bin{2:02d}.chan{3:03d}.log")\n'.format(inputimage, chan, i, chan))
@@ -46,9 +53,35 @@ for stokes in ["I","Q","U","V","XX","YY"]:
             loglines = open(logfile).readlines()
             for line in loglines:
                 if "maximum value [max]:" in line:
-                    dynspec[i][chan] = float(line.split()[-2])
+                    dynspec[i][chan] = float(line.split()[-2]) # unit = Jy/beam
                     break
         os.system("rm -f waterfall.*.log")
-        os.system("mv casa*.log bins2waterlogs")
+        os.system("mv casa*.log {0}bins2waterlogs".format(prefix))
 
     np.savetxt("{0}-imageplane-dynspectrum.stokes{1}.txt".format(src, stokes), dynspec)
+
+if args.rms:
+    for stokes in ["I","Q","U","V","XX","YY"]:
+        rms = np.zeros(nbins*nchan).reshape(nbins,nchan)
+    
+        for i in range(nbins):
+            noiseimage = "{0}bin{1:02g}/OFFSOURCE.cube.{2}.image".format(prefix, i, stokes)
+            getnoise = "get_rms.py"
+
+            # Get RMS from noise estimation images (using 75% of 512x512 image)
+            noiseout = open(getnoise, "w")
+            for chan in range(nchan):
+                noiseout.write('imstat(imagename="{0}",box="64,64,448,448",chans="{1:d}",logfile="noise.bin{2:02d}.chan{3:03d}.log")\n'.format(noiseimage, chan, i, chan))
+            noiseout.close()
+            os.system("casa --nologger -c {0}".format(getnoise))
+            for chan in range(nchan):
+                logfile = "noise.bin{0:02d}.chan{1:03d}.log".format(i, chan)
+                loglines = open(logfile).readlines()
+                for line in loglines:
+                    if "Root mean square [rms]:" in line:
+                        rms[i][chan] = float(line.split()[-2]) # unit = Jy/beam
+                        break
+            os.system("rm -f noise.*.log")
+            os.system("mv casa*.log {0}bins2waterlogs".format(prefix))
+
+        np.savetxt("{0}-imageplane-rms.stokes{1}.txt".format(src, stokes), rms)
