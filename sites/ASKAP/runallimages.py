3e814692d0cb611eebb8079196e748c59fb6f19b
XhrisPhillips <Chris.Phillips@csiro.au>
2019-04-24 04:13:51 +0000

    Added more user options
    git-svn-id: https://svn.atnf.csiro.au/difx@8859 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- sites/ASKAP/runallimages.py
+++ sites/ASKAP/runallimages.py
@@ -2,12 +2,12 @@
 import os,sys,argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--rfisub", type=bool, default=False, help="Do RFI subtraction")
+parser.add_argument("--rfisub", help="Do RFI subtraction", action="store_true")
 parser.add_argument("-t", "--targfile", nargs='+', type=str, default="", help="The gated target FITS file in need of RFI subtraction")
 parser.add_argument("-r", "--rfifile", type=str, default="", help="The RFI gated FITS file with which the RFI subtraction is performed")
 parser.add_argument("-s", "--scalefact", type=float, default=None, help="The scale factor by which to multiply the RFI file before it's subtracted from the target file: the ratio of the two effective integration times (target int time/RFI int time)")
 parser.add_argument("-o", "--outfile", type=str, default="", help="The output, RFI subtracted FITS file prefix (e.g. FRB_SUB)")
-parser.add_argument("--cal", type=bool, default=False, help="Calibrate the data")
+parser.add_argument("--cal", help="Calibrate the data", action="store_true")
 parser.add_argument("-c", "--caldata", type=str, default="", help="The calibrator FITS file data (note: must be uncalibrated")
 parser.add_argument("-d", "--targdata", nargs='+', type=str, default="", help="The target FITS file data to be calibrated; if --rfisub is set, this will default to the output FITS file from the RFI subtraction (e.g. FRB_SUB)")
 parser.add_argument("--calflag", type=str, default="", help="Text file containing the AIPS flags to be applied to the calibrator data")
@@ -15,6 +15,12 @@ parser.add_argument("--targflag", type=str, default="", help="Text file containi
 parser.add_argument("-a", "--avg", type=int, default=24, help="Number of channels to average together per image cube slice")
 parser.add_argument("-p","--phasecenter", type=str, default="", help="Phase centre for the target field (blank will leave it at correlation centre); format example: 'J2000 21h49m24s -52d58m15s' ")
 parser.add_argument("-n","--nbins", type=int, default=1, help="Number of bins/target files to RFI subtract or calibrate")
+parser.add_argument("-N","--noisecentre", default="", help="CASA format position at which noise should be estimated, blank=don't make an off-source image. Example: --noisecentre='J2000 21h33m23s -54d34m11s' ")
+parser.add_argument("-x", "--xpol", help="Do xpol correction during calibration", action="store_true")
+parser.add_argument("-i", "--imagecube", default=False, help="Do imaging", action="store_true")
+parser.add_argument("--refant", type=int, default=3, help="Reference antenna")
+parser.add_argument("-P", "--prefix", type=str, default = "", help="Prefix for bin directory")
+
 
 args = parser.parse_args()
 
@@ -25,8 +31,9 @@ if len(sys.argv) < 2:
 nbins = args.nbins
 tdata = []
 targfile = []
+prefix=args.prefix
 
-if args.rfisub is True:
+if args.rfisub:
     if args.targfile is '':
         parser.error("You must specify the target gated data FITS file")
     else:
@@ -45,18 +52,18 @@ if args.rfisub is True:
     outfile = []
     for i in range(nbins):
         if nbins>1:
-            outfile.append(args.outfile[i].replace('.FITS', ''))
+            outfile.append(args.outfile.replace('.FITS', ''))
         else: outfile.append(args.outfile.replace('.FITS', ''))
 
 
-if args.cal is True:
+if args.cal:
     if args.caldata is '':
         parser.error("You must specify a calibrator dataset")
     else:
         cdata = args.caldata
         cdatms = cdata.replace('.FITS', '').split('/')[-1]
     if args.targdata is '':
-        if args.rfisub is True:
+        if args.rfisub:
             tdata = outfile
         else: parser.error("You must specify a target dataset to calibrate")
     else:
@@ -77,17 +84,40 @@ avgchan = args.avg
 if args.phasecenter is '': pcen = ''
 else: pcen = '-p '+args.phasecenter
 
+
 for i in range(nbins):
 
-    if args.rfisub is True:
-        print "~/craft/psrvlbireduce/datareduction/uvsubScaled.py {0}.FITS {1} {2} {3}.FITS".format(targfile[i], rfi, scale, outfile[i])
-        os.system("~/craft/psrvlbireduce/datareduction/uvsubScaled.py {0}.FITS {1} {2} {3}.FITS".format(targfile[i], rfi, scale, outfile[i]))
+    if nbins > 1:
+        bins = "_B{0:02g}".format(i)
+    else: bins = ''
     
-    if args.cal is True:
-        print "~/difx/sites/ASKAP/calibrateFRB.py -c {0} -t {1}.FITS {2} {3} -i -a {4} {5} --uvsrt -x /data/CRAFT/0407.model.fits".format(cdata, tdata[i], cflag, tflag, avgchan, pcen)
+    if args.rfisub:
+        print "~/craft/psrvlbireduce/datareduction/uvsubScaled.py {0}.FITS {1} {2} {3}{4}.FITS".format(targfile[i], rfi, scale, outfile[i], bins)
+        os.system("~/craft/psrvlbireduce/datareduction/uvsubScaled.py {0}.FITS {1} {2} {3}{4}.FITS".format(targfile[i], rfi, scale, outfile[i], bins))
+    
+    if args.cal:
+        if args.xpol:
+            xpolmodel = "-x /data/CRAFT/0407.model.fits"
+        else: xpolmodel = ''
+        if args.noisecentre != '':
+            noisecen = "--noisecentre '" + args.noisecentre + "'"
+        else: noisecen=''
+
+        if args.imagecube:
+            doimage = '-i'
+        else: doimage = ''
+
+        if args.refant != "":
+            refant = "--refant=" + str(args.refant)
+        else: refant = ""
+        
+        print "~/difx/sites/ASKAP/calibrateFRB.py -c {0} -t {1}{2}.FITS {3} {4} -a {5} {6} --uvsrt {7} {8} {9} {10}".format(cdata, tdata[i], bins, cflag, tflag, avgchan, pcen, xpolmodel, noisecen, doimage, refant)
         os.system("rm -rf {0}_calibrated_uv.ms".format(cdatms))
-        os.system("~/difx/sites/ASKAP/calibrateFRB.py -c {0} -t {1}.FITS {2} {3} -i -a {4} {5} --uvsrt -x /data/CRAFT/0407.model.fits".format(cdata, tdata[i], cflag, tflag, avgchan, pcen))
-        os.system("mkdir bin{0:02g}".format(i))
-        os.system("mv TARGET* bin{0:02g}".format(i))
-        os.system("mv {0}_calibrated_uv.ms bin{1:02g}".format(tdata[i], i))
-        os.system("mv casa*.log bin{0:02g}".format(i))
+        os.system("~/difx/sites/ASKAP/calibrateFRB.py -c {0} -t {1}{2}.FITS {3} {4} -a {5} {6} --uvsrt {7} {8} {9} {10}".format(cdata, tdata[i], bins, cflag, tflag, avgchan, pcen, xpolmodel, noisecen, doimage, refant))
+        os.system("mkdir {0}bin{1:02g}".format(prefix, i))
+        os.system("mv TARGET* {0}bin{1:02g}".format(prefix, i))
+        if noisecen != '':
+            os.system("mv OFFSOURCE* {0}bin{1:02g}".format(prefix, i))
+        os.system("mv {0}{1}_calibrated_uv.ms {2}bin{3:02g}".format(tdata[i], bins, prefix, i))
+        os.system("mv casa*.log {0}bin{1:02g}".format(prefix, i))
+
