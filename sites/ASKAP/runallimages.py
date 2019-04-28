8a8f4855a828aea59080777ea52cbce5c523e9fd
XhrisPhillips <Chris.Phillips@csiro.au>
2019-04-28 22:28:07 +0000

    Fixed bug in file naming scheme
    git-svn-id: https://svn.atnf.csiro.au/difx@8876 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- sites/ASKAP/runallimages.py
+++ sites/ASKAP/runallimages.py
@@ -111,9 +111,9 @@ for i in range(nbins):
             refant = "--refant=" + str(args.refant)
         else: refant = ""
         
-        print "~/difx/sites/ASKAP/calibrateFRB.py -c {0} -t {1}{2}.FITS {3} {4} -a {5} {6} --uvsrt {7} {8} {9} {10}".format(cdata, tdata[i], bins, cflag, tflag, avgchan, pcen, xpolmodel, noisecen, doimage, refant)
+        print "~/difx/sites/ASKAP/calibrateFRB.py -c {0} -t {1}.FITS {2} {3} -a {4} {5} --uvsrt {6} {7} {8} {9}".format(cdata, tdata[i], cflag, tflag, avgchan, pcen, xpolmodel, noisecen, doimage, refant)
         os.system("rm -rf {0}_calibrated_uv.ms".format(cdatms))
-        os.system("~/difx/sites/ASKAP/calibrateFRB.py -c {0} -t {1}{2}.FITS {3} {4} -a {5} {6} --uvsrt {7} {8} {9} {10}".format(cdata, tdata[i], bins, cflag, tflag, avgchan, pcen, xpolmodel, noisecen, doimage, refant))
+        os.system("~/difx/sites/ASKAP/calibrateFRB.py -c {0} -t {1}.FITS {2} {3} -a {4} {5} --uvsrt {6} {7} {8} {9}".format(cdata, tdata[i], cflag, tflag, avgchan, pcen, xpolmodel, noisecen, doimage, refant))
         os.system("mkdir {0}bin{1:02g}".format(prefix, i))
         os.system("mv TARGET* {0}bin{1:02g}".format(prefix, i))
         if noisecen != '':
