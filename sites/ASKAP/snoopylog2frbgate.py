6f984dc115351ffc1584cdcfd67bbbe2776734bb
adamdeller <adeller@swin.edu.au>
2019-07-02 07:01:38 +0000

    Updates to snoopylog2frbgate, now makes multiple binconfigs
    git-svn-id: https://svn.atnf.csiro.au/difx@9039 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- sites/ASKAP/snoopylog2frbgate.py
+++ sites/ASKAP/snoopylog2frbgate.py
@@ -3,10 +3,20 @@ import os,sys,argparse
 
 parser = argparse.ArgumentParser(description='Turn a snoopy log into a binconfig and polyco for DiFX.')
 parser.add_argument('snoopylog', metavar='S', help='The snoopy log file')
-parser.add_argument("-f", "--freq", type=float, default=1152.0, help="The reference freq at which snoopy DM was calculated")
-parser.add_argument("--timediff", type=float, default=0, help="The time difference between the VCRAFT and snoopy log arrival times for the pulse, including geometric delay, in ms")
+parser.add_argument("-f", "--freq", type=float, default=-1, help="The reference freq at which snoopy DM was calculated")
+parser.add_argument("--timediff", type=float, default=-99999, help="The time difference between the VCRAFT and snoopy log arrival times for the pulse, including geometric delay, in ms")
 args = parser.parse_args()
 inttime = 10 # seconds
+
+if args.freq < 0:
+    print "You have to supply a frequency for the snoopy files! Don't be lazy, that's how accidents happen."
+    sys.exit()
+
+if args.timediff < -10000:
+    print "You have to specify a timediff! It should just be the geometric delay from ASKAP to the geocentre, "
+    print "now everything has been fixed.  Don't be lazy, that's how accidents happen."
+    sys.exit()
+timediffsec = args.timediff/1000.0
 
 with open(args.snoopylog) as snoopyin:
     snoopylines = snoopyin.readlines()
@@ -20,38 +30,113 @@ if len(nocommentlines) != 1:
     print "ERROR: No information found"
     sys.exit()
 splitline = nocommentlines[0].split()
-pulsewidth = float(splitline[3])*1.7 # filterbank width = 1.7ms
+pulsewidthms = float(splitline[3])*1.7 # filterbank width = 1.7ms
 dm = float(splitline[5])
 mjd = float(splitline[7])
     
-startmjd = mjd - inttime*pulsewidth/(2*86400000.0)
-nearestmjdstartsecond = int((startmjd - int(startmjd))*86400)
-fractionalsecond = (mjd - int(mjd))*86400 - nearestmjdstartsecond
-startphase = (fractionalsecond - pulsewidth/2000.0)/inttime # pulse width is in samples, which are 1.7ms
-endphase = (fractionalsecond + pulsewidth/2000.0)/inttime # pulse width is in samples, which are 1.7ms
+# Figure out when to start the polyco (go back by somewhere in the range 1-2 seconds, to an integer second boundary)
+polycorefmjdint = int(mjd)
+polycorefseconds = int((mjd - int(mjd))*86400 - 1.0)
+if polycorefseconds < 0:
+    polycorefseconds += 86400
+    polycorefmjdint -= 1
+hh = polycorefseconds/3600
+mm = (polycorefseconds - hh*3600)/60
+ss = polycorefseconds - (hh*3600 + mm*60)
+polycorefmjdfloat = polycorefmjdint  + float(polycorefseconds)/86400.0
 
-print nearestmjdstartsecond, fractionalsecond
-hh = nearestmjdstartsecond/3600
-mm = (nearestmjdstartsecond - hh*3600)/60
-ss = nearestmjdstartsecond - (hh*3600 + mm*60)
-polycostartmjd = int(mjd) + float(nearestmjdstartsecond)/86400.0
+# Write out the polyco file
+with open("craftfrb.polyco", "w") as polycoout:
+    polycoout.write("fake+fake DD-MMM-YY %02d%02d%05.2f %.15f %.4f 0.0 0.0\n" % (hh,mm,ss,polycorefmjdfloat, dm))
+    polycoout.write("0.0 %.3f 0 100 3 %.3f\n" % (1.0/float(inttime), args.freq))
+    polycoout.write("0.00000000000000000E-99 0.00000000000000000E-99 0.00000000000000000E-99\n")
+    polycoout.close()
 
-phaseoffset = args.timediff/10000. # ms --> phase units given the 10s "period"
+# Now write out a binconfig for the gate
+gatestartmjd = mjd - pulsewidthms/(2*86400000.0) # pulse width is in ms at this point
+gateendmjd = gatestartmjd + pulsewidthms/86400000.0 # pulse width is in ms at this point
+gatestartphase = (86400.0*(gatestartmjd - polycorefmjdfloat) + timediffsec)/inttime
+gateendphase = (86400.0*(gateendmjd -  polycorefmjdfloat) + timediffsec)/inttime
 
-newstartphase = startphase+phaseoffset
-newendphase = endphase+phaseoffset
-
-# Will use this if/when we figure out the 50-70ms offset...
-with open("craftfrb.binconfig", "w") as binconfout:
+with open("craftfrb.gate.binconfig", "w") as binconfout:
     binconfout.write("NUM POLYCO FILES:   1\n")
-    binconfout.write("POLYCO FILE 0:      craftfrb.polyco\n")
+    binconfout.write("POLYCO FILE 0:      %s/craftfrb.polyco\n" % os.getcwd())
     binconfout.write("NUM PULSAR BINS:    2\n")
     binconfout.write("SCRUNCH OUTPUT:     TRUE\n")
-    binconfout.write("BIN PHASE END 0:    %.9f\n" % newstartphase)
+    binconfout.write("BIN PHASE END 0:    %.9f\n" % gatestartphase)
     binconfout.write("BIN WEIGHT 0:       0.0\n")
-    binconfout.write("BIN PHASE END 1:    %.9f\n" % newendphase)
+    binconfout.write("BIN PHASE END 1:    %.9f\n" % gateendphase)
     binconfout.write("BIN WEIGHT 1:       1.0\n")
     binconfout.close()
+
+# Make an RFI binconfig
+rfistartphase1 = gatestartphase - 0.02/inttime # RFI gate (early side) starts 20ms before the start of the pulse
+rfiendphase1 = gatestartphase - 0.004/inttime # RFI gate (early side) ends 4ms before the start of the pulse
+rfistartphase2 = gateendphase + 0.004/inttime # RFI gate (late side) starts 4ms after the end of the pulse
+rfiendphase2 = gateendphase + 0.02/inttime # RFI gate (early side) ends 20ms after the end of the pulse
+
+with open("craftfrb.rfi.binconfig", "w") as binconfout:
+    binconfout.write("NUM POLYCO FILES:   1\n")
+    binconfout.write("POLYCO FILE 0:      %s/craftfrb.polyco\n" % os.getcwd())
+    binconfout.write("NUM PULSAR BINS:    4\n")
+    binconfout.write("SCRUNCH OUTPUT:     TRUE\n")
+    binconfout.write("BIN PHASE END 0:    %.9f\n" % rfistartphase1)
+    binconfout.write("BIN WEIGHT 0:       0.0\n")
+    binconfout.write("BIN PHASE END 1:    %.9f\n" % rfiendphase1)
+    binconfout.write("BIN WEIGHT 1:       1.0\n")
+    binconfout.write("BIN PHASE END 2:    %.9f\n" % rfistartphase2)
+    binconfout.write("BIN WEIGHT 2:       0.0\n")
+    binconfout.write("BIN PHASE END 3:    %.9f\n" % rfiendphase2)
+    binconfout.write("BIN WEIGHT 3:       1.0\n")
+    binconfout.close()
+
+# And make a high time resolution binconfig (216 microsec x bin width + 2ms either side)
+binstartmjd = mjd - inttime*pulsewidthms/(2*86400000.0) # pulse width is in ms at this point
+gateendmjd = gatestartmjd + pulsewidthms/86400000.0 # pulse width is in ms at this point
+binmicrosec = 216
+extrawidth = 2 # ms on either side of the snoopy detected pulse
+binstartphase = gatestartphase - float(extrawidth)/(1000.0*inttime)
+bindeltaphase = binmicrosec/(inttime*1e6)
+numbins = int((pulsewidthms + 2*extrawidth)/(binmicrosec/1000.0))
+with open("craftfrb.bin.binconfig", "w") as binconfout:
+    binconfout.write("NUM POLYCO FILES:   1\n")
+    binconfout.write("POLYCO FILE 0:      %s/craftfrb.polyco\n" % os.getcwd())
+    binconfout.write("NUM PULSAR BINS:    %d\n" % numbins)
+    binconfout.write("SCRUNCH OUTPUT:     FALSE\n")
+    for i in range(numbins+1):
+        phasestr = ("BIN PHASE END %d:" % i).ljust(20)
+        weightstr = ("BIN WEIGHT %d:" % i).ljust(20)
+        binconfout.write("%s%.9f\n" % (phasestr, binstartphase + i*bindeltaphase))
+        binconfout.write("%s1.0\n" % (weightstr))
+    binconfout.close()
+binscale = bindeltaphase / (rfiendphase2 + rfiendphase1 - rfistartphase2 - rfistartphase1)
+
+# And also make a "finders" binconfig, with just 5 bins spanning from the end of RFI window 1
+# through to the start of RFI window 2 (which will then normally be 2-3 ms wide each
+numifinderbins = 5
+bindeltaphase = (rfistartphase2 - rfiendphase1)/numfinderbins
+with open("craftfrb.finder.binconfig", "w") as binconfout:
+    binconfout.write("NUM POLYCO FILES:   1\n")
+    binconfout.write("POLYCO FILE 0:      %s/craftfrb.polyco\n" % os.getcwd())
+    binconfout.write("NUM PULSAR BINS:    %d\n" % numbins)
+    binconfout.write("SCRUNCH OUTPUT:     TRUE\n")
+    for i in range(numfinderbins+1):
+        phasestr = ("BIN PHASE END %d:" % i).ljust(20)
+        weightstr = ("BIN WEIGHT %d:" % i).ljust(20)
+        binconfout.write("%s%.9f\n" % (phasestr, binstartphase + i*bindeltaphase))
+        binconfout.write("%s1.0\n" % (weightstr))
+    binconfout.close()
+finderbinscale = bindeltaphase / (rfiendphase2 + rfiendphase1 - rfistartphase2 - rfistartphase1)
+
+# And write out a little script ready to do the various subtractions
+gatescale = (gateendphase - gatestartphase) / (rfiendphase2 + rfiendphase1 - rfistartphase2 - rfistartphase1)
+with open("dosubtractions.sh", "w") as subout:
+    subout.write("uvsubScaled.py FRB_GATE.FITS FRB_RFI.FITS %.9f\n" % (gatescale))
+    for i in range(numbins):
+        subout.write("uvsubScaled.py FRB_BIN%02d.FITS FRB_RFI.FITS %.9f\n" % (binscale))
+    for i in range(numfinderbins):
+        subout.write("uvsubScaled.py FRB_FINDERBIN%02d.FITS FRB_RFI.FITS %.9f\n" % (finderbinscale))
+    subout.close()
 
 # Instead cover a 150ms range with bins of size 5ms
 #with open("craftfrb.binconfig", "w") as binconfout:
@@ -65,11 +150,3 @@ with open("craftfrb.binconfig", "w") as binconfout:
 #        binconfout.write("%s%.9f\n" % (phasestr, startphase + (i-14)*0.0005))
 #        binconfout.write("%s1.0\n" % (weightstr))
 #    binconfout.close()
-
-
-
-with open("craftfrb.polyco", "w") as polycoout:
-    polycoout.write("fake+fake DD-MMM-12 %02d%02d%05.2f %.15f %.4f 0.0 0.0\n" % (hh,mm,ss,polycostartmjd, dm))
-    polycoout.write("0.0 %.3f 0 100 3 %.3f\n" % (1.0/float(inttime), args.freq))
-    polycoout.write("0.00000000000000000E-99 0.00000000000000000E-99 0.00000000000000000E-99\n")
-    polycoout.close()
