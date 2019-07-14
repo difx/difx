b378f032a3e529e3f5235228882302201e42868c
adamdeller <adeller@swin.edu.au>
2019-07-14 23:32:50 +0000

    added new tool for estimating time offsets
    git-svn-id: https://svn.atnf.csiro.au/difx@9050 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

---
+++ sites/ASKAP/getGeocentricDelay.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python2
+import math, sys, glob, os
+import astropy.units as u
+from astropy.time import Time
+from astropy.coordinates import SkyCoord, EarthLocation, AltAz
+
+askaplat = -26.697
+askaplon = 116.631
+askapheight = 361 # metres
+askapradius = 6374217 # metres from geocentre
+c = 299792458.0
+
+if not len(sys.argv) == 2:
+    print "Usage: %s <rawdata directory>" % sys.argv[0]
+    print "Raw data directory should contain snoopy.log and akXX/beamXX/*.vcraft.hdr files"
+    sys.exit()
+
+# Grab the snoopy file
+snoopyfile = sys.argv[1] + "/snoopy.log"
+if not os.path.exists(snoopyfile):
+    print snoopyfile, "doesn't exist"
+    sys.exit()
+
+# Parse the snoopy file
+with open(snoopyfile) as snoopyin:
+    snoopylines = snoopyin.readlines()
+nocommentlines = []
+for line in snoopylines:
+    print line
+    if len(line) > 1 and not line[0] =="#":
+        nocommentlines.append(line)
+        print "Snoopy info", nocommentlines
+if len(nocommentlines) != 1:
+    print "ERROR: No information found"
+    sys.exit()
+splitline = nocommentlines[0].split()
+mjd = float(splitline[7])
+
+
+# Grab all the headers and find the lowest frequency and the rough FRB direction
+antennas = sorted(glob.glob(sys.argv[1] + '/ak??/'))
+if len(antennas) == 0:
+    print "No antennas found!"
+    sys.exit()
+beams = glob.glob(antennas[0] + '/beam??/')
+if len(beams) == 0:
+    print "No beams found in", antennas[0], "(in /beam??/)"
+    sys.exit()
+hdrfiles = glob.glob(beams[0] + '/*hdr')
+if len(hdrfiles) == 0:
+    print "No vcraft header files found!"
+    sys.exit()
+lowestfreq = 99999999
+for hf in hdrfiles:
+    with open(hf) as headerin:
+        lines = headerin.readlines()
+        for line in lines:
+            if "FREQ" in line:
+                freqs = line.split()[1].split(',')
+                for f in freqs:
+                    if float(f) < lowestfreq:
+                        lowestfreq = float(f)
+            if "BEAM_RA" in line:
+                beamra = float(line.split()[1])
+            if "BEAM_DEC" in line:
+                beamdec = float(line.split()[1])
+            if "TRIGGER_MJD" in line:
+                triggermjd = float(line.split()[1])
+            #if "ANT_EL" in line:
+            #    ant_el_rad = float(line.split()[1])*math.pi/180.0
+
+frb = SkyCoord(beamra, beamdec, unit='deg')
+askap = EarthLocation(lon=askaplon*u.deg, lat=askaplat*u.deg, height=askapheight*u.m)
+t = Time(triggermjd, format='mjd')
+altaz = frb.transform_to(AltAz(obstime=t,location=askap))
+geocentricdelay = math.sin(altaz.alt.radian) * askapradius / c
+
+# Would be much simpler to just get the elevation from the vcraft file!!
+# But this is for the centre of the PAF, so could be a bit off vs the elevation of the actual beam
+# (which is itself already a bit off vs the actual FRB, but within half a degree which is good enough)
+#geocentricdelay = math.sin(ant_el_rad) * askapradius / c
+
+print "Geocentric delay is", geocentricdelay
+print "Lowest frequency is", lowestfreq
+
+print "snoopylog2frbgate.py -f %.3f --timediff %.3f %s" % (lowestfreq, geocentricdelay*1e3, snoopyfile)
