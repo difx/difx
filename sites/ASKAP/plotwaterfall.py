09542e04e76654ffaa25bfdf864649d86bb370ad
CherieDay <cday@swin.edu.au>
2019-11-19 05:56:17 +0000

    Added arguments for polarisations and number of channels averaged; added in color specification for each Stokes parameter
    git-svn-id: https://svn.atnf.csiro.au/difx@9324 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- sites/ASKAP/plotwaterfall.py
+++ sites/ASKAP/plotwaterfall.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
 import os, sys,argparse
 
 parser = argparse.ArgumentParser()
@@ -12,6 +13,8 @@ parser.add_argument("-r", "--res", type=float, default=None, help="Temporal reso
 parser.add_argument("-z", "--zero", default=False, help="Set zeroth bin equal to zero; use for nbins>1 when the zeroth bin contains little to no signal but mostly noise", action="store_true")
 parser.add_argument("-f", "--basefreq", type=float, default=None, help="The lowest frequency in the observation in MHz")
 parser.add_argument("-F", "--fscrunch", default=False, help="Make fscrunched plot", action="store_true")
+parser.add_argument("--pols", type=str, default="XX,YY,I,Q,U,V", help='The polarisations to be imaged if --imagecube is set. Defaulted to all. Input as a list of strings: e.g., "XX,YY"')
+parser.add_argument("-a", "--avg", type=int, default=24, help="Number of channels to average together per image cube slice")
 
 args = parser.parse_args()
 
@@ -43,7 +46,7 @@ res = args.res
 
 # Define dynamic spectra parameters
 basefreq = args.basefreq
-bandwidth = 336
+bandwidth = args.avg/6 * nchan # MHz
 startchan = 0
 endchan=nchan-3
 
@@ -55,7 +58,10 @@ endtime = nbins*res
 dynspec = {}
 fscrunch = {}
 
-for stokes in ["I","Q","U","V","XX","YY"]:
+# Change global font size
+matplotlib.rcParams.update({'font.size': 12})
+
+for stokes in args.pols.split(','):
 
     # Set figure size
     fig, ax = plt.subplots(figsize=(6,7))
@@ -77,29 +83,54 @@ for stokes in ["I","Q","U","V","XX","YY"]:
     else:
         if args.zero:
             dynspec[stokes][0] = 0
+#            dynspec[stokes][1] = 0
             print "Setting zeroth input bin equal to zero"
         else: print "Plotting all bins"
 
         if args.fscrunch:
             fscrunch[stokes] = np.sum(dynspec[stokes], 1)
-        
+            np.savetxt("{0}-imageplane-fscrunch-spectrum.stokes{1}.txt".format(src, stokes), fscrunch[stokes])
 
         ax.imshow(dynspec[stokes][:,startchan:endchan].transpose(), cmap=plt.cm.plasma, interpolation='none', extent=[starttime,endtime,endfreq,startfreq], aspect='auto')
-        #ax.set_aspect(0.03) # you may also use am.imshow(..., aspect="auto") to restore the aspect ratio
+        ax.set_aspect(0.003) # you may also use am.imshow(..., aspect="auto") to restore the aspect ratio
         ax.set_xlabel("Time (ms)")
         ax.set_ylabel("Frequency (MHz)")
-        plt.savefig('{0}-imageplane-dynspectrum.stokes{1}.png'.format(src, stokes))
+        plt.savefig('{0}-imageplane-dynspectrum.stokes{1}.png'.format(src, stokes), bbox_inches = 'tight')
         plt.clf()
 
 if args.fscrunch:
+
+    # Set figure size
+    fig, ax = plt.subplots(figsize=(8,7))
+    plt.title("Frequency averaged")
+
     print "fscrunching..."
     centretimes = np.arange(starttime+res, endtime+res, res)
-    plt.plot(centretimes,fscrunch["I"][:],label="I")
-    plt.plot(centretimes,fscrunch["Q"][:],label="Q")
-    plt.plot(centretimes,fscrunch["U"][:],label="U")
-    plt.plot(centretimes,fscrunch["V"][:],label="V")
-    plt.legend()
-    plt.xlabel("Time (ms)")
-    plt.ylabel("Amplitude (arbitrary units)")
-    plt.savefig("{0}-fscrunch.png".format(src))
+    for stokes in args.pols.split(','):
+
+        if stokes=="I": 
+            col='k'
+            plotlinestyle=':'
+        elif stokes=="Q": 
+            col='cyan'
+            plotlinestyle='-'
+        elif stokes=="U": 
+            col='magenta'
+            plotlinestyle='-.'
+        elif stokes=="V": 
+            col='r'
+            plotlinestyle='--'
+        elif stokes=="YY": 
+            col='orange'
+            plotlinestyle='-'
+        else: 
+            col='yellow'
+            plotlinestyle='--'
+    
+        amp_jy = fscrunch[stokes][:] * 0.01/res
+        ax.plot(centretimes,amp_jy, label=stokes, color=col, linestyle=plotlinestyle)
+        ax.legend()
+        ax.set_xlabel("Time (ms)")
+        ax.set_ylabel("Amplitude (Jy)")
+        plt.savefig("{0}-fscrunch.png".format(src), bbox_inches = 'tight')
 else: print "Done!"
