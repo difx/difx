285f9d766e4f2f431688da7cb1954a23928c3b36
walterfb <wbrisken@gmail.com>
2020-01-17 21:36:55 +0000

    update antenna location table
    git-svn-id: https://svn.atnf.csiro.au/difx@9405 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- libraries/difxio/trunk/tests/testantdb.c
+++ libraries/difxio/trunk/tests/testantdb.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 #include <difxio/antenna_db.h>
+
+enum PrintFormat
+{
+	PrintFormatHuman = 0,
+	PrintFormatMachine,
+	PrintFormatCSV
+};
 
 void findxyz(double x, double y, double z)
 {
@@ -17,9 +25,27 @@ void findxyz(double x, double y, double z)
 	}
 }
 
-int main()
+int main(int argc, char **argv)
 {
 	unsigned int i;
+	int a;
+	int format=PrintFormatHuman;
+
+	for(a = 1; a < argc; ++a)
+	{
+		if(strcmp(argv[a], "--human") == 0)
+		{
+			format = PrintFormatHuman;
+		}
+		else if(strcmp(argv[a], "-m") == 0 || strcmp(argv[a], "--machine") == 0)
+		{
+			format = PrintFormatMachine;
+		}
+		else if(strcmp(argv[a], "-c") == 0 || strcmp(argv[a], "--csv") == 0)
+		{
+			format = PrintFormatCSV;
+		}
+	}
 
 	for(i = 0; ; ++i)
 	{
@@ -31,11 +57,33 @@ int main()
 			break;
 		}
 
-		fprintAntennaDBEntry(stdout, ae);
+		if(format == PrintFormatHuman)
+		{
+			fprintAntennaDBEntry(stdout, ae);
+		}
+		else if(format == PrintFormatMachine)
+		{
+			double lat, lon, alt; /* rad, rad, m */
+
+			ecef2lla(&lat, &lon, &alt, ae->x, ae->y, ae->z);
+
+			printf("%5.3f %5.3f %5.3f  %5.6f %5.6f %5.3f  %5.2f # %s %s\n", ae->x, ae->y, ae->z, lat, lon, alt, ae->diameter, ae->name, ae->ivsName);
+		}
+		else if(format == PrintFormatMachine)
+		{
+			double lat, lon, alt; /* rad, rad, m */
+
+			ecef2lla(&lat, &lon, &alt, ae->x, ae->y, ae->z);
+
+			printf("%s,%s,%5.3f,%5.3f,%5.3f,%5.6f,%5.6f,%5.3f,%5.2f\n", ae->name, ae->ivsName, ae->x, ae->y, ae->z, lat, lon, alt, ae->diameter);
+		}
 	}
 
-	findxyz(-2388896.2, 5043350.1, -3078590.8);
-	findxyz(2388896.2, 5043350.1, 3078590.8);
+	if(format == PrintFormatHuman)
+	{
+		findxyz(-2388896.2, 5043350.1, -3078590.8);
+		findxyz(2388896.2, 5043350.1, 3078590.8);
+	}
 
 	return 0;
 }
