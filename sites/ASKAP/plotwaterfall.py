Fixed sign of RM in position angle correction
git-svn-id: https://svn.atnf.csiro.au/difx@9409 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

sites/ASKAP/plotwaterfall.py
@@ -184,7 +184,7 @@ lambda_sq = (c/(freqs*1e6))**2
 
 # Derotating using the measured RM 
 delta_pa = rotmeas * lambda_sq
-pa_corrected = pa - delta_pa
+pa_corrected = pa + delta_pa
 
 # Apply corrections to Stokes Q and U
 dynspec_rmcorrected["Q"] = p_qu * np.cos(2*pa_corrected)