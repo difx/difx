c7c58b84962cd7979e3fc04573bf2886b33ae2cc
CherieDay <cday@swin.edu.au>
2020-01-27 23:28:56 +0000

    Updated to CASA 5.6.2
    git-svn-id: https://svn.atnf.csiro.au/difx@9416 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- sites/ASKAP/runloadtarget.sh
+++ sites/ASKAP/runloadtarget.sh
@@ -3,6 +3,6 @@
 # Script to run the CASA call for converting the UVFITS file of the calibrator and target data to CASA Measurement Sets
 
 module purge
-module load casa/5.3.0
+module load casa/5.6.2
 
 casa --nologger -c loadtarget.py
