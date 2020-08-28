6338f09c92fd45e0cd6049079ed8d20f87049817
jwagnerhki <jan.wagner@iki.fi>
2020-08-28 07:27:43 +0000

    add copies of MPIfR simple parallelized fourfit'ting scripts
    git-svn-id: https://svn.atnf.csiro.au/difx@9685 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

---
+++ sites/MPIfR/hops/parFourfitSt.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+#
+# parFourfitSt.sh <fourfit control file> <station 1-letter ID> <scan1> [<scan2> <scan3> ...]
+#
+# Runs several fourfit instances in parallel, per baseline, for each
+# of the given scan(s). Fringe fits only the baselines to one particular station,
+# and excludes autocorrelations. The scripts waits if more than 20 fourfit instances
+# are active on the computer.
+#
+# Example: parFourfitSt.sh cf_eht A 1234/100-0243 1234/100-0517 1234/100-0525
+#
+# Optionally define env var FF_EXTRA_ARGS with fourfit arguments (e.g. EXTRA_ARGS=-PXL),
+# or FF_POST_ARGS with commands to append to fourfit (e.g. FF_POST_ARGS="set freqs a").
+
+set -m
+
+MAX_FF_INSTANCES=20
+
+if [ "$1" == "" ]; then
+	echo "Usage: parFourfitSt.sh <fourfit control file> <station 1-letter ID> <scan1> [<scan2> <scan3> ...]"
+fi
+
+for indir in "${@:3}"; do
+	echo $indir
+	rm -f ./$indir/*.lock
+
+        blines=()
+	for visfile in `ls -1 ./$indir/??..*`; do
+		bl=${visfile##*/}
+		bl=${bl%..*}
+		if [ "${bl:0:1}" == "${bl:1:1}" ]; then
+			# skip autocorrs and auto cross-pols?
+			continue  # to skip
+			#echo     # to keep
+		fi
+		if [ "${bl:0:1}" == "$2" ]; then
+			blines+=("$bl")
+		fi
+		if [ "${bl:1:1}" == "$2" ]; then
+			blines+=("$bl")
+		fi
+		# echo ${visfile##*/}  $bl
+	done
+
+	for bl in "${blines[@]}"; do
+		if compgen -G "./$indir/$bl..*" > /dev/null; then
+                        solcount=`find ./$indir/ -name "$bl.[A-Z].*" | wc -l`
+                        if [ "$solcount" != "4" ] && [ "$solcount" != "2" ]; then
+                                echo "Interrupted fourfit in scan $indir baseline $bl, got $solcount instead of 4 solutions, redoing!"
+                                rm -f ./$indir/$bl.?.*
+                        fi
+			if compgen -G "./$indir/$bl.[A-Z].*" > /dev/null; then
+				echo "Scan $indir baseline $bl fourfit already done"
+			else
+				export cnt=`ps axuf | grep fourfit | wc -l`
+				while (( $cnt > $MAX_FF_INSTANCES )); do
+					echo "Waiting $indir baseline $bl : still too many ($cnt > 20) fourfit processes running..."
+					sleep 2
+					export cnt=`ps axuf | grep fourfit | wc -l`
+				done
+				echo "Scan $indir baseline $bl : fourfit"
+				echo "fourfit -m1 -c $1 -b$bl $FF_EXTRA_ARGS -u $indir $FF_POST_ARGS"
+				# fourfit -m1 -c $1 -b$bl $FF_EXTRA_ARGS -u $indir $FF_POST_ARGS &
+				fourfit -m1 -c $1 -b$bl $FF_EXTRA_ARGS -u $indir $FF_POST_ARGS 2>&1 | grep 'sbd\|SNR' &
+				err=$?
+				if [  "$err" != "0" ]; then
+					echo "Fourfit returned error $err! Stopping."
+					exit $err
+				fi
+			fi
+		fi
+	done
+	wait
+done
+
