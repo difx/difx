c4b78343e0c1a688f414807f33d3265525a5c2f2
jwagnerhki <jan.wagner@iki.fi>
2020-08-28 08:27:27 +0000

    update docu of difxCheckMk6Modulepresence.py, change to python3, extend availability check also to etransferred files
    git-svn-id: https://svn.atnf.csiro.au/difx@9687 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- sites/MPIfR/mark6/difxCheckMk6Modulepresence.py
+++ sites/MPIfR/mark6/difxCheckMk6Modulepresence.py
@@ -1,29 +1,55 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 '''
 Usage: difxCheckMk6Modulepresence.py <difxjob_0001.input> [<difxjob_0002.input> ...]
 
-Looks at the Mark6 recordings listed in the DiFX .input file(s) and for files that
-match the pattern "/mark6-<nr>_fuse/<rest of path>.vdif" attempts to log in on
-the respective Mark6 and checks if the scan is really available.
+Verifies that any files referenced by the DiFX jobs are indeed available.
+
+Files that match the pattern "/mark6-<nr>_fuse/<rest of path>.vdif" are
+considered to be part of Mark6 FUSE file based correlation. Accessibility
+of these Mark6 FUSE files is verified via SSH login and a file size check.
+
+Files that do not match the above pattern are considered to be e-transferred
+or copied Mark6 VDIF files that reside on the cluster file system. Accessibility
+in this case is verified by a file size check done on the local host.
+
+(C) 2020 Jan Wagner
 '''
 
-import re, sys, commands, subprocess
+# TODO: add MSN-based lookup once libmark6gather & .input support Mark6 MSNs,
+#       the latter being critical (and ToDo...) to restrict the slots that libmark6gather collects data from
 
-def check_inputfile(fn):
-	print ('File %s : ' % (fn))
-	with open(fn, 'r') as f:
+import subprocess
+import sys
+
+def checkDatastreamAvailabilities(difxinputfile, checkLocalFiles=True):
+	'''
+	Collect all DATASTREAM entries from an .input file and check for availability of the
+	referenced Mark6 FUSE files via SSH to the respective hosts on the MPIfR VLBI cluster.
+	Optionally look up and check availability of non-Mark6 DATASTREAM files on BeeGFS.
+	'''
+
+	print ('Job %s : ' % (difxinputfile))
+
+	with open(difxinputfile, 'r') as f:
+
 		datastreamfiles = []
+		clusterfsfiles = []
+
 		for line in f:
 			if not 'FILE ' in line: continue
 			dstreamfile = line[20:].strip()
-			if not '/mark6-' in dstreamfile: continue
-			datastreamfiles.append(dstreamfile)
+			if not '/mark6-' in dstreamfile:
+				clusterfsfiles.append(dstreamfile)
+			else:
+				datastreamfiles.append(dstreamfile)
+
 		spawned = []
 		for dstreamfile in datastreamfiles:
 			host = dstreamfile[1:].split('/')[0]
 			host = host.split('_fuse')[0]
 			p = subprocess.Popen(["/usr/bin/ssh", host, "/usr/bin/du", "-h", dstreamfile], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 			spawned.append(p)
+
 		for i in range(len(datastreamfiles)):
 			(st,rc) = spawned[i].communicate()
 			dstreamfile = datastreamfiles[i]		
@@ -31,18 +57,25 @@ def check_inputfile(fn):
 			host = host.split('_fuse')[0]
 			# print (host, st)
 			if len(st)<1:
-				print('%s %s : missing ' % (host,dstreamfile))
+				print('%s %s : MISSING' % (host,dstreamfile))
 			else:
-				print('%s %s : ok ' % (host,dstreamfile))
+				print('%s %s : ok' % (host,dstreamfile))
+
+		if checkLocalFiles:
+			for clusterfsfile in clusterfsfiles:
+				p = subprocess.Popen(["/usr/bin/du", "-h", clusterfsfile], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+				(st,rc) = p.communicate()
+				if len(st)<1:
+					print('%s %s : MISSING' % ("localhost",clusterfsfile))
+				else:
+					print('%s %s : ok' % ("localhost",clusterfsfile))
 
 
-if '--help' in sys.argv:
-	print(__doc__)
-	sys.exit(0)
+if __name__ == "__main__":
 
-if '-h' in sys.argv:
-	print(__doc__)
-	sys.exit(0)
+	if '--help' in sys.argv or '-h' in sys.argv:
+		print(__doc__)
+		sys.exit(0)
 
-for fn in sys.argv[1:]:
-	check_inputfile(fn)
+	for difxinputfile in sys.argv[1:]:
+		checkDatastreamAvailabilities(difxinputfile)
