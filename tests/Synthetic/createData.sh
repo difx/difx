eca71dfae19bef6bb24dc9fc6784f20d3be227ee
XhrisPhillips <Chris.Phillips@csiro.au>
2020-11-24 04:44:46 +0000

    Added optional TaskSpooler usage
    git-svn-id: https://svn.atnf.csiro.au/difx@9834 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- tests/Synthetic/createData.sh
+++ tests/Synthetic/createData.sh
@@ -1,29 +1,41 @@
 #!/usr/bin/sh
 
+TSP=""
+TSP_SLOTS=9
+if command -v tsp > /dev/null 2>&1
+then
+    echo "Using TaskSpooler to parallelise"
+    tsp -S $TSP_SLOTS
+    TSP="tsp"
+fi
 
-export DURATION=1200
+export DURATION=100
 
 export SEED1=38573
 export SEED2=58573
 
-
 # USB Real 
-generateVDIF -seed=$SEED1 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.5 -dayno 100 -time 07:00:00 TEST1.vdif
-generateVDIF -seed=$SEED2 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.0 -dayno 100 -time 07:00:00 TEST2-usb.vdif
+$TSP generateVDIF -seed=$SEED1 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.5 -dayno 100 -time 07:00:00 TEST1.vdif
+$TSP generateVDIF -seed=$SEED2 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.0 -dayno 100 -time 07:00:00 TEST2-usb.vdif
 
 # LSB Real
-generateVDIF -seed=$SEED2 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.0 -dayno 100 -time 07:00:00 -lsb TEST2-lsb.vdif
+$TSP generateVDIF -seed=$SEED2 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.0 -dayno 100 -time 07:00:00 -lsb TEST2-lsb.vdif
 
 # Complex (single side band)
 
-generateVDIF -seed=$SEED1 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.5 -dayno 100 -time 07:00:00 -complex      TEST1-complex-usb.vdif
-generateVDIF -seed=$SEED2 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.0 -dayno 100 -time 07:00:00 -hilbert      TEST2-complex-usb.vdif
-generateVDIF -seed=$SEED2 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.0 -dayno 100 -time 07:00:00 -hilbert      TEST2-complex-usb.vdif
-generateVDIF -seed=$SEED2 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.0 -dayno 100 -time 07:00:00 -hilbert -lsb TEST2-complex-lsb.vdif
+$TSP generateVDIF -seed=$SEED1 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.5 -dayno 100 -time 07:00:00 -complex      TEST1-complex-usb.vdif
+$TSP generateVDIF -seed=$SEED2 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.0 -dayno 100 -time 07:00:00 -hilbert      TEST2-complex-usb.vdif
+$TSP generateVDIF -seed=$SEED2 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.0 -dayno 100 -time 07:00:00 -hilbert      TEST2-complex-usb.vdif
+$TSP generateVDIF -seed=$SEED2 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.0 -dayno 100 -time 07:00:00 -hilbert -lsb TEST2-complex-lsb.vdif
 
 # Complex (double side band)
 
-generateVDIF -seed=$SEED2 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.0 -dayno 100 -time 07:00:00 -hilbert -doublesideband      TEST2-dsb-usb.vdif
-generateVDIF -seed=$SEED2 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.0 -dayno 100 -time 07:00:00 -hilbert -doublesideband -lsb TEST2-dsb-lsb.vdif
+$TSP generateVDIF -seed=$SEED2 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.0 -dayno 100 -time 07:00:00 -hilbert -doublesideband      TEST2-dsb-usb.vdif
+$TSP generateVDIF -seed=$SEED2 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.0 -dayno 100 -time 07:00:00 -hilbert -doublesideband -lsb TEST2-dsb-lsb.vdif
 
 
+if command -v tsp > /dev/null 2>&1
+then
+    echo "Waiting for TaskSpooler to finish"
+    tsp -w
+fi
