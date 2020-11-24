33c79adbc331f109477d8d3c13959db8f47c1150
XhrisPhillips <Chris.Phillips@csiro.au>
2020-11-24 04:42:16 +0000

    A variety of complex USB/LSB etc correlations
    git-svn-id: https://svn.atnf.csiro.au/difx@9832 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

---
+++ tests/Synthetic/run-lsb.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+export EXPER=test-lsb
+
+vex2difx ${EXPER}.v2d
+difxcalc ${EXPER}.calc
+
+\rm -rf ${EXPER}.difx
+mpirun -machinefile machines -np 4 mpifxcorr ${EXPER}.input
+
+difx2fits ${EXPER}
+
+\mv -f TEST.0.bin0000.source0000.FITS ${EXPER}.fits
