83a0beff126bd472dac1556cfa21cc5fbcf080ea
jwagnerhki <jan.wagner@iki.fi>
2021-01-29 12:25:26 +0000

    python3'ify printDiFXPCal.py, treat large tone indices as a request for specific frequencies
    git-svn-id: https://svn.atnf.csiro.au/difx@9897 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- utilities/trunk/vis2screen/printDiFXPCal.py
+++ utilities/trunk/vis2screen/printDiFXPCal.py
@@ -1,12 +1,14 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """
-printDiFXPCal.py version 1.0  Jan Wagner  20200813
+printDiFXPCal.py version 1.1  Jan Wagner  20210129
 
-Usage: printDiFXPCal.py <output_xxx.difx/PCAL_id_timestamp> [datastream ID number] [toneIdx,[toneIdx,...]]
+Usage: printDiFXPCal.py <output_xxx.difx/PCAL_id_timestamp> [datastream ID number] [tone,[tone,...]]
 
 Prints the content of the PCAL file in a tabular format,
 converting complex Re,Im data into polar Mag,Phase form.
 
+Use datastream ID of -1 to get a list of available datastreams.
+Tones can be indices (0,1,...,n-1) or frequencies in MHz.
 """
 
 import math, sys
@@ -41,6 +43,7 @@ def printTones(pcfile,datastreamId,toneIndices):
 	ds_dframe = df[df[PC_column_dsId] == datastreamId]
 	ds_num_channels = ds_dframe[PC_column_numChan].unique()
 	ds_tones_per_channel = ds_dframe.at[1, PC_column_numTones_ch0]
+	ds_MaxTones = max(ds_num_channels) * ds_tones_per_channel
 
 	col = PC_column_numTones_ch0 + 1
 	rows_total = df.shape[0]
@@ -48,6 +51,18 @@ def printTones(pcfile,datastreamId,toneIndices):
 	if toneIndices is None:
 		toneIndices = range(ds_num_channels*ds_tones_per_channel)
 
+	# Remove tone indices that aren't present in data.
+	# Reinterpret any large indices as a frequency (MHz) and try to find the corresponding tone.
+	toneIndices_ = []
+	toneFreqs = [float(ds_dframe.at[1, PC_column_numTones_ch0+1+4*nn]) for nn in range(ds_MaxTones)]
+	for nn in toneIndices:
+		if nn >= 0 and nn < ds_MaxTones:
+			toneIndices_.append(int(nn))
+		elif float(nn) in toneFreqs:
+			toneIndices_.append(toneFreqs.index(float(nn)))
+	toneIndices = toneIndices_
+
+	# Assemble the title line
 	headings = []
 	for nn in toneIndices:
 		row = 1
@@ -57,15 +72,16 @@ def printTones(pcfile,datastreamId,toneIndices):
 		headings.append('%.1f%s' % (freqMHz, bandCode))
 	print('%5s %s' % ('', ''.join(['%16s' % h for h in headings])))
 
+	# Show tone amp,phase over time
 	# while row in range(rows)
 	for row in range(1,rows_total):
-		print('%5d ' % (row)),
+		print('%5d ' % (row), end='')
 		for nn in toneIndices:
 			col = PC_column_numTones_ch0 + 1 + 4*nn
 			re = float(ds_dframe.at[row, col+2])
 			im = float(ds_dframe.at[row, col+3])
 			mag,phase = cart2pol(re, im)
-			print('%7.2f %+7.2f' % (mag, phase)),
+			print('%8.2f %+7.2f' % (mag, phase), end='')
 		print('')
 
 if __name__ == '__main__':
@@ -78,6 +94,6 @@ if __name__ == '__main__':
 		if len(sys.argv) > 2:
 			ds = int(sys.argv[2])
 		if len(sys.argv) > 3:
-			tones = [int(t) for t in sys.argv[3].split(',')]
+			tones = [float(t) for t in sys.argv[3].split(',')]
 	
 		printTones(pcalfile, ds, tones)
