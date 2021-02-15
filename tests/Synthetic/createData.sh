f40602d6d4cac39aaae5585dc876e2ed8418dcdc
adamdeller <adeller@swin.edu.au>
2021-02-15 10:31:16 +0000

    Set synthetic data year to 2020 to ensure a match with the correlation setup
    git-svn-id: https://svn.atnf.csiro.au/difx@9911 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- tests/Synthetic/createData.sh
+++ tests/Synthetic/createData.sh
@@ -9,29 +9,29 @@ then
     TSP="tsp"
 fi
 
-export DURATION=1200
+export DURATION=10
 
 export SEED1=38573
 export SEED2=58573
 
 # USB Real 
-$TSP generateVDIF -seed=$SEED1 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.5 -dayno 100 -time 07:00:00 TEST1.vdif
-$TSP generateVDIF -seed=$SEED2 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.0 -dayno 100 -time 07:00:00 TEST2-usb.vdif
+$TSP generateVDIF -seed=$SEED1 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.5 -year 2020 -dayno 100 -time 07:00:00 TEST1.vdif
+$TSP generateVDIF -seed=$SEED2 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.0 -year 2020 -dayno 100 -time 07:00:00 TEST2-usb.vdif
 
 # LSB Real
-$TSP generateVDIF -seed=$SEED2 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.0 -dayno 100 -time 07:00:00 -lsb TEST2-lsb.vdif
+$TSP generateVDIF -seed=$SEED2 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.0 -year 2020 -dayno 100 -time 07:00:00 -lsb TEST2-lsb.vdif
 
 # Complex (single side band)
 
-$TSP generateVDIF -seed=$SEED1 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.5 -dayno 100 -time 07:00:00 -complex      TEST1-complex-usb.vdif
-$TSP generateVDIF -seed=$SEED2 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.0 -dayno 100 -time 07:00:00 -hilbert      TEST2-complex-usb.vdif
-$TSP generateVDIF -seed=$SEED2 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.0 -dayno 100 -time 07:00:00 -hilbert      TEST2-complex-usb.vdif
-$TSP generateVDIF -seed=$SEED2 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.0 -dayno 100 -time 07:00:00 -hilbert -lsb TEST2-complex-lsb.vdif
+$TSP generateVDIF -seed=$SEED1 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.5 -year 2020 -dayno 100 -time 07:00:00 -complex      TEST1-complex-usb.vdif
+$TSP generateVDIF -seed=$SEED2 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.0 -year 2020 -dayno 100 -time 07:00:00 -hilbert      TEST2-complex-usb.vdif
+$TSP generateVDIF -seed=$SEED2 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.0 -year 2020 -dayno 100 -time 07:00:00 -hilbert      TEST2-complex-usb.vdif
+$TSP generateVDIF -seed=$SEED2 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.0 -year 2020 -dayno 100 -time 07:00:00 -hilbert -lsb TEST2-complex-lsb.vdif
 
 # Complex (double side band)
 
-$TSP generateVDIF -seed=$SEED2 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.0 -dayno 100 -time 07:00:00 -hilbert -doublesideband      TEST2-dsb-usb.vdif
-$TSP generateVDIF -seed=$SEED2 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.0 -dayno 100 -time 07:00:00 -hilbert -doublesideband -lsb TEST2-dsb-lsb.vdif
+$TSP generateVDIF -seed=$SEED2 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.0 -year 2020 -dayno 100 -time 07:00:00 -hilbert -doublesideband      TEST2-dsb-usb.vdif
+$TSP generateVDIF -seed=$SEED2 -w 4 -b 2 -C 1  -l ${DURATION} -noise -amp2 0.05 -tone2 1.0 -year 2020 -dayno 100 -time 07:00:00 -hilbert -doublesideband -lsb TEST2-dsb-lsb.vdif
 
 
 if command -v tsp > /dev/null 2>&1
