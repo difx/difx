468e21213f08ecb3122008ab6ec044923297b888
jwagnerhki <jan.wagner@iki.fi>
2021-04-12 20:42:26 +0000

    update noema-vex-defs.py to provide VEX chan_defs for all four recorders or subsets of that, and follow the PolyFix to Mark6 channel definitions of 202104_pNOEMA_VLBI_backend_5-9GHz_freq_setup.xlsx
    git-svn-id: https://svn.atnf.csiro.au/difx@9992 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- sites/MPIfR/noema/noema-vex-defs.py
+++ sites/MPIfR/noema/noema-vex-defs.py
@@ -5,85 +5,190 @@ NOEMA VEX helper tool
 Script to manufacture VEX entries for use with NOEMA data.
 Sky frequencies of filterbank channels are derived from 1st LO
 and 2nd LO information. Channels are always 64 MHz wide.
+
+The Mark6 recorder to PolyFix units and channels assignment
+of EHT 2021 is assumed, and follows R. Garcias spreadsheet
+202104_pNOEMA_VLBI_backend_5-9GHz_freq_setup.xlsx
 '''
 
 import argparse
 import sys
 
 __author__ = "Jan Wagner (MPIfR)"
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 
 def parse_args(args: []):
+
 	cmd='%(prog)s <options>'
+
 	parser = argparse.ArgumentParser(description=__doc__, add_help=True, formatter_class=argparse.RawDescriptionHelpFormatter)
 	parser.add_argument('--version', action='version', version='%(prog)s ' + __version__)
-	parser.add_argument('-F', '--lo1', dest='lo1', metavar='MHz', default='92101.0', help='frequency of 1st LO in MHz (default: %(default)s)')
-	parser.add_argument('-f', '--lo2', dest='lo2', metavar='MHz', default='7744.0', help='frequency of 2nd LO in MHz (default: %(default)s)')
-	parser.add_argument('-S', '--sb1', dest='sb1', metavar='U|L', default='L', help='sideband of 1st mixing (default: %(default)s)')
-	opts = parser.parse_args(args)
-	return opts
+	parser.add_argument('-f', '--lo1', dest='lo1', metavar='GHz', default='221.100', help='frequency of 1st LO (GMVA 92.101, EHT 221.100; default: %(default)s)')
+	parser.add_argument('-F', '--lo2', dest='lo2', metavar='GHz', default='7.744', help='frequency of 2nd LO (default: %(default)s)')
+	parser.add_argument('-r', dest='recorders', default='1,2,3,4', help='list of recorder ID numbers (default: %(default)s)')
+
+	return parser.parse_args(args)
 
 
-def sideband2int(sideband: str) -> int:
-	if sideband.lower() in ['u','usb']:
-		return +1
-	if sideband.lower() in ['l','lsb']:
-		return -1
-	print('Unknown sideband "%s": expected one of L, LSB, U, or USB')
-	return 0
+class BandLabel:
+	'''Helper class to assign labels to frequency ranges'''
+
+	def __init__(self, name='b1', fstart_GHz=212.053, fstop_GHz=214.100):
+		self.name = name
+		if fstart_GHz > fstop_GHz:
+			fstart_GHz,fstop_GHz = fstop_GHz,fstart_GHz
+		self.fstart_GHz = fstart_GHz
+		self.fstop_GHz = fstop_GHz
+
+	def isContained(self, freq_GHz):
+		return freq_GHz >= self.fstart_GHz and freq_GHz <= self.fstop_GHz
+
+	def getName(self):
+		return self.name
 
 
-def int2sideband(sideband: int) -> str:
-	if sideband < 0:
-		return 'L'
-	return 'U'
+class EHTBandLabels:
+	'''Definitions of EHT frequency "bands"'''
+
+	def __init__(self):
+		self.bands = [
+			BandLabel('b1',212.053,214.100),
+			BandLabel('b2',214.100,216.148),
+			BandLabel('b3',226.052,228.100),
+			BandLabel('b4',228.100,230.148)
+		]
+
+	def lookUp(self, freq_GHz):
+		label = ''
+		for band in self.bands:
+			if band.isContained(freq_GHz):
+				label = band.getName()
+		return label
 
 
-def generate_chandefs(opts, pol='Lcp'):
+class NoemaVexFreqGenerator:
+	'''
+	VEX frequency block generator
+	Follows 202104_pNOEMA_VLBI_backend_5-9GHz_freq_setup.xlsx
+	'''
 
-	# PolyFiX correlator and VLBI interface board properties
-	bw = 64
-	Nchan = 64
-	Nchannels_per_thread = 4
-	Nthreads = Nchan // Nchannels_per_thread
-	Nthreads_per_10GbE = 4
+	def __init__(self, bandlabels=EHTBandLabels()):
+		self.lo1_GHz, self.lo2_GHz = 0, 0
+		self.recorders = []
+		self.bw_MHz = 64
+		self.pol2bbcnr = {'R':1, 'L':2, 'H':1, 'V':2}
+		self.indent = '   '
+		self.bandlabels = bandlabels
+		self.nvexchannels = 0
 
-	# User-specified tuning
-	sb1 = sideband2int(opts.sb1)
-	lo1 = float(opts.lo1)
-	lo2 = float(opts.lo2)
+	def __generate_block(self, rxUsb=True, outer=True, subbands=range(16)):
+		'''
+		fxUsb: True if upper receiver sideband USB,
+		outer: True if 'outer' rather than 'inner' polyphase filter bank portion (cf. NOEMA docs)
+		subbands: polyfix subband indices for which to compute the VEX channel frequencies
+		'''
+		lo2_sign, fIF2_sign = +1, +1
+		if not rxUsb: lo2_sign = -1
+		if not outer: fIF2_sign = -1
 
-	if pol.lower() in ['r','rcp']:
-		bbcnr = 1
-	else:
-		bbcnr = 2
+		refFreq_MHz = 1e3*(self.lo1_GHz + lo2_sign*self.lo2_GHz)
+		netSB_sign = lo2_sign * fIF2_sign
 
-	print('def Freq_Nq_%dx%d;' % (Nchan,bw))
-	print('  sample_rate =   %.1f Ms/sec;  * (2bits/sample)' % (2*bw))
-	for sb2 in [-1,+1]:
-		netsb = sb1*sb2
-		print('  * PolyFiX Polar %s - RX sideband %sSB - backend baseband %sSB - net %sSB' % (pol,int2sideband(sb1),int2sideband(sb2),int2sideband(netsb)))
-		for thread in range(0,Nthreads):
-			if (thread % Nthreads_per_10GbE) == 0:
-				tenGb_iface = thread // Nthreads_per_10GbE
-				print('  * 10GbE link %d' % (tenGb_iface))
-			for threadchannel in range(0,Nchannels_per_thread):
-				ch = thread*Nchannels_per_thread + threadchannel
-				# fcenter = lo1 + sb1*lo2 + sb2*ch*bw # would seem correct
-				fcenter = lo1 + sb1*lo2 + sb1*sb2*ch*bw # is what Excel sheet essentially uses, also makes sense, ch0 being referenced to middle not edge of IF
-				if netsb > 0:
-					fedge = fcenter - bw/2
-					flabel = 'U'
-				else:
-					fedge = fcenter + bw/2
-					flabel = 'L'
-				chan_def = '  chan_def = : %.1f MHz : %s : %.1f MHz : &Ch%02d : &BBC%02d : &%s;' % (fedge,flabel,bw,ch+1,bbcnr,pol)
-				note = ' * FX Ch:%d VDIF Th:%d Ch:%d mid %.1f' % (ch,thread,threadchannel,fcenter)
-				print(chan_def + note)
-	print('enddef;')
+		label = 'U'  # 'U' for USB
+		if netSB_sign <= 0:
+			label = 'L'
+
+		channels = []
+		for subband in subbands:
+			edge_freq = refFreq_MHz + netSB_sign*subband*self.bw_MHz - netSB_sign*self.bw_MHz/2
+			channels.append([edge_freq, label])
+
+		return channels
+
+
+	def __print_chan_def(self, freq_MHz, sideband, chNr, pollabel, bandlabel):
+
+		bbc = self.pol2bbcnr[pollabel]
+		if len(bandlabel) < 1:
+			print('%schan_def = &B: %.2f MHz : %1s : %.2f MHz : &CH%02d : &BBC%02d : &NoCal;' % (self.indent, freq_MHz, sideband, self.bw_MHz, chNr, bbc))
+		else:
+			print('%schan_def = &B: %.2f MHz : %1s : %.2f MHz : &CH%02d : &BBC%02d : &NoCal; * %s' % (self.indent, freq_MHz, sideband, self.bw_MHz, chNr, bbc, bandlabel))
+
+
+	def generate(self, lo1_GHz, lo2_GHz=7740.0, recorders=[1,2,3,4]):
+
+		def subblock(usb, outer, subbands, polzn):
+			channelblock = self.__generate_block(rxUsb=usb, outer=outer, subbands=subbands)
+			for idx,(freq_MHz,sideband) in enumerate(channelblock):
+				bandlabel = self.bandlabels.lookUp(freq_MHz*1e-3)
+				self.__print_chan_def(freq_MHz, sideband, idx + self.nvexchannels, polzn, bandlabel)
+			self.nvexchannels += len(subbands)
+
+		self.lo1_GHz, self.lo2_GHz = lo1_GHz, lo2_GHz
+		self.nvexchannels = 1
+
+		print('def FREQ_Nn; * derived for on lo1=%.3f lo2=%.3f GHz' % (lo1_GHz, lo2_GHz))
+		print('%ssample_rate = %.1f Ms/sec;  * (2bits/sample)' % (self.indent, 2*self.bw_MHz))
+
+		if 1 in recorders:
+
+			print('%s* Recorder 1, slot 1, LSB-Inner, RCP, subbands 0-15' % (self.indent))
+			subblock(False, False, range(16), 'R')
+			print('%s* Recorder 1, slot 2, LSB-Outer, RCP, subbands 0-15' % (self.indent))
+			subblock(False, True, range(16), 'R')
+			print('%s* Recorder 1, slot 3, LSB-Outer, LCP, subbands 0-15' % (self.indent))
+			subblock(False, True, range(16), 'L')
+			print('%s* Recorder 1, slot 4, LSB-Inner, LCP, subbands 0-15' % (self.indent))
+			subblock(False, False, range(16), 'L')
+
+		if 2 in recorders:
+
+			print('%s* Recorder 2, slot 1, LSB-Inner, RCP, subbands 32-47' % (self.indent))
+			subblock(False, False, range(32,48), 'R')
+			print('%s* Recorder 2, slot 2, LSB-Inner, RCP, subbands 16-31' % (self.indent))
+			subblock(False, False, range(16,32), 'R')
+			print('%s* Recorder 2, slot 3, LSB-Inner, LCP, subbands 16-31' % (self.indent))
+			subblock(False, False, range(16,32), 'L')
+			print('%s* Recorder 2, slot 4, LSB-Inner, LCP, subbands 32-47' % (self.indent))
+			subblock(False, False, range(32,48), 'L')
+
+		if 3 in recorders:
+
+			print('%s* Recorder 3, slot 1, USB-Inner, RCP, subbands 0-15' % (self.indent))
+			subblock(True, False, range(16), 'R')
+			print('%s* Recorder 3, slot 2, USB-Outer, RCP, subbands 0-15' % (self.indent))
+			subblock(True, True, range(16), 'R')
+			print('%s* Recorder 3, slot 3, USB-Outer, LCP, subbands 0-15' % (self.indent))
+			subblock(True, True, range(16), 'L')
+			print('%s* Recorder 3, slot 4, USB-Inner, LCP, subbands 0-15' % (self.indent))
+			subblock(True, False, range(16), 'L')
+
+		if 4 in recorders:
+
+			print('%s* Recorder 4, slot 1, USB-Inner, RCP, subbands 32-47' % (self.indent))
+			subblock(True, False, range(32,48), 'R')
+			print('%s* Recorder 4, slot 2, USB-Inner, RCP, subbands 16-31' % (self.indent))
+			subblock(True, False, range(16,32), 'R')
+			print('%s* Recorder 4, slot 3, USB-Inner, LCP, subbands 16-31' % (self.indent))
+			subblock(True, False, range(16,32), 'L')
+			print('%s* Recorder 4, slot 4, USB-Inner, LCP, subbands 32-47' % (self.indent))
+			subblock(True, False, range(32,48), 'L')
+
+		print('enddef;')
+
 
 if __name__ == "__main__":
-	opts = parse_args(sys.argv[1:])
-	generate_chandefs(opts)
 
+	opts = parse_args(sys.argv[1:])
+	lo1 = float(opts.lo1)
+	lo2 = float(opts.lo2)
+	recorders = [int(recNr) for recNr in opts.recorders.split(',')]
+
+	if lo2 > lo1:
+		print('Error: LO2 (-F %.3f GHz) should not be greater than LO1 (-f %.3f GHz)!' % (lo2, lo1))
+		sys.exit(1)
+
+	labels = EHTBandLabels()
+	gen = NoemaVexFreqGenerator(labels)
+	gen.generate(lo1, lo2, recorders)
