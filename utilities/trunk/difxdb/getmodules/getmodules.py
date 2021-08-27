32ea5d15946984a3b5dc7720b3bb0f9fc165cfd6
HelgeRottmann <rottmann@mpifr-bonn.mpg.de>
2021-08-27 18:49:04 +0000

    make python3 compatible
    git-svn-id: https://svn.atnf.csiro.au/difx@10127 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- utilities/trunk/difxdb/getmodules/getmodules.py
+++ utilities/trunk/difxdb/getmodules/getmodules.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python2
+#! /usr/bin/env python3
 # coding: latin-1
 
 #===========================================================================
@@ -41,12 +41,12 @@ __date__ ="$Date$"
 __lastAuthor__="$Author$"
 
 def printUsage():
-    print "%s   %s  %s (last changes by %s) \n" % (__prog__, __build__, __author__, __lastAuthor__)
-    print "A program to list all modules associated with an experiment\n"
-    print "Usage: %s <experiment_code>\n\n"  % __prog__
-    print "NOTE: %s requires the DIFXROOT environment to be defined." % __prog__
-    print "The program reads the database configuration from difxdb.ini located under $DIFXROOT/conf."
-    print "If the configuration is not found a sample one will be created for you."
+    print("%s   %s  %s (last changes by %s) \n" % (__prog__, __build__, __author__, __lastAuthor__))
+    print("A program to list all modules associated with an experiment\n")
+    print("Usage: %s <experiment_code>\n\n"  % __prog__)
+    print("NOTE: %s requires the DIFXROOT environment to be defined." % __prog__)
+    print("The program reads the database configuration from difxdb.ini located under $DIFXROOT/conf.")
+    print("If the configuration is not found a sample one will be created for you.")
 
     
     sys.exit(1)
@@ -82,33 +82,33 @@ if __name__ == "__main__":
         dbConn = Schema(connection)
         session = dbConn.session()
         
-	for expCode in expCodes:
-		expCode = upper(expCode)
-		try:
-			experiment = getExperimentByCode(session, expCode)
-		except:
-			raise Exception("Unknown experiment")
-			sys.exit
+        for expCode in expCodes:
+                expCode = expCode.upper()
+                try:
+                        experiment = getExperimentByCode(session, expCode)
+                except:
+                        raise Exception("Unknown experiment")
+                        sys.exit
 
-		sum = 0
-		count = 0
-		if (experiment is not None):
-			print "------------------"
-			print "%s" % expCode
-			print "------------------"
-			sortedModules = sorted(experiment.modules, key= attrgetter('stationCode'))
-			for module in sortedModules:
-				print module.vsn, module.slot.location, module.stationCode, module.capacity
-				count += 1
-				sum += module.capacity
+                sum = 0
+                count = 0
+                if (experiment is not None):
+                        print("------------------")
+                        print("%s" % expCode)
+                        print("------------------")
+                        sortedModules = sorted(experiment.modules, key= attrgetter('stationCode'))
+                        for module in sortedModules:
+                                print(module.vsn, module.slot.location, module.stationCode, module.capacity)
+                                count += 1
+                                sum += module.capacity
 
-			print "--------------------------------------------"
-			print "Total capacity: ", sum, "GB", " on", count , "modules"
-			print "\n\n"
-		
-	    
+                        print("--------------------------------------------")
+                        print("Total capacity: ", sum, "GB", " on", count , "modules")
+                        print("\n\n")
+                
+            
     except Exception as e:
-	sys.exit(e)
-	    
-	   
-	    
+        sys.exit(e)
+            
+           
+            
