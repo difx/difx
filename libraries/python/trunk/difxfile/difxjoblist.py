b8289edd0f18c6da68d1f80b89b30919470d2316
jwagnerhki <jan.wagner@iki.fi>
2021-10-04 07:48:03 +0000

    py3 difxjoblist.py
    git-svn-id: https://svn.atnf.csiro.au/difx@10226 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- libraries/python/trunk/difxfile/difxjoblist.py
+++ libraries/python/trunk/difxfile/difxjoblist.py
@@ -23,9 +23,26 @@
 # $LastChangedDate$
 #
 #============================================================================
-from string import split, strip, upper
+import sys
 #from difxutil import splitObservationCode, parseKeyValue
 import os
+
+if sys.version_info[0] <= 2:
+
+	from string import split, strip, upper
+
+else:
+
+	# TODO: get rid of these by rewriting DiFXJoblist code to use str.split() etc directly
+	def split(s, delim=None):
+		return s.split(delim)
+
+	def strip(s):
+		return s.strip()
+
+	def upper(s):
+		return s.upper()
+
 
 class DiFXJoblist:
 	"""
@@ -57,11 +74,11 @@ class DiFXJoblist:
 		# Parse the joblist file 
 		try:
 			data = open(os.path.join(self.path, self.filename)).readlines()
-		except Exception, ex:
+		except Exception as ex:
 			raise IOError(ex)
 
 		if len(data) < 2:
-			print 'Malformed .joblist file %s' % filename
+			print ('Malformed .joblist file %s' % (self.filename))
 			#TODO throw exception instead
 			exit(0)
 
@@ -71,7 +88,7 @@ class DiFXJoblist:
 
 		if len(self.parameters) < 1:
 			# TODO throw exception instead
-			print 'Malformed .joblist file %s line 1' % filename
+			print ('Malformed .joblist file %s line 1' % (self.filename))
 			exit(0)
 
 		# parse through the rest of the joblist file
@@ -92,7 +109,7 @@ class DiFXJoblist:
 				jobParts = split(jobName, '_')
 				if (len(jobParts) != 2):
 					#TODO throw exception instead
-					print 'Malformed job name "%s" in .joblist file should be of form name_number' % self.name
+					print ('Malformed job name "%s" in .joblist file should be of form name_number' % (self.name))
 					exit(0)
 				else:
 					job.name = jobName
@@ -155,15 +172,15 @@ class DiFXJoblist:
 
 			if len(badList) > 0:
 				# TODO throw an exception instead
-				print 'Some requested jobs are not in the .jobList file:', badList
+				print ('Some requested jobs are not in the .jobList file:', str(badList))
 				return []
 		return jobs
 					
 	
 	class Job(object):
-                '''
-                Inner class to handle Job
-                '''
+		'''
+		Inner class to handle Job
+		'''
 
 		
 
@@ -171,7 +188,7 @@ class DiFXJoblist:
 			'''
 			Constructor
 			'''
-                        self.vlbaAntennas = ['BR', 'FD', 'HN', 'KP', 'LA', 'MK', 'NL', 'OV', 'PT', 'SC']
+			self.vlbaAntennas = ['BR', 'FD', 'HN', 'KP', 'LA', 'MK', 'NL', 'OV', 'PT', 'SC']
 
 			# check that path exists
 			if not os.path.isdir(path):
@@ -211,14 +228,14 @@ class DiFXJoblist:
 			Returns true if all checks succeeded, false otherwise
 			'''
 			if self.nPulsarBins > 1 and self.nSourceFiles > 1:
-				print "Can't do multiple phase centres and pulsar mode in one pass!"
+				print ("Can't do multiple phase centres and pulsar mode in one pass!")
 				return False
 
 			extensions = ['.calc', '.input']
 			for extension in extensions:
 				file = os.path.join(self.path, self.name + extension)
 				if not os.path.isfile(file):
-					print 'Error : %s not found' % file
+					print ('Error : %s not found' % (file))
 					return False
 			if preCorr:
 				if not self.inputSanityCheck(os.path.join(self.path, self.name + '.input')):
@@ -226,7 +243,7 @@ class DiFXJoblist:
 			else:
 				ddir = os.path.join(self.path, self.name + '.difx')
 				if not os.path.isdir(ddir):
-					print 'difx output dir %s not found' % ddir
+					print ('difx output dir %s not found' % (ddir))
 					 
 					return False
 			return True
@@ -245,24 +262,36 @@ class DiFXJoblist:
 				s = split(strip(d))
 				if len(s) == 3:
 					if s[0] == 'FILE' and upper(s[2]) == 'NONE':
-						print 'Error: %s line %d: Cannot have VSN or filename of "%s"' \
-							% (inputFile, l, s[2])
+						print ('Error: %s line %d: Cannot have VSN or filename of "%s"' \
+							% (inputFile, l, s[2]))
 						OK = False
 				l += 1
 
-                        return OK
+			return OK
   
-def parseKeyValue(str):
+def parseKeyValue(istr):
 	"""
 	Parses a string to find key=value pairs
 	"""
 	kv = {}
-	ss = split(str)
+	ss = split(istr)
 	for s in ss:
 		p = split(s, '=')
 		if len(p) != 2:
 			# TODO throw exception instead
-			print 'Error parsing key=value statement: %s', s
+			print ('Error parsing key=value statement: %s' % (s))
 			return {}
 		kv[p[0]] = p[1]
 	return kv
+
+if __name__ == "__main__":
+	# run python difxjoblist <jobdir> <jobfilename> to execute this test code
+	if len(sys.argv) < 3:
+		print("difxjoblist <jobdir> <jobfilename>")
+		sys.exit(1)
+
+	joblist = DiFXJoblist(sys.argv[1], sys.argv[2])
+	for job in joblist.getJobs():
+		job.countNonVLBAAntennas()
+		job.validate()
+
