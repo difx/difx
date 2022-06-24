609e6dca2a423a8b57edfae8681b3165bd08264a
CormacReynolds <Cormac.Reynolds@csiro.au>
2022-06-24 02:25:04 +0000

    Split functionality of espresso.py into separate prep and launch scripts to work better with singularity image.
    git-svn-id: https://svn.atnf.csiro.au/difx@10516 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- applications/espresso/trunk/espressolib.py
+++ applications/espresso/trunk/espressolib.py
@@ -33,6 +33,7 @@ from email.mime.text import MIMEText
 import astropy.time
 import datetime
 import subprocess
+import fileinput
 
 
 def get_corrhosts(hostsfilename):
@@ -260,8 +261,8 @@ def convertdate(indate, outformat="mjd", strict=True):
     date = None
     for timeformat in timeformats1:
         try:
-        # MJD?
             if timeformat == "mjd":
+                # MJD should be treated as float
                 indate = float(indate)
             date = astropy.time.Time(indate, format=timeformat)
         except ValueError:
@@ -346,7 +347,7 @@ class Email:
         try:
             self.server.login(self.user, self.passwd)
         except Exception as connectionError:
-            print (connectionError)
+            print(connectionError)
             raise
 
     def sendmail(self, message):
@@ -400,7 +401,7 @@ def dhms2sec(time_in):
         dayhour = time_split[0].split("-")
         sec += int(dayhour.pop())*3600
         if dayhour:
-           sec += int(dayhour[0])*3600*24
+            sec += int(dayhour[0])*3600*24
 
     return sec
 
@@ -409,12 +410,12 @@ class batchenv:
     """Set the batch commands to use. SLURM and PBS are in principle both
     supported but PBS has not been tested in a long time. Everyone uses SLURM
     now, right?
-    
+
     If automatic detection does not work, can explicitly call self.set_style
     with either 'slurm' or 'pbs' as argument.
     """
 
-    def __init__(self, jobnames): 
+    def __init__(self, jobnames):
         self._jobnames = jobnames
         if which("sbatch"):
             self.set_style("slurm")
@@ -451,8 +452,9 @@ class batchenv:
             self.launch = "sbatch --parsable --export=ALL"
             #self.cancel = "scancel -n"
             self.cancel = "scancel"
-            self.stats = ("sacct -j {:s} -X --format "
-                "JobName%-15,JobId,elapsed,NNodes,Time,Reserved")
+            self.stats = (
+                    "sacct -j {:s} -X --format "
+                    "JobName%-15,JobId,elapsed,NNodes,Time,Reserved")
             self.q = self._batchq_slurm(self._jobnames)
             self.speedup = self._slurm_speedup
         elif self._style == "pbs":
@@ -472,7 +474,7 @@ class batchenv:
         """Run an sacct command and parse the output to calculate job
         speedup
         """
-    
+
         speedup = 0.
         speedup_wait = 0.
         try:
@@ -493,7 +495,7 @@ class batchenv:
         except:
             speedup = 0.0
             speedup_wait = 0.0
-    
+
         return speedup, speedup_wait
 
     def _pbs_speedup(self, jobid, joblen):
@@ -501,10 +503,10 @@ class batchenv:
         return 0.0, 0.0
 
     def _batchq_slurm(self, jobnames):
-        """Return a SLURM queue command for the current jobs. 
-        
+        """Return a SLURM queue command for the current jobs.
+
         Make sure the squeue format accommodates the full job name"""
-    
+
         longest = 0
         batch_q = None
         for jobname in jobnames:
@@ -516,3 +518,107 @@ class batchenv:
         batch_q = 'squeue -o "{0:s}" -n {1:s}'.format(
                 squeue_format, ",".join(jobnames))
         return batch_q
+
+
+def parse_joblistfile(joblistfilename, set_speedup=None):
+    """Get the full list of jobs from the joblist file
+
+    Return a dictionary. Keys are the job names. For each jobname record a list
+    of stations under key 'stations' and the job length under key
+    'joblen'
+    """
+
+    def predict_speedup(tops, joblen):
+        """Placeholder for future idea - could be complicated"""
+        speedup = (joblen*0.8e5)/tops
+        speedup = 1.0
+        return speedup
+
+
+    joblistfile = open(joblistfilename).readlines()
+
+    joblistfile.pop(0)
+    joblist = dict()
+    for line in joblistfile:
+        jobvals = line.split()
+        jobname, jobstart, jobend = jobvals[0:3]
+        tops = float(jobvals[6])
+        joblen = (float(jobend) - float(jobstart))
+        if set_speedup is None:
+            speedup = predict_speedup(tops, joblen)
+        else:
+            speedup = set_speedup
+        joblist[jobname] = dict()
+        stations = re.search(r"#\s+(.*)", line).group(1)
+        joblist[jobname]["stations"] = stations
+
+        job_time = joblen/speedup
+        # add 10 minutes for startup
+        job_time += 10./(24.*60.)
+        days, hours, minutes, seconds = daysToDhms(job_time)
+        joblist[jobname]["jobtime"] = (
+                "{0:02d}:{1:02d}:{2:02d}".format(hours, minutes, seconds))
+        joblist[jobname]["joblen"] = (joblen*60*24)
+
+    return joblist
+
+
+def parse_binconfig(binconfigfilename):
+    """extract file names from the binconfig file"""
+
+    binconfigfile = open(binconfigfilename).readlines()
+    polycofilename = str()
+    for line in binconfigfile:
+        # remove comments
+        line = re.sub(r"@.*", "", line)
+        polycomatch = re.search(r"POLYCO FILE \d+\s*:\s*(\S*)", line)
+        if polycomatch:
+            polycofilename = polycomatch.group(1)
+
+    return polycofilename
+
+
+def parse_v2dfile(v2dfilename):
+    """extract file names from the v2d file"""
+
+    v2dfile = open(v2dfilename).readlines()
+    vexfilename = str()
+    binconfigfilename = None
+    for line in v2dfile:
+        # remove comments
+        line = re.sub(r"#.*", "", line)
+        vexmatch = re.search(r"vex\s*=\s*(\S*)", line)
+        if vexmatch:
+            vexfilename = vexmatch.group(1)
+        binconfigmatch = re.search(r"binConfig\s*=\s*(\S*)", line)
+        if binconfigmatch:
+            binconfigfilename = binconfigmatch.group(1)
+
+    return vexfilename, binconfigfilename
+
+
+
+
+def change_path(inputfilename, changeparm, oldpath, newpath):
+    """modify paths in the input file"""
+
+    for line in fileinput.FileInput(inputfilename, inplace=1, backup=".org"):
+        if re.match(changeparm, line):
+            line = line.replace(oldpath, newpath)
+        print (line, end="")
+
+    fileinput.close()
+
+
+def get_difxlogname(outdir, jobname):
+    """Return difx log file name"""
+
+    return outdir + "/" + jobname + ".difxlog"
+
+def wait_for_file(filename):
+    """Wait until specified file is available"""
+
+    while not os.path.exists(filename):
+        print ("Waiting for", filename)
+        time.sleep(1)
+    print (filename, "found!")
---
+++ applications/espresso/trunk/espressoprep.py
@@ -0,0 +1,304 @@
+#!/usr/bin/env python
+# =======================================================================
+# Copyright (C) 2022 Cormac Reynolds
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+# Free Software Foundation, Inc.,
+# 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+# =======================================================================
+
+# take care of a few logistics before launching the correlator
+# suitable for use in singularity container
+# Cormac Reynolds: June 2022
+
+
+from __future__ import print_function, division
+import subprocess
+import optparse
+import re
+import shutil
+import os
+import sys
+import time
+import fileinput
+import pprint
+import espressolib
+
+
+def run_vex2difx(v2dfilename, vex2difx_options):
+    """run vex2difx, and wait for completion"""
+
+    command = " ".join(["vex2difx", vex2difx_options, v2dfilename])
+    print (command)
+    subprocess.check_call(command, stdout=sys.stdout, shell=True)
+
+
+def run_calcif2(jobname, calcfilename):
+    """tidy up old calcif2 files and run calcif2 again"""
+
+    calcoutputfiles = [".uvw", ".rate", ".im", ".delay"]
+    for filename in calcoutputfiles:
+        # clear up old calc files so we are sure it completed
+        filename = jobname + filename
+        if os.path.exists(filename):
+            os.remove(filename)
+    command = " ".join(["calcif2", calcfilename])
+    print (command)
+    subprocess.check_call(command, stdout=sys.stdout, shell=True)
+
+
+def make_new_runfiles(
+        jobname, expname, jobtime, difx_message_port, difx_image,
+        ntasks_per_node=1):
+    """make copies of the prototype run and .thread files"""
+
+    runfile = "run_" + jobname
+    shutil.copy("run", runfile)
+    for line in fileinput.FileInput(runfile, inplace=1):
+        # update placeholders in prototype
+        line = re.sub(r"{JOBNAME}", jobname, line)
+        line = re.sub(r"{TIME}", jobtime, line)
+        line = re.sub(r"{DIFX_MESSAGE_PORT}", difx_message_port, line)
+        line = re.sub(r"{NTASKS-PER-NODE}", ntasks_per_node, line)
+        line = re.sub(
+                r"{DIFXLOGNAME}", 
+                espressolib.get_difxlogname(outdir, jobname),
+                line)
+        line = re.sub(r"{DIFX_IMAGE}", difx_image, line)
+        print (line, end="")
+    fileinput.close()
+    os.chmod(runfile, 0o775)
+
+    threadfilename = jobname + ".threads"
+    shutil.copy(expname + ".threads", threadfilename)
+
+    machinesfilename = jobname + ".machines"
+    shutil.copy(expname + ".machines", machinesfilename)
+
+
+def run_lbafilecheck(
+        datafilename, stations, computehead, no_rmaps_seq, interactive,
+        ntasks_per_node):
+    """run lbafilecheck creating machines and .threads files for this job"""
+
+    stations = stations.strip()
+    stations = re.sub(r"\s+", ",", stations)
+    stations = "'" + stations + "'"
+    #options = ""
+    options = " ".join(["--ntasks_per_node", str(ntasks_per_node)])
+    if computehead:
+        options = " ".join([options, "-H"])
+    if no_rmaps_seq:
+        options = " ".join([options, "-M"])
+    if not interactive:
+        options = " ".join([options, "-n"])
+
+    command = " ".join(
+            ["lbafilecheck.py -F", options, "-s", stations, datafilename])
+    print (command)
+    subprocess.check_call(command, stdout=sys.stdout, shell=True)
+
+
+def get_options(usage):
+    '''parse the options'''
+    parser = optparse.OptionParser(usage=usage, version="%prog " + "1.0")
+    parser.add_option(
+            "--force", "-f",
+            dest="force", action="store_true", default=False,
+            help="run vex2difx with -f switch")
+    parser.add_option(
+            "--clock", "-c",
+            dest="clockjob", action="store_true", default=False,
+            help="Store output in a clock subdirectory. Also pass -f to"
+            " vex2difx")
+    parser.add_option(
+            "--test", "-t", dest="testjob", action="store_true", default=False,
+            help="Store output in a test subdirectory. Also passes -f to"
+            " vex2difx")
+    parser.add_option(
+            "--nocalc", "-C",
+            dest="nocalc", action="store_true", default=False,
+            help="Do not re-run calc")
+    parser.add_option(
+            "--novex", "-n",
+            dest="novex", action="store_true", default=False,
+            help="Do not re-run vex2difx")
+    parser.add_option(
+            "--nopause", "-p",
+            dest="nopause", action="store_true", default=False,
+            help="Do not pause after running calc - proceed straight to "
+            " correlation")
+    parser.add_option(
+            "--nocomputehead", "-H",
+            dest="computehead", action="store_false", default=True,
+            help="Don't Use head and datastream nodes as compute nodes")
+    parser.add_option(
+            "--no_rmaps_seq", "-M",
+            dest="no_rmaps_seq", action="store_true", default=False,
+            help="Don't Pass the '--mca rmaps seq' instruction to mpirun")
+    parser.add_option(
+            "--jobtime", "-j",
+            type="str", dest="jobtime", default=None,
+            help="Max. job time for batch jobs."
+            " Default is joblength * speedup + 10 mins."
+            " Format = hh:mm:ss")
+    parser.add_option(
+            "--speedup", "-s",
+            type="float", dest="predicted_speedup", default=0.7,
+            help="Predicted speedup factor to determine job run time."
+            " Default=%default")
+    parser.add_option(
+            "--ntasks_per_node",
+            type="int", dest="ntasks_per_node", default=2,
+            help="Number of MPI processes per node. Default=%default")
+    parser.add_option(
+            "--interactive", "-i",
+            dest="interactive", action="store_true", default=False,
+            help="Run interactively, else assume slurm/pbs batch jobs")
+    
+    (options, args) = parser.parse_args()
+    return options, args
+
+
+# Main program start.
+usage = """%prog <expname>
+    will:
+    run "vex2difx <expname>.v2d"
+    correct the output file names
+    run calcif2
+    prepare job launch files (run*, *threads, *machines)
+
+"""
+
+options, args = get_options(usage)
+
+if options.testjob and options.clockjob:
+    raise Exception("Don't use both -t and -c together!")
+
+if len(args) < 1:
+    parser.print_help()
+    parser.error("Give v2d filename")
+
+# Can be overridden with the options switch.
+ntasks_per_node = int(options.ntasks_per_node)
+# multiple tasks per node only makes sense with computehead=true
+if ntasks_per_node > 1:
+    if options.computehead is False:
+        print ("ntasks_per_node > 1, setting computehead to True")
+        options.computehead = True
+
+# Determine the name of the correlator pass from the v2d filename
+v2dfilename = args[0] + ".v2d"
+print ("v2dfilename", v2dfilename)
+passname = re.sub(".v2d$", "", v2dfilename)
+print ("passname", passname)
+
+# a '-' is used to distinguish different correlator passes. If only one pass,
+# then the expname and passname are the same
+passid = str()
+expname = passname
+if "-" in passname:
+    expname, passid = expname.split("-")
+
+# preliminaries: clear old input files out of the way
+if not options.novex:
+   for filename in os.listdir(os.getcwd()):
+       if re.match(passname + r"_\d+\.input$", filename):
+           os.rename(filename, filename + ".bak")
+
+vex2difx_options = ""
+if options.clockjob or options.testjob or options.force:
+    vex2difx_options = "-f"
+
+# run vex2difx.
+vexfilename, binconfigfilename = espressolib.parse_v2dfile(v2dfilename)
+if not vexfilename:
+    raise Exception("Could not find VEX file in " + v2dfilename)
+
+if binconfigfilename:
+    polycofilename = espressolib.parse_binconfig(binconfigfilename)
+    if not polycofilename:
+        raise Exception("Could not find polycofile in " + binconfigfilename)
+
+if not options.novex:
+    run_vex2difx(v2dfilename, vex2difx_options)
+
+joblistfilename = passname + ".joblist"
+(corrjoblist) = espressolib.parse_joblistfile(
+        joblistfilename, options.predicted_speedup)
+
+print ("job list:\n", pprint.pformat(corrjoblist), "\n")
+
+# get the paths of our input and output directories
+indir = os.getcwd() + os.sep
+try:
+    outdirbase = os.environ.get("CORR_DATA") + os.sep
+except:
+    raise Exception("You must set $CORR_DATA to an output data directory!")
+
+outdir = outdirbase + expname + os.sep
+if options.clockjob:
+    outdir += "clocks/"
+if options.testjob:
+    outdir += "test/"
+
+# do the prep work for each job.
+for jobname in sorted(corrjoblist.keys()):
+    # figure out filenames, directories, etc. using normal difx conventions
+    inputfilename = jobname + ".input"
+    calcfilename = jobname + ".calc"
+
+    # fix the output filename to point at the data disk
+    print ()
+    if not options.novex:
+        print (
+                "renaming the 'OUTPUT FILENAME' in", inputfilename, "from",
+                indir, "to", outdir)
+        espressolib.change_path(
+                inputfilename, 'OUTPUT FILENAME:', indir, outdir)
+
+    # run calcif2
+    if not options.nocalc:
+        run_calcif2(jobname, calcfilename)
+
+
+# set the $DIFX_MESSAGE_PORT 
+difx_message_port = os.environ.get("DIFX_MESSAGE_PORT", 50201)
+difx_image = os.environ.get("DIFX_IMAGE", "")
+
+# create the mpi files for each job
+for jobname in sorted(corrjoblist.keys()):
+    # run lbafilecheck to get the new machines and .threads files
+
+    datafilename = expname + ".datafiles"
+    run_lbafilecheck(
+            datafilename, corrjoblist[jobname]["stations"],
+            options.computehead, options.no_rmaps_seq, options.interactive,
+            ntasks_per_node)
+
+    # duplicate the run and thread and machines files for the full number of
+    # jobs
+    print (
+            "duplicating the run file, machines file and .threads files for",
+            jobname, "\n")
+    if options.jobtime:
+        jobtime = options.jobtime
+    else:
+        jobtime = corrjoblist[jobname]["jobtime"]
+
+    make_new_runfiles(
+            jobname, expname, jobtime, str(difx_message_port), str(difx_image),
+            str(ntasks_per_node))
+
+print("Job prep complete. Now launch the run files at your leisure. espressolaunch.py may do this for you.")
