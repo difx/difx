18742cbda3cb81039e96bca3214acf6490d69168
jwagnerhki <jan.wagner@iki.fi>
2022-08-18 19:59:08 +0000

    untidy extension of difxCheckMk6Modulepresence.py with grep-like args '-l' and '-L' to list complete/incomplete jobs only
    git-svn-id: https://svn.atnf.csiro.au/difx@10601 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- sites/MPIfR/mark6/difxCheckMk6Modulepresence.py
+++ sites/MPIfR/mark6/difxCheckMk6Modulepresence.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''
-Usage: difxCheckMk6Modulepresence.py <difxjob_0001.input> [<difxjob_0002.input> ...]
+Usage: difxCheckMk6Modulepresence.py [-l|-L] <difxjob_0001.input> [<difxjob_0002.input> ...]
 
 Verifies that any files referenced by the DiFX jobs are indeed available.
 
@@ -12,6 +12,10 @@ Files that do not match the above pattern are considered to be e-transferred
 or copied Mark6 VDIF files that reside on the cluster file system. Accessibility
 in this case is verified by a file size check done on the local host.
 
+Options:
+ -l    list only those jobs where all files all available
+ -L    list only those jobs that have inaccessible files
+
 (C) 2020 Jan Wagner
 '''
 
@@ -21,19 +25,21 @@ in this case is verified by a file size check done on the local host.
 import subprocess
 import sys
 
-def checkDatastreamAvailabilities(difxinputfile, checkLocalFiles=True):
+def checkDatastreamAvailabilities(difxinputfile, checkLocalFiles=True, verbose=True, listCompletes=False, listIncompletes=False):
 	'''
 	Collect all DATASTREAM entries from an .input file and check for availability of the
 	referenced Mark6 FUSE files via SSH to the respective hosts on the MPIfR VLBI cluster.
 	Optionally look up and check availability of non-Mark6 DATASTREAM files on BeeGFS.
 	'''
 
-	print ('Job %s : ' % (difxinputfile))
+	if verbose:
+		print ('Job %s : ' % (difxinputfile))
 
 	with open(difxinputfile, 'r') as f:
 
 		datastreamfiles = []
 		clusterfsfiles = []
+		isComplete = True
 
 		for line in f:
 			if not 'FILE ' in line: continue
@@ -57,25 +63,49 @@ def checkDatastreamAvailabilities(difxinputfile, checkLocalFiles=True):
 			host = host.split('_fuse')[0]
 			# print (host, st)
 			if len(st)<1:
-				print('%s %s : MISSING' % (host,dstreamfile))
+				isComplete = False
+				if verbose:
+					print('%s %s : MISSING' % (host,dstreamfile))
 			else:
-				print('%s %s : ok' % (host,dstreamfile))
+				if verbose:
+					print('%s %s : ok' % (host,dstreamfile))
 
 		if checkLocalFiles:
 			for clusterfsfile in clusterfsfiles:
 				p = subprocess.Popen(["/usr/bin/du", "-h", clusterfsfile], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 				(st,rc) = p.communicate()
 				if len(st)<1:
-					print('%s %s : MISSING' % ("localhost",clusterfsfile))
+					isComplete = False
+					if verbose:
+						print('%s %s : MISSING' % ("localhost",clusterfsfile))
 				else:
-					print('%s %s : ok' % ("localhost",clusterfsfile))
+					if verbose:
+						print('%s %s : ok' % ("localhost",clusterfsfile))
 
+		if listCompletes and isComplete:
+			print(difxinputfile)
+		if listIncompletes and not isComplete:
+			print(difxinputfile)
 
 if __name__ == "__main__":
+
+	verbose = True
+	listCompleteJobs = False
+	listIncompleteJobs = False
 
 	if '--help' in sys.argv or '-h' in sys.argv:
 		print(__doc__)
 		sys.exit(0)
 
+	if '-l' in sys.argv:
+		verbose = False
+		listCompleteJobs = True
+		sys.argv.remove('-l')
+
+	if '-L' in sys.argv:
+		verbose = False
+		listIncompleteJobs = True
+		sys.argv.remove('-L')
+
 	for difxinputfile in sys.argv[1:]:
-		checkDatastreamAvailabilities(difxinputfile)
+		checkDatastreamAvailabilities(difxinputfile, verbose=verbose, listCompletes=listCompleteJobs, listIncompletes=listIncompleteJobs)
