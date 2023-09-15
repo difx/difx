71efe62ea4a49ace50b2810345d8feddc76f1555
gbcrew <gbc@haystack.mit.edu>
2023-09-15 16:09:07 +0000

    Completed the test and enabled it.
    git-svn-id: https://svn.atnf.csiro.au/difx@11097 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- applications/difxcalc11/trunk/check_stations.sh
+++ applications/difxcalc11/trunk/check_stations.sh
@@ -8,19 +8,28 @@
 #      Parameter (MaxStat = 41)
 #[gbc@gefera trunk]$ grep Nstation2 src/c2poly.i | grep =
 #      Parameter (Nstation2 = 41)
+# srcdir and verbose may be supplied via environment variables
 #
-srcdir=.
-set -- `grep Max_Stat $srcdir/src/cmxst11.i | grep = | tr -s ')(=' '   '` \
-    `grep MaxStat $srcdir/src/d_input.i | grep = | tr -s ')(=' '   '` \
-    `grep Nstation2 $srcdir/src/c2poly.i | grep = | tr -s ')(=' '   '`
+[ -n "$srcdir" ] || srcdir=.
+[ -n "$verbose" ] && verbose=true || verbose=false
+set -- `grep Max_Stat $srcdir/src/cmxst11.i |& grep = | tr -s ')(=' '   '` \
+    `grep MaxStat $srcdir/src/d_input.i |& grep = | tr -s ')(=' '   '` \
+    `grep Nstation2 $srcdir/src/c2poly.i |& grep = | tr -s ')(=' '   '`
+[ $# -eq 9 ] || {
+    echo Sources not found or have changed too much, dollar-hash is $#
+    exit 1
+}
 a=$3 b=$6 c=$9
+$verbose &&
+    echo grep gave $# items &&
+    echo Max_Stat is $a &&
+    echo MaxStat is $b &&
+    echo Nstation2 is $c
 b=$((b - 1))
 err=0
 [ $a -eq $b ] || { echo Max_Stat is $a and MaxStat-1 is $b ; err=$(($err+1)); }
 [ $c -eq $b ] || { echo Nstation2 is $c and MaxStat-1 is $b ; err=$(($err+1)); }
-#echo $a
-#echo $b
-#echo $c
+$verbose && echo Exit status $err
 exit $err
 #
 ## eof
