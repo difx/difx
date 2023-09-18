9d2296fe94e6d3cdd7bcd90a7591f85b660bc5cf
shaoguangleo <sgguo@shao.ac.cn>
2023-09-18 11:20:25 +0000

    py3 compatible
    git-svn-id: https://svn.atnf.csiro.au/difx@11104 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

--- applications/calc/trunk/calc9/check_tai.py
+++ applications/calc/trunk/calc9/check_tai.py
@@ -22,12 +22,12 @@ for line in lines:
         datsec = int(splitline[2][3:-2])
 
 if latestsec == datsec:
-    print "UTC-TAI.dat and dat.for agree the latest UTC-TAI value is " + str(latestsec)
+    print("UTC-TAI.dat and dat.for agree the latest UTC-TAI value is " + str(latestsec))
 else:
-    print "WARNING!!!! dat.for seems to be out of date on the leap seconds."
-    print "dat.for has the latest leap second as " + str(datsec)
-    print "UTC-TAI has the latest leap second as " + str(latestsec)
+    print("WARNING!!!! dat.for seems to be out of date on the leap seconds.")
+    print("dat.for has the latest leap second as " + str(datsec))
+    print("UTC-TAI has the latest leap second as " + str(latestsec))
     if dowait:
-        raw_input("Don't press enter until you've updated dat.for, recompiled it *AND* recompiled gencalc_delays!!!")
+        input("Don't press enter until you've updated dat.for, recompiled it *AND* recompiled gencalc_delays!!!")
     else:
         print("Don't press enter until you've updated dat.for, recompiled it *AND* recompiled gencalc_delays!!!")
