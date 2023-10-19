59e64effcb27e83c6fe014eff643ab735f3fc5bd
Jan Wagner <jwagner@mpifr.de>
2023-10-19 10:11:49 +0200

    extend class fraction with a bunch of maths operators

--- mpifxcorr/src/fraction.h
+++ mpifxcorr/src/fraction.h
@@ -16,6 +16,10 @@ class fraction {
       fraction(int numerator, int denominator) : num(numerator), denom(denominator) { }
       fraction(long numerator, long denominator) : num(numerator), denom(denominator) { }
 
+      ~fraction() { }
+
+      // Copy constructors
+
       fraction& operator= (const fraction& o) {
           this->num = o.num;
           this->denom = o.denom;
@@ -45,7 +49,84 @@ class fraction {
           this->denom = 1;
       }
 
-      ~fraction() { }
+      // Math operators
+
+      fraction& operator+ (const long& m) {
+          this->num += m * this->denom;
+          return *this;
+      }
+      fraction operator+ (const long& m) const {
+          return fraction(this->num + m * this->denom, this->denom);
+      }
+
+      fraction& operator- (const long& m) {
+          this->num -= m * this->denom;
+          return *this;
+      }
+      fraction operator- (const long& m) const {
+          return fraction(this->num - m * this->denom, this->denom);
+      }
+
+      fraction& operator* (const long& m) {
+          this->num *= m;
+          return *this;
+      }
+      fraction operator* (const long& m) const {
+          return fraction(m * this->num, this->denom);
+      }
+
+      fraction& operator/ (const long& d) {
+          if ((this->num % d) == 0) {
+              this->num /= d;
+          } else {
+              this->denom *= d;
+          }
+          return *this;
+      }
+      fraction operator/ (const long& d) const {
+          if ((this->num % d) == 0) {
+              return fraction(this->num / d, this->denom);
+          } else {
+              return fraction(this->num, d * this->denom);
+          }
+      }
+
+      // Math operators with other fractions
+
+      fraction& operator+ (const fraction& rhs) {
+          if (this->denom == rhs.denom) {
+              this->num += rhs.num;
+          } else {
+              this->num = this->num * rhs.denom + rhs.num * this->denom;
+              this->denom = this->denom * rhs.denom;
+          }
+          return *this;
+      }
+      fraction operator+ (const fraction& rhs) const {
+          if (this->denom == rhs.denom) {
+              return fraction(this->num + rhs.num, this->denom);
+          } else {
+              return fraction(this->num * rhs.denom + rhs.num * this->denom, this->denom * rhs.denom);
+          }
+      }
+
+      fraction& operator- (const fraction& rhs) {
+          if (this->denom == rhs.denom) {
+              this->num -= rhs.num;
+          } else {
+              this->num = this->num * rhs.denom - rhs.num * this->denom;
+              this->denom = this->denom * rhs.denom;
+          }
+          return *this;
+      }
+      fraction operator- (const fraction& rhs) const {
+          if (this->denom == rhs.denom) {
+              return fraction(this->num - rhs.num, this->denom);
+          } else {
+              return fraction(this->num * rhs.denom - rhs.num * this->denom, this->denom * rhs.denom);
+          }
+      }
+
 
    public:
 
@@ -63,5 +144,14 @@ class fraction {
 
       long num, denom;
 };
+
+inline bool operator==(const fraction& lhs, const fraction& rhs) {
+    return (lhs.num == rhs.num && lhs.denom == rhs.denom)
+           || ((lhs.num * rhs.denom - rhs.num * lhs.denom) == 0);
+}
+
+inline bool operator==(const fraction& lhs, const long& rhs) {
+    return ((lhs.num - rhs * lhs.denom) == 0);
+}
 
 #endif
