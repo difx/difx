64fe167522435948b095abe6a014687b79a5ddf7
Shaoguang Guo(Sky & Star) <sgguo@shao.ac.cn>
2024-04-08 08:44:47 +0800

    Clean up SVN properties for mark6sg

--- libraries/mark6sg/mark6sg/mark6_sg_vfs.c
+++ libraries/mark6sg/mark6sg/mark6_sg_vfs.c
@@ -16,16 +16,6 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
-//===========================================================================
-// SVN properties (DO NOT CHANGE)
-//
-// $Id: mark6_sg_vfs.c 10551 2022-07-26 12:41:48Z JanWagner $
-// $HeadURL: https://svn.atnf.csiro.au/difx/master_tags/DiFX-2.8.1/libraries/mark6sg/mark6sg/mark6_sg_vfs.c $
-// $LastChangedRevision: 10551 $
-// $Author: JanWagner $
-// $LastChangedDate: 2022-07-26 20:41:48 +0800 (二, 2022-07-26) $
-//
-//============================================================================
 #include "mark6sg.h"
 #include "mark6_sg_vfs_impl.h"
 #include "mark6_sg_format.h"
