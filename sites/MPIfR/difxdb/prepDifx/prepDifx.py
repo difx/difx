1ae3ff8a0d1b01776c075b1d7d49daa7158c3fd8
Shaoguang Guo(Sky & Star) <sgguo@shao.ac.cn>
2024-04-09 19:12:41 +0800

    Clean up SVN properties for sites

--- sites/MPIfR/difxdb/prepDifx/prepDifx.py
+++ sites/MPIfR/difxdb/prepDifx/prepDifx.py
@@ -1,16 +1,6 @@
 #! /usr/bin/env python3
 # coding: latin-1
 
-#===========================================================================
-# SVN properties (DO NOT CHANGE)
-#
-# $Id: prepDifx.py 10500 2022-06-10 13:03:43Z HelgeRottmann $
-# $HeadURL: https://svn.atnf.csiro.au/difx/sites/MPIfR/difxdb/prepDifx/prepDifx.py $
-# $LastChangedRevision: 10500 $
-# $Author: HelgeRottmann $
-# $LastChangedDate: 2022-06-10 21:03:43 +0800 (五, 2022-06-10) $
-#
-#============================================================================
 
 import argparse
 import os
