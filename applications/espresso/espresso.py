731f094c338d93c060dc5ab5df57d38efd0a5ad2
Cormac Reynolds <cormac.reynolds@csiro.au>
2024-09-25 14:24:11 +0800

    Change criteria for marking job 'Incomplete'

--- applications/espresso/espresso.py
+++ applications/espresso/espresso.py
@@ -438,7 +438,7 @@ def run_batch(corrjoblist, outdir):
         #time.sleep(1)
         job_logfilename = get_difxlogname(outdir, jobname)
         if os.path.exists(job_logfilename):
-            job_ok, job_errors = chk_difxlog(open(job_logfilename).readlines())
+            job_ok, job_errors, job_finish = chk_difxlog(open(job_logfilename).readlines(), jobname)
             job_errors = [
                     "{:s}: {:s}".format(jobname, err) for err in job_errors]
         else:
@@ -453,7 +453,7 @@ def run_batch(corrjoblist, outdir):
         if batch.stats is not None:
             # print the user stats
             job_status = "Incomplete"
-            if job_ok:
+            if job_finish:
                 job_status = "Complete"
             jobinfo = "{:s} {:.2f}m {:s}\n{:s} ({:d})\n".format(
                     jobname, corrjoblist[jobname]["joblen"], job_status,
@@ -538,12 +538,12 @@ def write_difxlog(log_in, outdir, jobname):
     logfile.write(log_lines)
     logfile.close()
     job_errors = []
-    job_ok, job_errors = chk_difxlog(log_lines)
+    job_ok, job_errors, job_finish = chk_difxlog(log_lines, jobname)
 
     return job_ok, job_errors
 
 
-def chk_difxlog(logfile):
+def chk_difxlog(logfile, jobname):
     """Check for errors and shutdown messages in a difx log file."""
 
     job_finish = False
@@ -560,7 +560,7 @@ def chk_difxlog(logfile):
     if not job_finish:
         job_errors.append(jobname + " did not finish\n")
 
-    return job_ok, job_errors
+    return job_ok, job_errors, job_finish
 
 
 def print_queue(queue_command, jobnames):
