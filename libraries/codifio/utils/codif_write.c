9e731faf3d052373e52044cd39b3d86ef73f7092
Chris Phillips <Chris.Phillips@csiro.au>
2024-10-11 13:57:39 +1100

    Print time of first packet when waiting

--- libraries/codifio/utils/codif_write.c
+++ libraries/codifio/utils/codif_write.c
@@ -423,6 +423,24 @@ int main (int argc, char * const argv[]) {
     
     if (wait && first) {
       t0 = tim();
+
+      char buffer[26];
+      int millisec;
+      struct tm* tm_info;
+      struct timeval tv;
+
+      gettimeofday(&tv, NULL);
+      
+      millisec = lrint(tv.tv_usec/1000.0); // Round to nearest millisec
+      if (millisec>=1000) { // Allow for rounding up to nearest second
+	millisec -=1000;
+	tv.tv_sec++;
+      }
+
+      tm_info = localtime(&tv.tv_sec);
+
+      strftime(buffer, 26, "%Y:%m:%d %H:%M:%S", tm_info);
+      printf("%s.%03d\n", buffer, millisec);
     }
     
     // Block alarm signal while we are updating these values
