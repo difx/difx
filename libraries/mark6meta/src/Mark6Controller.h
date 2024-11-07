5edf363ee41002e040a7dafbf6b31787dbb48b30
Helge Rottmann <rottmann@mpifr-bonn.mpg.de>
2024-11-07 14:13:03 +0100

    Remove SVN properties

--- libraries/mark6meta/src/Mark6Controller.h
+++ libraries/mark6meta/src/Mark6Controller.h
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright (C) 2016  Max-Planck-Institut für Radioastronomie, Bonn, Germany 
+* Copyright (C) 2024  Max-Planck-Institut für Radioastronomie, Bonn, Germany 
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
 * the Free Software Foundation, either version 3 of the License, or
