e1bbb02a4827f99e341c7f52b144f8ae3b4020bc
gcrew <gbc@mit.edu>
2026-05-26 14:09:28 -0400

    pull WSB updates from dev to this branch from main
    Starting the process of providing the HOPS 3.26 vendor branch

--- applications/hops/postproc/fourfit/fft1.c
+++ applications/hops/postproc/fourfit/fft1.c
@@ -18,7 +18,7 @@
 #include <complex.h>
 #include "mk4_data.h"
 
-void FFT1 (complex In[MAXMAX*2], int NN, int ISign, complex Out[MAXMAX*2], int rev)    
+void FFT1 (complex In[MAXMAX*2], int NN, int ISign, complex Out[MAXMAX*2], int rev)
     {
     double wr,wi,wpr,wpi,wtemp,theta,tempr,tempi,Data[MAXMAX*8 + 1];
     int i,j,n,istep,mmax,m;
